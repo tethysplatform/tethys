591b0bb796a811b8f3ef6799b24d4017b09ca7df
Nathan Swain <nathan.swain86@gmail.com>
2014-08-28 10:53:05 -0600

    Old change password form.

--- tethys_site/views/user.py
+++ tethys_site/views/user.py
@@ -2,10 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
-
 from django.contrib import messages
 
-from tethys_site.forms import UserSettingsForm
+from tethys_site.forms import UserSettingsForm, UserPasswordChangeForm
 
 @login_required()
 def profile(request, username=None):
@@ -43,3 +42,33 @@ def settings(request, username=None):
     context = {'form': form}
 
     return render(request, 'tethys_site/user/settings.html', context)
+
+@login_required()
+def change_password(request, username=None):
+    if request.method == 'POST' and 'change-password-submit' in request.POST:
+        # Create a form populated with request data
+        form = UserPasswordChangeForm(user=request.user, data=request.POST)
+
+        if form.is_valid():
+            # Validate the old and new passwords
+            form.clean_current_password()
+            form.clean_confirm_password()
+
+            # If no exceptions raised to here, then the old password is valid and the new passwords match.
+            # Save the new passwords to the database.
+            form.save()
+
+            # Return to the settings page
+            return redirect('user:settings', username=username)
+
+        else:
+            pass
+
+    else:
+        # Create a form populated with data from the instance user
+        form = UserPasswordChangeForm(user=request.user)
+
+    # Create template context object
+    context = {'form': form}
+
+    return render(request, 'tethys_site/user/change_password.html', context)
