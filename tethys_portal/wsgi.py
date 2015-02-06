af5eae05af5c1cd496a29df9d686605314894f38
Nathan Swain <nathan.swain86@gmail.com>
2015-02-05 23:51:37 -0700

    Version 1.0.0

--- tethys_portal/wsgi.py
+++ tethys_portal/wsgi.py
@@ -8,7 +8,9 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tethys_portal.settings")
+import sys
+sys.path.append('/usr/lib/tethys/src')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tethys_apps.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
