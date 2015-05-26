bf3575abfdd53c4e62329e44e6c853fd1793485a
Nathan Swain <nathan.swain86@gmail.com>
2015-05-25 20:40:55 -0600

    tethys #78 Broken CLI command: tethys manage collectstatic: closed.

--- tethys_apps/cli/__init__.py
+++ tethys_apps/cli/__init__.py
@@ -8,47 +8,13 @@ from django.conf import settings
 
 from tethys_apps.terminal_colors import TerminalColors
 from .docker_commands import *
-from .manage_commands import pre_collectstatic
+from .manage_commands import manage_command, get_manage_path, MANAGE_START, MANAGE_SYNCDB, MANAGE_COLLECTSTATIC
 from .gen_commands import GEN_SETTINGS_OPTION, GEN_APACHE_OPTION, generate_command
 from tethys_apps.helpers import get_installed_tethys_apps
 
 # Module level variables
 VALID_GEN_OBJECTS = (GEN_SETTINGS_OPTION, GEN_APACHE_OPTION)
-DEFAULT_INSTALLATION_DIRECTORY = '/usr/lib/tethys/src'
-DEVELOPMENT_DIRECTORY = '/usr/lib/tethys/tethys'
 PREFIX = 'tethysapp'
-MANAGE_START = 'start'
-MANAGE_SYNCDB = 'syncdb'
-MANAGE_COLLECTSTATIC = 'collectstatic'
-
-
-def get_manage_path(args):
-    """
-    Validate user defined manage path, use default, or throw error
-    """
-    # Determine path to manage.py file
-    manage_path = os.path.join(DEFAULT_INSTALLATION_DIRECTORY, 'manage.py')
-
-    # Check for path option
-    if args.manage:
-        manage_path = args.manage
-
-        # Throw error if path is not valid
-        if not os.path.isfile(manage_path):
-            print('ERROR: Can\'t open file "{0}", no such file.'.format(manage_path))
-            exit(1)
-
-    elif not os.path.isfile(manage_path):
-        # Try the development path version
-        manage_path = os.path.join(DEVELOPMENT_DIRECTORY, 'manage.py')
-
-        # Throw error if default path is not valid
-        if not os.path.isfile(manage_path):
-            print('ERROR: Cannot find the "manage.py" file at the default location. Try using the "--manage"'
-                  'option to provide the path to the location of the "manage.py" file.')
-            exit(1)
-
-    return manage_path
 
 
 def scaffold_command(args):
@@ -80,42 +46,6 @@ def scaffold_command(args):
 
     process = ['paster', 'create', '-t', 'tethys_app_scaffold', project_name]
     subprocess.call(process)
-
-
-def manage_command(args):
-    """
-    Management commands.
-    """
-    # Get the path to manage.py
-    manage_path = get_manage_path(args)
-
-    # Define the process to be run
-    process = None
-
-    if args.command == MANAGE_START:
-        if args.port:
-            process = ['python', manage_path, 'runserver', str(args.port)]
-        else:
-            process = ['python', manage_path, 'runserver']
-    elif args.command == MANAGE_SYNCDB:
-        process = ['python', manage_path, 'syncdb']
-
-    elif args.command == MANAGE_COLLECTSTATIC:
-        if settings.STATIC_ROOT:
-            installed_apps = get_installed_tethys_apps()
-            pre_collectstatic(installed_apps, settings.STATIC_ROOT)
-            process = ['python', manage_path, 'collectstatic']
-        else:
-            print('WARNING: Cannot find the STATIC_ROOT setting in the settings.py file. '
-                  'Please provide the path to the static directory using the STATIC_ROOT setting and try again.')
-            exit(1)
-
-    # Call the process with a little trick to ignore the keyboard interrupt error when it happens
-    if process:
-        try:
-            subprocess.call(process)
-        except KeyboardInterrupt:
-            pass
 
 
 def uninstall_command(args):
--- tethys_apps/cli/manage_commands.py
+++ tethys_apps/cli/manage_commands.py
@@ -1,8 +1,85 @@
 import os
 import shutil
+import subprocess
+
+# from django.conf import settings
 
 # Setup Django settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tethys_apps.settings")
+
+from tethys_apps.helpers import get_installed_tethys_apps
+from tethys_apps import settings
+
+DEFAULT_INSTALLATION_DIRECTORY = '/usr/lib/tethys/src'
+DEVELOPMENT_DIRECTORY = '/usr/lib/tethys/tethys'
+MANAGE_START = 'start'
+MANAGE_SYNCDB = 'syncdb'
+MANAGE_COLLECTSTATIC = 'collectstatic'
+
+
+def get_manage_path(args):
+    """
+    Validate user defined manage path, use default, or throw error
+    """
+    # Determine path to manage.py file
+    manage_path = os.path.join(DEFAULT_INSTALLATION_DIRECTORY, 'manage.py')
+
+    # Check for path option
+    if args.manage:
+        manage_path = args.manage
+
+        # Throw error if path is not valid
+        if not os.path.isfile(manage_path):
+            print('ERROR: Can\'t open file "{0}", no such file.'.format(manage_path))
+            exit(1)
+
+    elif not os.path.isfile(manage_path):
+        # Try the development path version
+        manage_path = os.path.join(DEVELOPMENT_DIRECTORY, 'manage.py')
+
+        # Throw error if default path is not valid
+        if not os.path.isfile(manage_path):
+            print('ERROR: Cannot find the "manage.py" file at the default location. Try using the "--manage"'
+                  'option to provide the path to the location of the "manage.py" file.')
+            exit(1)
+
+    return manage_path
+
+
+def manage_command(args):
+    """
+    Management commands.
+    """
+    # Get the path to manage.py
+    manage_path = get_manage_path(args)
+
+    # Define the process to be run
+    process = None
+
+    if args.command == MANAGE_START:
+        if args.port:
+            process = ['python', manage_path, 'runserver', str(args.port)]
+        else:
+            process = ['python', manage_path, 'runserver']
+    elif args.command == MANAGE_SYNCDB:
+        process = ['python', manage_path, 'syncdb']
+
+    elif args.command == MANAGE_COLLECTSTATIC:
+        if settings.STATIC_ROOT:
+            installed_apps = get_installed_tethys_apps()
+            pre_collectstatic(installed_apps, settings.STATIC_ROOT)
+            process = ['python', manage_path, 'collectstatic']
+        else:
+            print('WARNING: Cannot find the STATIC_ROOT setting in the settings.py file. '
+                  'Please provide the path to the static directory using the STATIC_ROOT setting and try again.')
+            exit(1)
+
+    # Call the process with a little trick to ignore the keyboard interrupt error when it happens
+    if process:
+        try:
+            subprocess.call(process)
+        except KeyboardInterrupt:
+            pass
 
 
 def pre_collectstatic(installed_apps, static_root):
@@ -21,10 +98,15 @@ def pre_collectstatic(installed_apps, static_root):
 
         # Clear out old symbolic links/directories if necessary
         try:
+            # Remove link
             os.remove(static_root_path)
         except OSError:
-            # Remove directory
-            shutil.rmtree(static_root_path)
+            try:
+                # Remove directory
+                shutil.rmtree(static_root_path)
+            except OSError:
+                # No file
+                pass
 
         # Create appropriate symbolic link
         if os.path.isdir(public_path):
