4036a7d5efdf63b477a7aabd520ffeb8bf9553d3
Nathan Swain <nathan.swain86@gmail.com>
2015-05-25 21:36:17 -0600

    tethys #27 Improve Production Installation Instructions: Better fix.

--- tethys_apps/cli/manage_commands.py
+++ tethys_apps/cli/manage_commands.py
@@ -2,13 +2,7 @@ import os
 import shutil
 import subprocess
 
-# from django.conf import settings
-
-# Setup Django settings
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tethys_apps.settings")
-
 from tethys_apps.helpers import get_installed_tethys_apps
-from tethys_apps import settings
 
 DEFAULT_INSTALLATION_DIRECTORY = '/usr/lib/tethys/src'
 DEVELOPMENT_DIRECTORY = '/usr/lib/tethys/tethys'
@@ -65,14 +59,15 @@ def manage_command(args):
         process = ['python', manage_path, 'syncdb']
 
     elif args.command == MANAGE_COLLECTSTATIC:
-        if settings.STATIC_ROOT:
-            installed_apps = get_installed_tethys_apps()
-            pre_collectstatic(installed_apps, settings.STATIC_ROOT)
-            process = ['python', manage_path, 'collectstatic']
-        else:
-            print('WARNING: Cannot find the STATIC_ROOT setting in the settings.py file. '
-                  'Please provide the path to the static directory using the STATIC_ROOT setting and try again.')
-            exit(1)
+        # Run pre_collectstatic
+        process = ['python', manage_path, 'pre_collectstatic']
+        try:
+            subprocess.call(process)
+        except KeyboardInterrupt:
+            pass
+
+        # Setup for main collectstatic
+        process = ['python', manage_path, 'collectstatic']
 
     # Call the process with a little trick to ignore the keyboard interrupt error when it happens
     if process:
@@ -80,39 +75,3 @@ def manage_command(args):
             subprocess.call(process)
         except KeyboardInterrupt:
             pass
-
-
-def pre_collectstatic(installed_apps, static_root):
-    """
-    Symbolically link the static directories of each app into the static/public directory specified by the STATIC_ROOT
-    parameter of the settings.py. Do this prior to running Django's collectstatic method.
-    """
-    # Provide feedback to user
-    print('INFO: Linking static and public directories of apps to "{0}".'.format(static_root))
-
-    for app, path in installed_apps.iteritems():
-        # Check for both variants of the static directory (public and static)
-        public_path = os.path.join(path, 'public')
-        static_path = os.path.join(path, 'static')
-        static_root_path = os.path.join(static_root, app)
-
-        # Clear out old symbolic links/directories if necessary
-        try:
-            # Remove link
-            os.remove(static_root_path)
-        except OSError:
-            try:
-                # Remove directory
-                shutil.rmtree(static_root_path)
-            except OSError:
-                # No file
-                pass
-
-        # Create appropriate symbolic link
-        if os.path.isdir(public_path):
-            os.symlink(public_path, static_root_path)
-            print('INFO: Successfully linked public directory to STATIC_ROOT for app "{0}".'.format(app))
-
-        elif os.path.isdir(static_path):
-            os.symlink(static_path, static_root_path)
-            print('INFO: Successfully linked static directory to STATIC_ROOT for app "{0}".'.format(app))
