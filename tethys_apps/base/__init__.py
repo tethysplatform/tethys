ec34e1995e06130100dc2dc10849c9f0037cd3d7
Nathan Swain <nathan.swain86@gmail.com>
2015-08-05 17:06:47 -0600

    tethys #30 Formalize Workspaces for Apps: Implemented get_user_workspace() and get_app_workspace() as class methods on the app base class. To use, developers import the class and call the methods on the class.

--- tethys_apps/base/__init__.py
+++ tethys_apps/base/__init__.py
@@ -3,3 +3,4 @@ from tethys_apps.base.persistent_store import PersistentStore
 from tethys_apps.base.controller import app_controller_maker
 from tethys_services.base import DatasetService, SpatialDatasetService, WpsService
 from tethys_apps.base.url_map import url_map_maker
+from tethys_apps.base.workspace import TethysWorkspace
