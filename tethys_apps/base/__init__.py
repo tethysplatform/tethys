6d3cf7639e525bf2e9ab7d2077b7e77ebaa23419
Nathan Swain <nathan.swain86@gmail.com>
2015-08-06 18:08:42 -0600

    tethys-119: Headers added to all tethys_apps

--- tethys_apps/base/__init__.py
+++ tethys_apps/base/__init__.py
@@ -1,3 +1,13 @@
+"""
+********************************************************************************
+* Name: base/__init__.py
+* Author: Nathan Swain
+* Created On: August 2013
+* Copyright: (c) Brigham Young University 2013
+* License: BSD 2-Clause
+********************************************************************************
+"""
+
 from tethys_apps.base.app_base import TethysAppBase
 from tethys_apps.base.persistent_store import PersistentStore
 from tethys_apps.base.controller import app_controller_maker
