bcfe85a2968baa4c49dc6ab6a5cff72fa75b39ea
Nathan Swain <nathan.swain86@gmail.com>
2015-08-11 17:26:31 -0600

    tethys #59 App Interaction: A skeleton of the handoff feature is in place.

--- tethys_apps/base/handoff.py
+++ tethys_apps/base/handoff.py
@@ -1,6 +1,6 @@
 """
 ********************************************************************************
-* Name: persistent store
+* Name: handoff.py
 * Author: Nathan Swain
 * Created On: August 11, 2015
 * Copyright: (c) Brigham Young University 2015
@@ -8,13 +8,25 @@
 ********************************************************************************
 """
 
+
 class HandoffHandler(object):
     """
-    An object that is used to reagister a Handoff handler functions.
+    An object that is used to register a Handoff handler functions.
 
     Attributes:
       name(str): Name of the handoff handler.
       handler(str): Path to the handler function for the handoff interaction. Use dot-notation with a colon delineating the function (e.g.: "foo.bar:function").
     """
 
-    def __init__(self):
+    def __init__(self, name, handler):
+        """
+        Constructor
+        """
+        self.name = name
+        self.handler = handler
+
+    def __repr__(self):
+        """
+        String representation
+        """
+        return '<Handoff Handler: name={0}, handler={1}>'.format(self.name, self.handler)
