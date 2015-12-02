dcf644db8ef8dd417b00a5a4b3361a4417cfe61f
ezra <ezra.j.rice@gmail.com>
2015-12-02 15:21:53 -0700

    #114 Attr with Spaces

--- tethys_gizmos/gizmo_options/base.py
+++ tethys_gizmos/gizmo_options/base.py
@@ -7,14 +7,15 @@
 * License: BSD 2-Clause
 ********************************************************************************
 """
-
+import re
+import shlex
 
 class TethysGizmoOptions(dict):
     """
     Base class for Tethys Gizmo Options objects.
     """
 
-    def __init__(self, attributes='', classes=''):
+    def __init__(self, attributes={}, classes=''):
         """
         Constructor for Tethys Gizmo Options base.
         """
@@ -24,6 +25,16 @@ class TethysGizmoOptions(dict):
         # Dictionary magic
         self.__dict__ = self
 
+        if isinstance(attributes, basestring):
+            # 'key="value" key2="value with spaces"'
+            pattern = '(\w+)='
+            pairs = re.split(pattern, attributes)
+            if pairs:
+                pairs = [x.strip().strip('\'').strip('\"') for x in pairs]
+                attributes = dict()
+                for i in range(1, len(pairs), 2):
+                    attributes[pairs[i]] = pairs[i+1]
+                    print attributes
         self.attributes = attributes
         self.classes = classes
 
