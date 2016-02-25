a165ff51f05eb1a296d73c9b574cf403de1a1381
sdc50 <sdc50@byu.net>
2016-02-25 12:19:06 -0700

    tethys-177 Error catching on URL loading is masking true errors

--- tethys_apps/__init__.py
+++ tethys_apps/__init__.py
@@ -7,3 +7,10 @@
 * License: BSD 2-Clause
 ********************************************************************************
 """
+import logging
+
+tethys_log = logging.getLogger('tethys')
+default_log_format = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
+default_log_handler = logging.StreamHandler()
+default_log_handler.setFormatter(default_log_format)
+tethys_log.addHandler(default_log_handler)
