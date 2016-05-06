1d4a61a066785ebe43e2bd2b0346e697212bb8d1
swainn <nswain@aquaveo.com>
2016-05-06 15:59:16 -0600

    First pass at database model for apps. Experiments with object level permissions.

--- tethys_apps/admin.py
+++ tethys_apps/admin.py
@@ -8,5 +8,18 @@
 ********************************************************************************
 """
 from django.contrib import admin
+from guardian.admin import GuardedModelAdmin
+from tethys_apps.models import TethysApp
 
-# Register your models here.
+
+class TethysAppAdmin(GuardedModelAdmin):
+    readonly_fields = ('package',)
+    fields = ('package', 'name', 'description', 'enabled', 'show_in_apps_library', 'enable_feedback', 'feedback_emails')
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+admin.site.register(TethysApp, TethysAppAdmin)
