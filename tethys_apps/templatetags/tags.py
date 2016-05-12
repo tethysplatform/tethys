e27f6ef3b9bcf8210ab2a7de6d57b48c59e53efe
SarvaPulla <sarvapulla@gmail.com>
2016-05-11 18:02:03 -0600

    The tags can now be separated by commas and you can use multiple words for tags.

--- tethys_apps/templatetags/tags.py
+++ tethys_apps/templatetags/tags.py
@@ -8,8 +8,27 @@ def get_tags_from_apps(apps):
     tags_list = []
     for app in apps:
         tags = app.tags
-        tags = filter(None, re.split("[, \-!?:]+",tags))
+        tags = filter(None, re.split("[,]+",tags))
         for tag in tags:
+            tag = re.sub(r"\s+", '', tag)
             tags_list.append(tag)
             tags_list = list(set(tags_list))
+    return tags_list
+
+@register.filter
+def get_class(app):
+    get_tags = app.tags
+    get_tags = filter(None, re.split("[,]+", get_tags))
+    tags = []
+    for tag in get_tags:
+        tag = re.sub(r"\s+",'',tag)
+        tags.append(tag)
+
+    tags_list = ' '.join(tags)
+    # for tag in tags:
+    #         tags_list.append(tag)
+        # tags = filter(None, re.split("[,]+",tags))
+        # app_tags = []
+        # for tag in tags:
+        #     app_tags.append(tag)
     return tags_list
