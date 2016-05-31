23157ac9ee4144e09d252488d45e0f4f29e41318
SarvaPulla <sarvapulla@gmail.com>
2016-05-31 16:13:33 -0600

    Made changes as suggested byt the development team. The tags only show up after 6 apps have been installed in the portal.

--- tethys_apps/templatetags/tags.py
+++ tethys_apps/templatetags/tags.py
@@ -2,21 +2,26 @@ from django import template
 import re
 
 register = template.Library()
-#Generating a list of all available tags. This list then used to generate the filters/buttons in the app library page.
+
 @register.filter
 def get_tags_from_apps(apps):
     tags_list = []
+    apps_list =[]
     for app in apps:
-        tags = app.tags
-        tags = filter(None, re.split("[,]+",tags))
-        for tag in tags:
-            tag = re.sub(r"\s+", '', tag)
-            tags_list.append(tag)
-            tags_list = list(set(tags_list))
-    return tags_list
+        apps_list.append(app)
+
+    if len(apps_list) > 5:
+        for app in apps:
+            tags = app.tags
+            tags = filter(None, re.split("[,]+",tags))
+            for tag in tags:
+                tag = re.sub(r"\s+", '', tag)
+                tags_list.append(tag)
+                tags_list = list(set(tags_list))
+        return tags_list
 
 @register.filter
-def get_class(app):
+def get_tag_class(app):
     get_tags = app.tags
     get_tags = filter(None, re.split("[,]+", get_tags))
     tags = []
@@ -25,10 +30,5 @@ def get_class(app):
         tags.append(tag)
 
     tags_list = ' '.join(tags)
-    # for tag in tags:
-    #         tags_list.append(tag)
-        # tags = filter(None, re.split("[,]+",tags))
-        # app_tags = []
-        # for tag in tags:
-        #     app_tags.append(tag)
+
     return tags_list
