ba949c87dda620a1d57f52c415d029d8ecf4e583
snowman2 <alan.d.snow@usace.army.mil>
2016-11-10 08:54:31 -0600

    added gizmo dependency pre-loading, added gizmo global js/css, and bokeh & plotly plot gizmos

--- tethys_gizmos/gizmo_options/bokeh_view.py
+++ tethys_gizmos/gizmo_options/bokeh_view.py
@@ -1,19 +1,19 @@
 # coding=utf-8
-import plotly.offline as opy
+from bokeh.embed import components
 
 from .base import TethysGizmoOptions
 
-__all__ = ['PlotlyView']
+__all__ = ['BokehView']
 
 
-class PlotlyView(TethysGizmoOptions):
+class BokehView(TethysGizmoOptions):
     """
     Simple options object for plotly view.
     
     .. info:: See https://plot.ly/python for Plotly API
 
     Attributes:
-        plot_input(plotly graph_objs): A plotly graph_objs to be plotted.
+        plot_input(bokeh figure): A bokeh figure to be plotted.
         height(Optional[str]): Height of the plot element. Any valid css unit of length.
         width(Optional[str]): Width of the plot element. Any valid css unit of length.
         attributes(Optional[dict]): Dictionary of attributed to add to the outer div.
@@ -24,41 +24,27 @@ class PlotlyView(TethysGizmoOptions):
                                  An example case of setting to False is using AJAX to add another chart.
                                  Default is True.
                                  
-    Controller Code Example 1::
+    Controller Code Example::
     
-        import datetime as datetime
-        import plotly.graph_objs as go
-        from tethys_sdk.gizmos import PlotlyView
+        from tethys_sdk.gizmos import BokehView
+        from bokeh.plotting import figure
+        
+        plot = figure()
+        plot.circle([1,2], [3,4])
 
-        x = [datetime(year=2013, month=10, day=04),
-             datetime(year=2013, month=11, day=05),
-             datetime(year=2013, month=12, day=06)]
-    
-        my_plotly_view = PlotlyView([go.Scatter(x=x, y=[1, 3, 6])])
-        
-        context = {'plotly_view_input': my_plotly_view}
-        
-    Controller Code Example 2::
-    
-        import numpy as np
-        import pandas as pd
-        from tethys_sdk.gizmos import PlotlyView
-        
-        df = pd.DataFrame(np.random.randn(1000, 2), columns=['A', 'B']).cumsum()
+        my_bokeh_view = BokehView(plot)
 
-        my_plotly_view = PlotlyView(df.iplot(asFigure=True))
-
-        context = {'plotly_view_input': my_plotly_view}
+        context = {'bokeh_view_input': my_bokeh_view}
         
-    Template Code::
+    Template Code Example::
     
         {% load tethys_gizmos %}
         
         {% block register_gizmos %}
-          {% register_gizmo_dependency plotly_view %}
+          {% register_gizmo_dependency bokeh_view %}
         {% endblock %}
     
-        {% gizmo plotly_view plotly_view_input %}
+        {% gizmo bokeh_view bokeh_view_input %}
     """
     def __init__(self, plot_input, height='520px', width='100%', 
                  attributes='', classes='', divid='', hidden=False,
@@ -67,13 +53,8 @@ class PlotlyView(TethysGizmoOptions):
         Constructor
         """
         # Initialize the super class
-        super(PlotlyView, self).__init__()
-        
-        self.plotly_div = opy.plot(plot_input, 
-                                   auto_open=False, 
-                                   output_type='div',
-                                   include_plotlyjs=False,
-                                   show_link=show_link)
+        super(BokehView, self).__init__()
+        self.script, self.div = components(plot_input)
         self.height = height
         self.width = width
         self.attributes = attributes
