b793712ca71af9e3ad5b714f96958eaa6846629e
Shawn Crawley <scrawley@aquaveo.com>
2016-11-21 16:27:36 -0700

    modified import (cherry picked from commit c2e4878)

--- tethys_sdk/testing.py
+++ tethys_sdk/testing.py
@@ -8,4 +8,4 @@
 ********************************************************************************
 """
 # DO NOT ERASE
-from tethys_apps.base.testing import *
+from tethys_apps.base.testing import TethysTestCase
