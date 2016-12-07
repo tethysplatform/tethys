e0b14730bcfe56311b09a1596c7c1e11220a5041
zhiyuli <zyli2004@gmail.com>
2016-12-06 18:08:14 -0700

    fix bugs in hydroshare-beta

--- tethys_services/backends/hydroshare_beta.py
+++ tethys_services/backends/hydroshare_beta.py
@@ -14,3 +14,8 @@ class HydroShareBetaOAuth2(HydroShareOAuth2):
     # override necessary settings
     auth_server_hostname = "beta.hydroshare.org"
     name = 'hydroshare_beta'
+    http_scheme = "https"
+
+    auth_server_full_url = "{0}://{1}".format(http_scheme, auth_server_hostname)
+    AUTHORIZATION_URL = '{0}/o/authorize/'.format(auth_server_full_url)
+    ACCESS_TOKEN_URL = '{0}/o/token/'.format(auth_server_full_url)
