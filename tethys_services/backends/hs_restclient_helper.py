7dec54561d2c250409b2395bb1c24464a9e4d96f
zhiyuli <zyli2004@gmail.com>
2016-12-08 22:35:29 -0700

    handle refresh_token

--- tethys_services/backends/hs_restclient_helper.py
+++ tethys_services/backends/hs_restclient_helper.py
@@ -1,6 +1,8 @@
 import logging
+import time
 import hs_restclient as hs_r
 from django.conf import settings
+from social.apps.django_app.utils import load_strategy
 
 logger = logging.getLogger(__name__)
 
@@ -22,6 +24,9 @@ def get_oauth_hs(request):
                 client_secret = getattr(settings, "SOCIAL_AUTH_{0}_SECRET".format(backend_name.upper()), 'None')
 
                 if hs is None:
+
+                    refresh_user_token(social_auth_obj)
+
                     auth = hs_r.HydroShareAuthOAuth2(client_id, client_secret, token=social_auth_obj.extra_data)
                     hs = hs_r.HydroShare(auth=auth, hostname=auth_server_hostname)
                     logger.debug("hs object initialized: {0} @ {1}".format(user_id, auth_server_hostname))
@@ -43,3 +48,53 @@ class HSClientInitException(Exception):
         self.value = value
     def __str__(self):
         return repr(self.value)
+
+
+def _send_refresh_request(user_social):
+    """
+    Private function that refresh an user access token
+    """
+    logger.debug("------------------refresh token-----------------")
+    logger.debug("------------------old token---------------------")
+    logger.debug(user_social.extra_data)
+
+    strategy = load_strategy()
+    user_social.refresh_token(strategy)
+
+    # update token_dict for back compatible
+    data = user_social.extra_data
+    token_dict = {
+       'access_token': data['access_token'],
+       'token_type': data['token_type'],
+       'expires_in': data['expires_in'],
+       'expires_at': data['expires_at'],
+       'refresh_token': data['refresh_token'],
+       'scope': data['scope']
+       }
+    data["token_dict"] = token_dict
+    user_social.set_extra_data(extra_data=data)
+    user_social.save()
+
+    logger.debug("------------------new token-----------------")
+    logger.debug(user_social.extra_data)
+
+
+def refresh_user_token(user_social):
+    """
+    Utility function to refresh the access token if is (almost) expired
+    Args:
+        user_social (UserSocialAuth): a user social auth instance
+    """
+    try:
+        try:
+            expires_at = user_social.extra_data.get('expires_at')
+        except Exception:
+            _send_refresh_request(user_social)
+            return
+
+        current_time = int(time.time())
+        if current_time >= expires_at:
+            _send_refresh_request(user_social)
+    except Exception as ex:
+        logger.error("Failed to refresh token: " + ex.message)
+        raise ex
--- tethys_services/backends/hydroshare.py
+++ tethys_services/backends/hydroshare.py
@@ -7,6 +7,8 @@
 * License: BSD 2-Clause
 ********************************************************************************
 """
+from datetime import datetime
+import time
 from social.backends.oauth import BaseOAuth2
 
 class HydroShareOAuth2(BaseOAuth2):
@@ -33,25 +35,49 @@ class HydroShareOAuth2(BaseOAuth2):
         ('token_type', 'token_type'),
         ('refresh_token', 'refresh_token'),
         ('scope', 'scope'),
-        ('token_dict', 'token_dict'),
     ]
+
+    # small margin in expires_at to be safe
+    margin_in_seconds = 300
+
+    # testing purpose
+    #set_expires_in_to = margin_in_seconds + 20
+    set_expires_in_to = None
 
     def extra_data(self, user, uid, response, details=None, *args, **kwargs):
         data = super(HydroShareOAuth2, self).extra_data(user, uid, response,
                                                         details,
                                                         *args, **kwargs)
 
-        # Reconstitute token dictionary for client convenience
+        # testing purpose
+        if self.set_expires_in_to is not None:
+            data['expires_in'] = self.set_expires_in_to
 
+        # current server local Epoch time (in sec)
+        current_time = int(time.time())
+        current_time_str = datetime.fromtimestamp(current_time).strftime('%Y-%m-%d %H:%M:%S')
+        data["updated_at"] = current_time_str
+
+        # expire at current Epoch time (in sec) + expire_in - margin_in_seconds
+        expires_at = current_time + data['expires_in'] - self.margin_in_seconds
+        data['expires_at'] = expires_at
+        expires_at_str = datetime.fromtimestamp(expires_at).strftime('%Y-%m-%d %H:%M:%S')
+        data["expires_at_str"] = expires_at_str
+
+
+        # Reconstitute token dictionary for client convenience
         # backward compatible
         token_dict = {
             'access_token': data['access_token'],
             'token_type': data['token_type'],
             'expires_in': data['expires_in'],
+            'expires_at': data['expires_at'],
             'refresh_token': data['refresh_token'],
             'scope': data['scope']
         }
         data['token_dict'] = token_dict
+
+
 
         return data
 
@@ -72,3 +98,27 @@ class HydroShareOAuth2(BaseOAuth2):
             return self.get_json(url, params={'access_token': access_token})
         except ValueError:
             return None
+
+    def refresh_token(self, token, *args, **kwargs):
+        """
+        Overridden method to add extra info during refresh token.
+        Args:
+            token (str): valid refresh token
+        """
+
+        response = super(HydroShareOAuth2, self).refresh_token(token, *args, **kwargs)
+
+        # testing purpose
+        if self.set_expires_in_to is not None:
+            response['expires_in'] = self.set_expires_in_to
+
+        current_time = int(time.time())
+        current_time_str = datetime.fromtimestamp(current_time).strftime('%Y-%m-%d %H:%M:%S')
+        response["updated_at"] = current_time_str
+
+        expires_at = current_time + response['expires_in'] - self.margin_in_seconds
+        response['expires_at'] = expires_at
+        expires_at_str = datetime.fromtimestamp(expires_at).strftime('%Y-%m-%d %H:%M:%S')
+        response["expires_at_str"] = expires_at_str
+
+        return response
