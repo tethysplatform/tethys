13a744dd70b25a7f15ae8254388de74667c1f207
swainn <nathan.swain86@gmail.com>
2016-12-10 15:36:27 -0600

    Created initial structure of tethys_layouts module.

---
+++ tethys_layouts/urls/__init__.py
@@ -0,0 +1,9 @@
+"""
+********************************************************************************
+* Name: tethys_layouts/urls/__init__.py
+* Author: Nathan Swain
+* Created On: December 10, 2016
+* Copyright: (c) Tethys Platform 2016
+* License: BSD 2-Clause
+********************************************************************************
+"""
