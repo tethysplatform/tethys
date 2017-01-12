131a95e7c116b79df4624b177a53867e453b5cee
Alan D. Snow <alan.d.snow@usace.army.mil>
2017-01-12 15:27:01 -0600

    security updates; make password requirements more secure; hide admin login page if not logged in with permissions

--- tethys_portal/urls.py
+++ tethys_portal/urls.py
@@ -13,10 +13,13 @@ from django.contrib.auth.views import password_reset, password_reset_done, passw
     password_reset_complete
 admin.autodiscover()
 
+# ensure at least staff users logged in before accessing admin login page
+from django.contrib.admin.views.decorators import staff_member_required
+admin.site.login = staff_member_required(admin.site.login, redirect_field_name="", login_url='/accounts/login/')
+
 from tethys_portal.views import accounts as tethys_portal_accounts, developer as tethys_portal_developer, \
     error as tethys_portal_error, home as tethys_portal_home, user as tethys_portal_user
 from tethys_apps import views as tethys_apps_views
-
 
 account_urls = [
     url(r'^login/$', tethys_portal_accounts.login_view, name='login'),
