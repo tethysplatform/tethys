fa559707b6a73355ea4cf5d04c554eb12ece676d
swainn <nathan.swain86@gmail.com>
2017-03-29 21:15:58 -0500

    A few more updates on the persistent store service setting.

---
+++ tethys_apps/exceptions.py
@@ -0,0 +1,13 @@
+"""
+********************************************************************************
+* Name: exceptions.py
+* Author: Nathan Swain
+* Created On: March 25, 2017
+* Copyright:
+* License: BSD 2-Clause
+********************************************************************************
+"""
+
+
+class TethysAppSettingDoesNotExist(Exception):
+    pass
