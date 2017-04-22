9b643980d6a828c2553ca5f21b15e8f9acf61247
swainn <nathan.swain86@gmail.com>
2017-04-22 00:00:05 -0500

    Added types to the CustomSetting with validation and a custom getter that will caste the value to the appropriate type. Properly enforcing required field. Removed old persistent store stuff and moved the TethysFunctionExtractor to it's own module.

--- tethys_apps/admin.py
+++ tethys_apps/admin.py
@@ -9,9 +9,8 @@
 """
 from django.contrib import admin
 from guardian.admin import GuardedModelAdmin
-# from tethys_services.models import DatasetService
 from tethys_apps.models import (TethysApp,
-                                CustomTethysAppSetting,
+                                CustomSetting,
                                 DatasetServiceSetting,
                                 SpatialDatasetServiceSetting,
                                 WebProcessingServiceSetting,
@@ -29,25 +28,16 @@ class TethysAppSettingInline(admin.TabularInline):
         return False
 
 
-class CustomTethysAppSettingInline(TethysAppSettingInline):
-    readonly_fields = ('name', 'description', 'required')
-    fields = ('name', 'description', 'value', 'required')
-    model = CustomTethysAppSetting
+class CustomSettingInline(TethysAppSettingInline):
+    readonly_fields = ('name', 'description', 'type', 'required')
+    fields = ('name', 'description', 'type', 'value', 'required')
+    model = CustomSetting
 
 
 class DatasetServiceSettingInline(TethysAppSettingInline):
     readonly_fields = ('name', 'description', 'required', 'engine')
     fields = ('name', 'description', 'dataset_service', 'engine', 'required')
     model = DatasetServiceSetting
-
-#    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-#        """
-#        Filter by dataset service engine
-#        """
-#        if db_field.name == "dataset_service":
-#            kwargs['queryset'] = DatasetService.objects.filter(engine=DatasetService.CKAN)
-#        return super(DatasetServiceSettingInline, self) \
-#            .formfield_for_foreignkey(db_field, request, **kwargs)
 
 
 class SpatialDatasetServiceSettingInline(TethysAppSettingInline):
@@ -60,6 +50,7 @@ class WebProcessingServiceSettingInline(TethysAppSettingInline):
     readonly_fields = ('name', 'description', 'required')
     fields = ('name', 'description', 'web_processing_service', 'required')
     model = WebProcessingServiceSetting
+
 
 #TODO: Figure out how to initialize persistent stores with button in admin
 # Consider: https://medium.com/@hakibenita/how-to-add-custom-action-buttons-to-django-admin-8d266f5b0d41
@@ -78,7 +69,7 @@ class PersistentStoreDatabaseSettingInline(TethysAppSettingInline):
 class TethysAppAdmin(GuardedModelAdmin):
     readonly_fields = ('package',)
     fields = ('package', 'name', 'description', 'tags', 'enabled', 'show_in_apps_library', 'enable_feedback')
-    inlines = [CustomTethysAppSettingInline,
+    inlines = [CustomSettingInline,
                DatasetServiceSettingInline,
                SpatialDatasetServiceSettingInline,
                WebProcessingServiceSettingInline,
