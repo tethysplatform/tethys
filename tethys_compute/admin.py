2cbd80fc5cf1bc3639d660e72424f36cfa8e3380
rditlsc9 <scott.d.christensen@erdc.dren.mil>
2017-06-17 09:30:51 -0500

    Squash migrations

--- tethys_compute/admin.py
+++ tethys_compute/admin.py
@@ -10,38 +10,8 @@
 from django.contrib import admin
 from django.forms import Textarea
 from django.db import models
-from tethys_compute.models import SettingsCategory, Setting, Scheduler, TethysJob
+from tethys_compute.models import Scheduler, TethysJob
 # Register your models here.
-
-
-class SettingInline(admin.TabularInline):
-    fields = ('name', 'content', 'date_modified')
-    readonly_fields = ('name', 'date_modified')
-    model = Setting
-    extra = 0
-
-    formfield_overrides = {
-        models.TextField: {'widget': Textarea(attrs={'rows': 2, 'cols': 65})},
-    }
-
-    def has_delete_permission(self, request, obj=None):
-        return False
-
-    def has_add_permission(self, request):
-        return False
-
-
-@admin.register(SettingsCategory)
-class SettingCategoryAdmin(admin.ModelAdmin):
-    fields = ('name',)
-    readonly_fields = ('name',)
-    inlines = [SettingInline]
-
-    def has_delete_permission(self, request, obj=None):
-        return False
-
-    def has_add_permission(self, request):
-        return False
 
 
 @admin.register(Scheduler)
