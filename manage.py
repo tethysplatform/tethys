ffadd4f648db702ab2d56bc71d888998a285ff4a
swainn <nathan.swain86@gmail.com>
2017-06-20 01:05:00 -0500

    Validated advanced solution.

--- manage.py
+++ manage.py
@@ -13,7 +13,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tethys_apps.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tethys_portal.settings")
 
     from django.core.management import execute_from_command_line
 
