07b85cf156fe243e16e847202bf210eaad2a7979
Shawn Crawley <scrawley@aquaveo.com>
2017-07-20 16:01:21 -0600

    fixed issues with testing framework

--- tethys_apps/base/testing/environment.py
+++ tethys_apps/base/testing/environment.py
@@ -1,5 +1,7 @@
 from os import environ, unsetenv
 
+TESTING_DB_FLAG = 'tethys-testing_'
+
 
 def is_testing_environment():
     return environ.get('TETHYS_TESTING_IN_PROGRESS')
@@ -12,3 +14,12 @@ def set_testing_environment(val):
         environ['TETHYS_TESTING_IN_PROGRESS'] = ''
         del environ['TETHYS_TESTING_IN_PROGRESS']
         unsetenv('TETHYS_TESTING_IN_PROGRESS')
+
+
+def get_test_db_name(orig_name):
+    if TESTING_DB_FLAG not in orig_name:
+        test_db_name = '{0}{1}'.format(TESTING_DB_FLAG, orig_name)
+    else:
+        test_db_name = orig_name
+
+    return test_db_name
