51f7c2b4ba68f38afc3425784499a8bf43f8eb31
Alan D. Snow <alansnow21@gmail.com>
2017-09-20 14:08:40 -0500

    added exception handling middleware to raise exceptions in apps

---
+++ tethys_exception/exception_handler.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""exception_handler.py
+
+    This is for handling ajax exceptions.
+
+    Author: Alan D. Snow
+    Created: September 20, 2017
+    License: BSD 3-Clause
+"""
+from django.http import HttpResponseBadRequest
+
+
+class TethysError(Exception):
+    """This is an exception for database errors."""
+    __prepend__ = "Tethys"
+    
+    @property
+    def ajax_error_message(self):
+        return "{prepend} error: {exception}" \
+               .format(prepend=self.__prepend__,
+                       exception=self)
+    pass
+
+
+class DatabaseError(TethysError):
+    """This is an exception for database errors."""
+    __prepend__ = "Database"
+    pass
+
+
+class GeoServerError(TethysError):
+    """This is an exception for GeoServer errors."""
+    __prepend__ = "GeoServer"
+    pass
+
+
+class InvalidDataError(TethysError):
+    """This is an exception for request input validation errors."""
+    __prepend__ = "Invalid data"
+    pass
+
+
+class NotFoundError(TethysError):
+    """This is an exception for items not found."""
+    __prepend__ = "Not found"
+    pass
+
+
+class SettingsError(TethysError):
+    """This is an exception for app settings errors."""
+    __prepend__ = "Settings"
+    pass
+
+
+class UploadError(TethysError):
+    """This is an exception for uploading errors."""
+    __prepend__ = "Upload"
+    pass
+
+
+class ErrorMiddleware(object):
+    def process_exception(self, request, exception):
+        """
+        This processes the exeption of the request.
+        """
+        if isinstance(exception, TethysError):
+            return HttpResponseBadRequest(exception.ajax_error_message)
+
---
+++ tethys_sdk/exceptions.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""exceptions.py
+
+    Author: Alan Snow
+    Created: September 20, 2017
+    License: BSD-3-Clause
+"""
+import tethys_exception.exception_handler as ajax
