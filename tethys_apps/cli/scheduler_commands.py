cac5cd47415f0609dbec6b3a879e9dacbb75fdb8
Shawn Crawley <scrawley@aquaveo.com>
2017-10-16 10:21:46 -0600

    add --force args to commands; check if Scheduler w/name exists

--- tethys_apps/cli/scheduler_commands.py
+++ tethys_apps/cli/scheduler_commands.py
@@ -12,6 +12,12 @@ def scheduler_create_command(args):
     private_key_path = args.private_key_path
     private_key_pass = args.private_key_pass
 
+    existing_scheduler = Scheduler.objects.filter(name=name).first()
+    if existing_scheduler:
+        with pretty_output(FG_RED) as p:
+            p.write('A Scheduler with name "{}" already exists. Command aborted.'.format(name))
+        exit(1)
+
     scheduler = Scheduler(
         name=name,
         host=host,
@@ -53,21 +59,31 @@ def schedulers_remove_command(args):
     from tethys_compute.models import Scheduler
     scheduler = None
     name = args.scheduler_name
+    force = args.force
 
     try:
         scheduler = Scheduler.objects.get(name=name)
     except ObjectDoesNotExist:
         with pretty_output(FG_RED) as p:
             p.write('Scheduler with name "{}" does not exist.\nCommand aborted.'.format(name))
+        exit(1)
 
-    proceed = raw_input('Are you sure you want to delete this Scheduler? [y/n]: ')
-    while proceed not in ['y', 'n', 'Y', 'N']:
-        proceed = raw_input('Please enter either "y" or "n": ')
-
-    if proceed in ['y', 'Y']:
+    if force:
         scheduler.delete()
         with pretty_output(FG_GREEN) as p:
             p.write('Successfully removed Scheduler "{0}"!'.format(name))
+        exit(0)
     else:
-        with pretty_output(FG_RED) as p:
-            p.write('Aborted. Scheduler not removed.')
+        proceed = raw_input('Are you sure you want to delete this Scheduler? [y/n]: ')
+        while proceed not in ['y', 'n', 'Y', 'N']:
+            proceed = raw_input('Please enter either "y" or "n": ')
+
+        if proceed in ['y', 'Y']:
+            scheduler.delete()
+            with pretty_output(FG_GREEN) as p:
+                p.write('Successfully removed Scheduler "{0}"!'.format(name))
+            exit(0)
+        else:
+            with pretty_output(FG_RED) as p:
+                p.write('Aborted. Scheduler not removed.')
+            exit(1)
