1267fa03161b0f969cc7c51ddb27182d14243a6d
nswain <nswain@aquaveo.com>
2018-02-23 17:15:29 -0700

    Generalized url loading to load urls from apps and extensions separately so that the appropriate namespace could be applied to the controllers (tethysext vs tethysapp). Loading extension urls with "extensions" as the root of the url.

--- tethys_apps/urls.py
+++ tethys_apps/urls.py
@@ -23,11 +23,17 @@ urlpatterns = [
 ]
 
 # Append the app urls urlpatterns
-app_url_patterns = generate_url_patterns()
+app_url_patterns, extension_url_patterns = generate_url_patterns()
 
 for namespace, urls in app_url_patterns.items():
     root_pattern = r'^{0}/'.format(namespace.replace('_', '-'))
     urlpatterns.append(url(root_pattern, include(urls, namespace=namespace)))
+
+extension_urls = []
+
+for namespace, urls in extension_url_patterns.items():
+    root_pattern = r'^{0}/'.format(namespace.replace('_', '-'))
+    extension_urls.append(url(root_pattern, include(urls, namespace=namespace)))
 
 # Register permissions here?
 try:
