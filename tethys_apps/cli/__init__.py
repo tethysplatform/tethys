5fd074630f316744bc5676c425cbc99214012fde
nswain <nswain@aquaveo.com>
2018-03-05 14:55:36 -0700

    Expanded tethys test command to test extensions. Added default test package back to the scaffold for extensions.

--- tethys_apps/cli/__init__.py
+++ tethys_apps/cli/__init__.py
@@ -154,8 +154,9 @@ def test_command(args):
     # Define the process to be run
     primary_process = ['python', manage_path, 'test']
 
-    # Tag to later check if tests are being run on a specific app
+    # Tag to later check if tests are being run on a specific app or extension
     app_package_tag = 'tethys_apps.tethysapp.'
+    extension_package_tag = 'tethysext.'
 
     if args.coverage or args.coverage_html:
         os.environ['TETHYS_TEST_DIR'] = tests_path
@@ -165,6 +166,12 @@ def test_command(args):
             core_app_package = '{}{}'.format(app_package_tag, app_name)
             app_package = 'tethysapp.{}'.format(app_name)
             config_opt = '--source={},{}'.format(core_app_package, app_package)
+        elif args.file and extension_package_tag in args.file:
+            extension_package_parts = args.file.split(extension_package_tag)
+            extension_name = extension_package_parts[1].split('.')[0]
+            core_extension_package = '{}{}'.format(extension_package_tag, extension_name)
+            extension_package = 'tethysext.{}'.format(extension_name)
+            config_opt = '--source={},{}'.format(core_extension_package, extension_package)
         else:
             config_opt = '--rcfile={0}'.format(os.path.join(tests_path, 'coverage.cfg'))
         primary_process = ['coverage', 'run', config_opt, manage_path, 'test']
@@ -179,7 +186,7 @@ def test_command(args):
     # print(primary_process)
     run_process(primary_process)
     if args.coverage:
-        if args.file and app_package_tag in args.file:
+        if args.file and (app_package_tag in args.file or extension_package_tag in args.file):
             run_process(['coverage', 'report'])
         else:
             run_process(['coverage', 'report', config_opt])
@@ -187,7 +194,7 @@ def test_command(args):
         report_dirname = 'coverage_html_report'
         index_fname = 'index.html'
 
-        if args.file and app_package_tag in args.file:
+        if args.file and (app_package_tag in args.file or extension_package_tag in args.file):
             run_process(['coverage', 'html', '--directory={0}'.format(os.path.join(tests_path, report_dirname))])
         else:
             run_process(['coverage', 'html', config_opt])
