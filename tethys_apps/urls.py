9604f8d6dd0b02d693c5cbb66244bacae4d7380e
nswain <nswain@aquaveo.com>
2018-03-07 13:21:01 -0700

    Fixed bug with uninstalling apps and extensions.

--- tethys_apps/urls.py
+++ tethys_apps/urls.py
@@ -7,8 +7,6 @@
 * License: BSD 2-Clause
 ********************************************************************************
 """
-# from django.db.utils import ProgrammingError
-# from django.core.exceptions import ObjectDoesNotExist
 from django.conf.urls import url, include
 from tethys_apps.harvester import SingletonHarvester
 from tethys_apps.views import library, send_beta_feedback_email
@@ -16,18 +14,13 @@ import logging
 
 tethys_log = logging.getLogger('tethys.' + __name__)
 
-# Get the harvester
-harvester = SingletonHarvester()
-
-# Sync the tethys apps database
-harvester.sync_tethys_db()
-
 urlpatterns = [
     url(r'^$', library, name='app_library'),
     url(r'^send-beta-feedback/$', send_beta_feedback_email, name='send_beta_feedback'),
 ]
 
 # Append the app urls urlpatterns
+harvester = SingletonHarvester()
 app_url_patterns, extension_url_patterns = harvester.get_url_patterns()
 
 for namespace, urls in app_url_patterns.items():
@@ -39,10 +32,3 @@ extension_urls = []
 for namespace, urls in extension_url_patterns.items():
     root_pattern = r'^{0}/'.format(namespace.replace('_', '-'))
     extension_urls.append(url(root_pattern, include(urls, namespace=namespace)))
-
-# # Register permissions here?
-# try:
-#     register_app_permissions()
-# except (ProgrammingError, ObjectDoesNotExist) as e:
-#     tethys_log.error(e)
-
