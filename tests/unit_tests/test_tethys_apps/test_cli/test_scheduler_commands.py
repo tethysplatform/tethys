764837bd99f0b195ed92cb6a927724e2b9cd3bc5
dgallup <dgallup@aquaveo.com>
2018-08-02 13:40:37 -0600

    Updated tests for tethys_apps.cli.scheduler_commands

--- tests/unit_tests/test_tethys_apps/test_cli/test_scheduler_commands.py
+++ tests/unit_tests/test_tethys_apps/test_cli/test_scheduler_commands.py
@@ -3,6 +3,7 @@ import mock
 
 from tethys_apps.cli.scheduler_commands import scheduler_create_command, schedulers_list_command, \
     schedulers_remove_command
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class SchedulerCommandsTest(unittest.TestCase):
@@ -50,7 +51,7 @@ class SchedulerCommandsTest(unittest.TestCase):
 
         self.assertRaises(SystemExit, scheduler_create_command, mock_args)
 
-        mock_scheduler.assert_not_called()
+        mock_scheduler.objects.filter.assert_called()
         po_call_args = mock_pretty_output().__enter__().write.call_args_list
         self.assertIn('already exists', po_call_args[0][0][0])
         mock_exit.assert_called_with(0)
@@ -78,22 +79,123 @@ class SchedulerCommandsTest(unittest.TestCase):
         mock_args = mock.MagicMock()
         schedulers_list_command(mock_args)
 
+        mock_scheduler.objects.all.assert_called()
+
         po_call_args = mock_pretty_output().__enter__().write.call_args_list
         self.assertEqual(1, len(po_call_args))
         self.assertEqual('There are no Schedulers registered in Tethys.', po_call_args[0][0][0])
 
-    def test_schedulers_remove_command_force(self):
-        pass
+    @mock.patch('tethys_apps.cli.scheduler_commands.pretty_output')
+    @mock.patch('tethys_apps.cli.scheduler_commands.exit')
+    @mock.patch('tethys_compute.models.Scheduler')
+    def test_schedulers_remove_command_force(self, mock_scheduler, mock_exit, mock_pretty_output):
+        mock_args = mock.MagicMock()
+        mock_args.force = True
+        # NOTE: to prevent our tests from exiting prematurely, we change the behavior of exit to raise an exception
+        # to break the code execution, which we catch below.
+        mock_exit.side_effect = SystemExit
 
-    def test_schedulers_remove_command_force_invalid_proceed_char(self):
-        pass
+        self.assertRaises(SystemExit, schedulers_remove_command, mock_args)
 
-    def test_schedulers_remove_command_no_force_proceed(self):
-        pass
+        mock_scheduler.objects.get.assert_called()
+        mock_scheduler.objects.get().delete.assert_called()
 
-    def test_schedulers_remove_command_no_force_no_proceed(self):
-        pass
+        po_call_args = mock_pretty_output().__enter__().write.call_args_list
+        self.assertEqual(1, len(po_call_args))
+        self.assertIn('Successfully removed Scheduler', po_call_args[0][0][0])
 
-    def test_schedulers_remove_command_does_not_exist(self):
-        pass
+    @mock.patch('__builtin__.raw_input')
+    @mock.patch('tethys_apps.cli.scheduler_commands.pretty_output')
+    @mock.patch('tethys_apps.cli.scheduler_commands.exit')
+    @mock.patch('tethys_compute.models.Scheduler')
+    def test_schedulers_remove_command_force_invalid_proceed_char(self, mock_scheduler, mock_exit, mock_pretty_output,
+                                                                  mock_input):
+        mock_args = mock.MagicMock()
+        mock_args.force = False
+        # NOTE: to prevent our tests from exiting prematurely, we change the behavior of exit to raise an exception
+        # to break the code execution, which we catch below.
+        mock_exit.side_effect = SystemExit
+        mock_input.side_effect = ['foo', 'N']
 
+        self.assertRaises(SystemExit, schedulers_remove_command, mock_args)
+
+        mock_scheduler.objects.get.assert_called()
+
+        po_call_args = mock_pretty_output().__enter__().write.call_args_list
+        self.assertEqual(1, len(po_call_args))
+        self.assertEqual('Aborted. Scheduler not removed.', po_call_args[0][0][0])
+
+        po_call_args = mock_input.call_args_list
+        self.assertEqual(2, len(po_call_args))
+        self.assertEqual('Are you sure you want to delete this Scheduler? [y/n]: ', po_call_args[0][0][0])
+        self.assertEqual('Please enter either "y" or "n": ', po_call_args[1][0][0])
+
+    @mock.patch('__builtin__.raw_input')
+    @mock.patch('tethys_apps.cli.scheduler_commands.pretty_output')
+    @mock.patch('tethys_apps.cli.scheduler_commands.exit')
+    @mock.patch('tethys_compute.models.Scheduler')
+    def test_schedulers_remove_command_no_force_proceed(self, mock_scheduler, mock_exit, mock_pretty_output,
+                                                        mock_input):
+        mock_args = mock.MagicMock()
+        mock_args.force = False
+        mock_input.side_effect = ['Y']
+        # NOTE: to prevent our tests from exiting prematurely, we change the behavior of exit to raise an exception
+        # to break the code execution, which we catch below.
+        mock_exit.side_effect = SystemExit
+
+        self.assertRaises(SystemExit, schedulers_remove_command, mock_args)
+
+        mock_scheduler.objects.get.assert_called()
+        mock_scheduler.objects.get().delete.assert_called()
+
+        po_call_args = mock_pretty_output().__enter__().write.call_args_list
+        self.assertEqual(1, len(po_call_args))
+        self.assertIn('Successfully removed Scheduler', po_call_args[0][0][0])
+
+        po_call_args = mock_input.call_args_list
+        self.assertEqual(1, len(po_call_args))
+        self.assertEqual('Are you sure you want to delete this Scheduler? [y/n]: ', po_call_args[0][0][0])
+
+    @mock.patch('__builtin__.raw_input')
+    @mock.patch('tethys_apps.cli.scheduler_commands.pretty_output')
+    @mock.patch('tethys_apps.cli.scheduler_commands.exit')
+    @mock.patch('tethys_compute.models.Scheduler')
+    def test_schedulers_remove_command_no_force_no_proceed(self, mock_scheduler, mock_exit, mock_pretty_output,
+                                                           mock_input):
+        mock_args = mock.MagicMock()
+        mock_args.force = False
+        mock_input.side_effect = ['N']
+        # NOTE: to prevent our tests from exiting prematurely, we change the behavior of exit to raise an exception
+        # to break the code execution, which we catch below.
+        mock_exit.side_effect = SystemExit
+
+        self.assertRaises(SystemExit, schedulers_remove_command, mock_args)
+
+        mock_scheduler.objects.get.assert_called()
+        mock_scheduler.objects.get().delete.assert_not_called()
+
+        po_call_args = mock_pretty_output().__enter__().write.call_args_list
+        self.assertEqual(1, len(po_call_args))
+        self.assertEqual('Aborted. Scheduler not removed.', po_call_args[0][0][0])
+
+        po_call_args = mock_input.call_args_list
+        self.assertEqual(1, len(po_call_args))
+        self.assertEqual('Are you sure you want to delete this Scheduler? [y/n]: ', po_call_args[0][0][0])
+
+    @mock.patch('tethys_apps.cli.scheduler_commands.pretty_output')
+    @mock.patch('tethys_apps.cli.scheduler_commands.exit')
+    @mock.patch('tethys_compute.models.Scheduler')
+    def test_schedulers_remove_command_does_not_exist(self, mock_scheduler, mock_exit, mock_pretty_output):
+        mock_args = mock.MagicMock()
+        mock_scheduler.objects.get.side_effect = ObjectDoesNotExist
+        # NOTE: to prevent our tests from exiting prematurely, we change the behavior of exit to raise an exception
+        # to break the code execution, which we catch below.
+        mock_exit.side_effect = SystemExit
+
+        self.assertRaises(SystemExit, schedulers_remove_command, mock_args)
+
+        mock_scheduler.objects.get.assert_called()
+
+        po_call_args = mock_pretty_output().__enter__().write.call_args_list
+        self.assertEqual(1, len(po_call_args))
+        self.assertIn('Command aborted.', po_call_args[0][0][0])
