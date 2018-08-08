e34043517ba81d60a77e06071c849f6cc140f659
dgallup <dgallup@aquaveo.com>
2018-08-08 12:19:31 -0600

    Updates for test, including assert_called_with

--- tests/unit_tests/test_tethys_apps/test_cli/test_uninstall_command.py
+++ tests/unit_tests/test_tethys_apps/test_cli/test_uninstall_command.py
@@ -12,12 +12,12 @@ class UninstallCommandTests(unittest.TestCase):
     def tearDown(self):
         pass
 
-    @mock.patch('subprocess.call')
+    @mock.patch('tethys_apps.cli.uninstall_command.subprocess.call')
     @mock.patch('tethys_apps.cli.uninstall_command.get_manage_path')
     def test_uninstall_command(self, mock_get_manage_path, mock_subprocess_call):
         mock_args = mock.MagicMock()
         mock_args.is_extension = True
-        mock_args.app_or_extension = 'foo'
+        mock_args.app_or_extension = 'foo_ext'
         mock_get_manage_path.return_value = '/foo/manage.py'
         mock_subprocess_call.return_value = True
 
@@ -25,13 +25,29 @@ class UninstallCommandTests(unittest.TestCase):
 
         mock_get_manage_path.assert_called_once()
         mock_subprocess_call.assert_called_once()
+        mock_subprocess_call.assert_called_with(['python', '/foo/manage.py', 'tethys_app_uninstall', 'foo_ext', '-e'])
 
-    @mock.patch('subprocess.call')
+    @mock.patch('tethys_apps.cli.uninstall_command.subprocess.call')
+    @mock.patch('tethys_apps.cli.uninstall_command.get_manage_path')
+    def test_uninstall_command_no_extension(self, mock_get_manage_path, mock_subprocess_call):
+        mock_args = mock.MagicMock()
+        mock_args.is_extension = False
+        mock_args.app_or_extension = 'foo_app'
+        mock_get_manage_path.return_value = '/foo/manage.py'
+        mock_subprocess_call.return_value = True
+
+        uninstall_command(mock_args)
+
+        mock_get_manage_path.assert_called_once()
+        mock_subprocess_call.assert_called_once()
+        mock_subprocess_call.assert_called_with(['python', '/foo/manage.py', 'tethys_app_uninstall', 'foo_app'])
+
+    @mock.patch('tethys_apps.cli.uninstall_command.subprocess.call')
     @mock.patch('tethys_apps.cli.uninstall_command.get_manage_path')
     def test_uninstall_command_assert(self, mock_get_manage_path, mock_subprocess_call):
         mock_args = mock.MagicMock()
         mock_args.is_extension = True
-        mock_args.app_or_extension = 'foo'
+        mock_args.app_or_extension = 'foo_ext'
         mock_get_manage_path.return_value = '/foo/manage.py'
         mock_subprocess_call.side_effect = KeyboardInterrupt
 
@@ -39,3 +55,4 @@ class UninstallCommandTests(unittest.TestCase):
 
         mock_get_manage_path.assert_called_once()
         mock_subprocess_call.assert_called_once()
+        mock_subprocess_call.assert_called_with(['python', '/foo/manage.py', 'tethys_app_uninstall', 'foo_ext', '-e'])
