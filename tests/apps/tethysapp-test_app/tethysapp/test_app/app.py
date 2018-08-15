d48f1fbe24e1f1c4132f55ff4aca4e394455f690
nswain <nswain@aquaveo.com>
2018-08-15 13:53:56 -0600

    First half of tests for TethysApp model.

--- tests/apps/tethysapp-test_app/tethysapp/test_app/app.py
+++ tests/apps/tethysapp-test_app/tethysapp/test_app/app.py
@@ -1,5 +1,6 @@
 from tethys_sdk.base import TethysAppBase, url_map_maker
-from tethys_sdk.app_settings import CustomSetting
+from tethys_sdk.app_settings import CustomSetting, PersistentStoreDatabaseSetting, PersistentStoreConnectionSetting, \
+    DatasetServiceSetting, SpatialDatasetServiceSetting, WebProcessingServiceSetting
 
 
 class TestApp(TethysAppBase):
@@ -55,15 +56,104 @@ class TestApp(TethysAppBase):
               name='change_factor',
               type=CustomSetting.TYPE_FLOAT,
               description='Change factor that is applied to some process.',
-              required=True
+              required=False
           ),
           CustomSetting(
               name='enable_feature',
               type=CustomSetting.TYPE_BOOLEAN,
               description='Enable this feature when True.',
-              required=True
+              required=False
           )
         )
 
         return custom_settings
+
+    def persistent_store_settings(self):
+        """
+        Example persistent_store_settings method.
+        """
+        ps_settings = (
+            # Connection only, no database
+            PersistentStoreConnectionSetting(
+                name='primary',
+                description='Connection with superuser role needed.',
+                required=True
+            ),
+            # Connection only, no database
+            PersistentStoreConnectionSetting(
+                name='creator',
+                description='Create database role only.',
+                required=False
+            ),
+            # Spatial database
+            PersistentStoreDatabaseSetting(
+                name='spatial_db',
+                description='for storing important spatial stuff',
+                required=False,
+                initializer='appsettings.model.init_spatial_db',
+                spatial=True,
+            ),
+            # Non-spatial database
+            PersistentStoreDatabaseSetting(
+                name='temp_db',
+                description='for storing temporary stuff',
+                required=False,
+                initializer='appsettings.model.init_temp_db',
+                spatial=False,
+            )
+        )
+
+        return ps_settings
+
+    def dataset_service_settings(self):
+        """
+        Example dataset_service_settings method.
+        """
+        ds_settings = (
+            DatasetServiceSetting(
+                name='primary_ckan',
+                description='Primary CKAN service for app to use.',
+                engine=DatasetServiceSetting.CKAN,
+                required=True,
+            ),
+            DatasetServiceSetting(
+                name='hydroshare',
+                description='HydroShare service for app to use.',
+                engine=DatasetServiceSetting.HYDROSHARE,
+                required=False
+            )
+        )
+
+        return ds_settings
+
+    def spatial_dataset_service_settings(self):
+        """
+        Example spatial_dataset_service_settings method.
+        """
+        sds_settings = (
+            SpatialDatasetServiceSetting(
+                name='primary_geoserver',
+                description='spatial dataset service for app to use',
+                engine=SpatialDatasetServiceSetting.GEOSERVER,
+                required=True,
+            ),
+        )
+
+        return sds_settings
+
+    def web_processing_service_settings(self):
+        """
+        Example wps_services method.
+        """
+        wps_services = (
+            WebProcessingServiceSetting(
+                name='primary_52n',
+                description='WPS service for app to use',
+                required=True,
+            ),
+        )
+
+        return wps_services
+
+
 
