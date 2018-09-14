e2169a11d657f8f78e8644ca21f485bfd299a7c3
teva <tveluppillai@aquaveo.com>
2018-09-14 12:19:08 -0600

    Fix the failing test from test_jobs_table in test_gizmo_options.

--- tests/unit_tests/test_tethys_gizmos/test_gizmo_options/test_jobs_table.py
+++ tests/unit_tests/test_tethys_gizmos/test_gizmo_options/test_jobs_table.py
@@ -19,12 +19,36 @@ class TestJobsTable(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_JobsTable(self):
+    @mock.patch('tethys_gizmos.gizmo_options.jobs_table.JobsTable.set_rows_and_columns')
+    def test_JobsTable_init(self, mock_set):
         job1 = JobObject(1, 'name1', 'des1', 1, 1)
         job2 = JobObject(2, 'name2', 'des2', 2, 2)
         jobs = [job1, job2]
         column_fields = ['id', 'name', 'description', 'creation_time', 'run_time']
 
+        ret = gizmo_jobs_table.JobsTable(jobs=jobs, column_fields=column_fields)
+
+        mock_set.assert_called_with(jobs,  ['id', 'name', 'description', 'creation_time', 'run_time'])
+        self.assertTrue(ret.status_actions)
+        self.assertTrue(ret.run)
+        self.assertTrue(ret.delete)
+        self.assertTrue(ret.delay_loading_status)
+        self.assertFalse(ret.hover)
+        self.assertFalse(ret.bordered)
+        self.assertFalse(ret.striped)
+        self.assertFalse(ret.condensed)
+        self.assertFalse(ret.attributes)
+        self.assertEqual('', ret.results_url)
+        self.assertEqual('', ret.classes)
+        self.assertEqual(5000, ret.refresh_interval)
+
+    def test_set_rows_and_columns(self):
+        job1 = JobObject(1, 'name1', 'des1', 1, 1)
+        job2 = JobObject(2, 'name2', 'des2', 2, 2)
+        jobs = [job1, job2]
+        column_fields = ['id', 'name', 'description', 'creation_time', 'run_time']
+
+        # This set_rows_and_columns method is called at the init
         result = gizmo_jobs_table.JobsTable(jobs=jobs, column_fields=column_fields)
         self.assertIn(job1.id, result['rows'][0])
         self.assertIn(job1.name, result['rows'][0])
@@ -39,16 +63,16 @@ class TestJobsTable(unittest.TestCase):
         self.assertTrue(result['status_actions'])
 
     @mock.patch('tethys_gizmos.gizmo_options.jobs_table.log.warning')
-    def test_jobsTable_warning(self, mock_log):
+    def test_set_rows_and_columns_warning(self, mock_log):
         job1 = JobObject(1, 'name1', 'des1', 1, 1)
         jobs = [job1]
-        column_name = 'id2'
+        column_name = 'not_exist'
         column_fields = [column_name]
 
         gizmo_jobs_table.JobsTable(jobs=jobs, column_fields=column_fields)
 
-        mock_log.assert_called_with('Column %s was not added because Job has no attribute %s.',
-                                    column_name.title(), column_name)
+        mock_log.assert_called_with('Column %s was not added because the %s has no attribute %s.',
+                                    'Not Exist', str(job1), column_name)
 
     def test_get_gizmo_js(self):
         self.assertIn('jobs_table.js', gizmo_jobs_table.JobsTable.get_gizmo_js()[0])
