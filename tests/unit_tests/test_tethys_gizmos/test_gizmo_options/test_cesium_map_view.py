0b844d384d719486ce4a5171e67ecda67d2ddb6a
Nathan Swain <swainn@users.noreply.github.com>
2018-12-07 15:11:45 -0700

    Cesium Map View (#362)
    * Cesium Map View
    
    * Created CesiumMapView gizmo options class
    * Implemented cesium_map_view.js that is able to dynamically generate appropriate code from Python (i.e.: instantiate classes and call functions from strings) to initialize Cesium Viewer.
    * CesiumMapView supports wms layers, terrain layers, czml layers, and 3D models.
    * Implemented a simple 2D annotation capability to draw points, lines, polygons, extents and circles on the map.
    * CesiumMapView uses a hidden text field mechanism for retrieving drawn geometry in a form, like MapView.
    * Added gizmo showcase page with several views that demonstrates much of the functionality of CesiumMapView.
    * Added documentation for CesiumMapView.
    * Added and updated tests to maintain coverage.

---
+++ tests/unit_tests/test_tethys_gizmos/test_gizmo_options/test_cesium_map_view.py
@@ -0,0 +1,82 @@
+import unittest
+import tethys_gizmos.gizmo_options.cesium_map_view as gizmo_map_view
+
+
+class MockObject(object):
+    def __init__(self, debug=True):
+        self.DEBUG = debug
+
+
+class TestCesiumMapView(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_CesiumMapView(self):
+        options = {'options': 'options_value'}
+        view = {'view': 'view_value'}
+        layers = ['layer1']
+        entities = ['entities1', 'entities2']
+        terrain = {'terrain': 'terrain_value'}
+        models = ['model1', 'model2']
+        height = '80%'
+        width = '80%'
+
+        gizmo_map_view.CesiumMapView.cesium_version = '1.51'
+        result = gizmo_map_view.CesiumMapView(height=height, width=width, options=options, view=view,
+                                              layers=layers, entities=entities, terrain=terrain, models=models)
+
+        # Check Result
+        self.assertEqual(height, result['height'])
+        self.assertEqual(width, result['width'])
+        self.assertEqual(options, result['options'])
+        self.assertEqual(view, result['view'])
+        self.assertEqual(layers, result['layers'])
+        self.assertEqual(entities, result['entities'])
+        self.assertEqual(terrain, result['terrain'])
+        self.assertEqual(models, result['models'])
+        self.assertFalse(result['draw'])
+
+        # Check sources
+        self.assertIn('https://cesiumjs.org/releases/1.51/Build/Cesium/Cesium.js',
+                      gizmo_map_view.CesiumMapView.get_vendor_js()[0])
+        self.assertIn('https://cesiumjs.org/releases/1.51/Build/Cesium/Widgets/widgets.css',
+                      gizmo_map_view.CesiumMapView.get_vendor_css()[0])
+        self.assertIn('.js', gizmo_map_view.CesiumMapView.get_gizmo_js()[0])
+        self.assertIn('.css', gizmo_map_view.CesiumMapView.get_vendor_css()[0])
+        self.assertIn('.css', gizmo_map_view.CesiumMapView.get_gizmo_css()[0])
+
+    def test_CesiumMapView_build_version(self):
+        options = {'options': 'options_value'}
+        view = {'view': 'view_value'}
+        layers = ['layer1']
+        entities = ['entities1', 'entities2']
+        terrain = {'terrain': 'terrain_value'}
+        models = ['model1', 'model2']
+
+        # Set cesium_version to blank to use the build version.
+        gizmo_map_view.CesiumMapView.cesium_version = ''
+        result = gizmo_map_view.CesiumMapView(options=options, view=view, layers=layers, entities=entities,
+                                              terrain=terrain, models=models, draw=True)
+
+        # Check Result
+        self.assertEqual('100%', result['height'])
+        self.assertEqual('100%', result['width'])
+        self.assertEqual(options, result['options'])
+        self.assertEqual(view, result['view'])
+        self.assertEqual(layers, result['layers'])
+        self.assertEqual(entities, result['entities'])
+        self.assertEqual(terrain, result['terrain'])
+        self.assertEqual(models, result['models'])
+        self.assertTrue(result['draw'])
+
+        # Check sources
+        self.assertIn('https://cesiumjs.org/Cesium/Build/Cesium/Cesium.js',
+                      gizmo_map_view.CesiumMapView.get_vendor_js()[0])
+        self.assertIn('https://cesiumjs.org/Cesium/Build/Cesium/Widgets/widgets.css',
+                      gizmo_map_view.CesiumMapView.get_vendor_css()[0])
+        self.assertIn('.js', gizmo_map_view.CesiumMapView.get_gizmo_js()[0])
+        self.assertIn('.css', gizmo_map_view.CesiumMapView.get_vendor_css()[0])
+        self.assertIn('.css', gizmo_map_view.CesiumMapView.get_gizmo_css()[0])
