5657ec8f9d14ac0e0d4de2ed922ec40d647a322d
Corey Krewson <ckrewson@aquaveo.local>
2019-01-16 14:55:05 -0700

    open portal capabilities and made a custom login_required for open portal mode. Added Apps link to main tethys page when open portal is enabled

--- tethys_apps/views.py
+++ tethys_apps/views.py
@@ -8,7 +8,7 @@
 ********************************************************************************
 """
 import logging
-from django.contrib.auth.decorators import login_required
+from tethys_sdk.permissions import login_required
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.core.mail import send_mail
