53aaffa3366a0724c1948db345f3841fbe36fff7
rditlsc9 <Scott.D.Christensen@erdc.dren.mil>
2019-01-24 15:38:12 -0600

    add "set" template tag

--- tethys_gizmos/templatetags/tethys_gizmos.py
+++ tethys_gizmos/templatetags/tethys_gizmos.py
@@ -77,6 +77,38 @@ class HighchartsDateEncoder(DjangoJSONEncoder):
         return super(HighchartsDateEncoder, self).default(obj)
 
 
+class SetVarNode(template.Node):
+
+    def __init__(self, var_name, var_value):
+        self.var_names = var_name.split('.')
+        self.var_name = self.var_names.pop()
+        self.var_value = var_value
+
+    def render(self, context):
+        try:
+            value = template.Variable(self.var_value).resolve(context)
+        except template.VariableDoesNotExist:
+            value = ''
+
+        for name in self.var_names:
+            context = context[name]
+
+        context[self.var_name] = value
+
+        return ''
+
+@register.tag(name='set')
+def set_var(parser, token):
+    """
+    {% set some_var = '123' %}
+    """
+    parts = token.split_contents()
+    if len(parts) < 4:
+        raise template.TemplateSyntaxError("'set' tag must be of the form: {% set <var_name> = <var_value> %}")
+
+    return SetVarNode(parts[1], parts[3])
+
+
 @register.filter(is_safe=True)
 def isstring(value):
     """
