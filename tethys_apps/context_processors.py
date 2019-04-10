ba1780fedb69c381762ca8e04d13882025098bd1
rileyhales <rileyhales1@gmail.com>
2019-04-10 17:11:23 -0600

    implements tracking based only on the load analytical tag

--- tethys_apps/context_processors.py
+++ tethys_apps/context_processors.py
@@ -23,8 +23,6 @@ def tethys_apps_context(request):
         request: Django request object.
     """
 
-    from django.core.management import settings
-
     # Setup variables
     context = {'tethys_app': None}
 
@@ -49,32 +47,5 @@ def tethys_apps_context(request):
 
             if hasattr(app, 'enable_feedback'):
                 context['tethys_app']['enable_feedback'] = app.enable_feedback
-
-        # Web-Analytics services checks/Adding appropriate tags
-        context['analytics'] = {
-            'analytical': bool('analytical' in settings.INSTALLED_APPS),
-            'clickmap': bool(settings.CLICKMAP_TRACKER_ID),
-            'clicky': bool(settings.CLICKY_SITE_ID),
-            'crazyegg': bool(settings.CRAZY_EGG_ACCOUNT_NUMBER),
-            'gauges': bool(settings.GAUGES_SITE_ID),
-            'googleanalytics': bool(settings.GOOGLE_ANALYTICS_JS_PROPERTY_ID),
-            'gosquared': bool(settings.GOSQUARED_SITE_TOKEN),
-            'hotjar': bool(settings.HOTJAR_SITE_ID),
-            'hubspot': bool(settings.HUBSPOT_PORTAL_ID),
-            'intercomio': bool(settings.INTERCOM_APP_ID),
-            'kissinsights': bool(settings.KISSINSIGHTS_ACCOUNT_NUMBER and settings.KISSINSIGHTS_SITE_CODE),
-            'kissmetrics': bool(settings.KISS_METRICS_API_KEY),
-            'mixpanel': bool(settings.MIXPANEL_API_TOKEN),
-            'olark': bool(settings.OLARK_SITE_ID),
-            'optimizely': bool(settings.OPTIMIZELY_ACCOUNT_NUMBER),
-            'performable': bool(settings.PERFORMABLE_API_KEY),
-            'piwik': bool(settings.PIWIK_DOMAIN_PATH and settings.PIWIK_SITE_ID),
-            'mailru': bool(settings.RATING_MAILRU_COUNTER_ID),
-            'snapengage': bool(settings.SNAPENGAGE_WIDGET_ID),
-            'springmetrics': bool(settings.SPRING_METRICS_TRACKING_ID),
-            'uservote': bool(settings.USERVOICE_WIDGET_KEY),
-            'woopra': bool(settings.WOOPRA_DOMAIN),
-            'yandex': bool(settings.YANDEX_METRICA_COUNTER_ID),
-        }
 
     return context
