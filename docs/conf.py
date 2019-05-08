a1f6b6bf12a81bfed01f169b06135c72a9c6609f
Tylor Bayer <tbayer@aquaveo.com>
2019-05-08 15:35:15 -0600

    removed space....

--- docs/conf.py
+++ docs/conf.py
@@ -265,7 +265,7 @@ latex_elements = {
     # 'pointsize': '10pt',
 
     # Additional stuff for the LaTeX preamble.
-    # 'preamble':  '',
+    # 'preamble': '',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
