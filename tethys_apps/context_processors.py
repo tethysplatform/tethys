5e5e43d2be394351b1982adca5757a986f0fc6f9
nswain <nswain@aquaveo.com>
2019-07-09 17:10:11 -0500

    Pulled bootstrap and jquery versions into the main tethys_apps.context_processor (so we can change version in one place in the future.

--- tethys_apps/context_processors.py
+++ tethys_apps/context_processors.py
@@ -41,4 +41,19 @@ def tethys_apps_context(request):
             if hasattr(app, 'enable_feedback'):
                 context['tethys_app']['enable_feedback'] = app.enable_feedback
 
+    # Dependency Versions
+    context.update({
+        'tethys': {
+            'jquery': {
+                'version': '3.4.1',
+                'css_integrity': 'sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=',
+                'js_integrity': 'sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd'
+            },
+            'bootstrap': {
+                'version': '3.4.1',
+                'integrity': 'sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu'
+            }
+        }
+    })
+
     return context
