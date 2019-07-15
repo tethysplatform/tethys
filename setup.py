576fdb33fbf818e6380247f6cd002c80465a08dd
rditlsc9 <scott.d.christensen@erdc.dren.mil>
2019-07-15 16:21:03 -0500

    conda recipe updates MANIFEST.in added to manage pkg data

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@
 ********************************************************************************
 """
 import os
+from pathlib import Path
 from setuptools import setup, find_packages
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
@@ -16,6 +17,12 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
+# mangle settings.py if it exists to prevent from being included in installation
+settings = Path('tethys_portal/settings.py')
+temp_settings = Path('tethys_portal/__settings.tmp')
+if settings.is_file():
+    settings.rename(temp_settings)
+
 requires = []
 
 version = '3.0.0a2'
@@ -23,7 +30,7 @@ version = '3.0.0a2'
 setup(
     name='tethys_platform',
     version=version,
-    packages=find_packages(),
+    packages=find_packages(exclude=['tethys_tests', 'tethys_tests.*']),
     include_package_data=True,
     license='BSD 2-Clause License',
     description='Primary Tethys Platform Django Site Project',
@@ -38,8 +45,8 @@ setup(
         'License :: OSI Approved :: BSD License',  # example license
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
@@ -53,3 +60,7 @@ setup(
         ],
     },
 )
+
+# restore mangled settings.py
+if temp_settings.is_file():
+    temp_settings.rename(settings)
