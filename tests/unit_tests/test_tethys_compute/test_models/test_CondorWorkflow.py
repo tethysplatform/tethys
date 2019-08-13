51d6056ef64cb54e06d6bce312647cffa7c986b4
swainn <nswain@aquaveo.com>
2019-08-13 17:34:54 -0600

    Fixes tests for new CondorWorkflow refactor.

--- tests/unit_tests/test_tethys_compute/test_models/test_CondorWorkflow.py
+++ tests/unit_tests/test_tethys_compute/test_models/test_CondorWorkflow.py
@@ -13,17 +13,21 @@ import os.path
 
 class CondorWorkflowTest(TethysTestCase):
     def set_up(self):
-        path = os.path.dirname(__file__)
-        self.workspace_dir = os.path.join(path, 'workspace')
+        test_models_dir = os.path.dirname(__file__)
+        self.workspace_dir = os.path.join(test_models_dir, 'workspace')
         self.user = User.objects.create_user('tethys_super', 'user@example.com', 'pass')
+
+        files_dir = os.path.join(os.path.dirname(test_models_dir), 'files')
+        self.private_key = os.path.join(files_dir, 'keys', 'testkey')
+        self.private_key_pass = 'password'
 
         self.scheduler = CondorScheduler(
             name='test_scheduler',
             host='localhost',
             username='tethys_super',
             password='pass',
-            private_key_path='test_path',
-            private_key_pass='test_pass'
+            private_key_path=self.private_key,
+            private_key_pass=self.private_key_pass
         )
         self.scheduler.save()
 
