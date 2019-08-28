f9e9f05a898752adb380094244078c4a6be2c47a
msouff <msouffront@aquaveo.com>
2019-08-28 13:05:14 -0600

    working on consumer test

--- tests/apps/tethysapp-test_app/tethysapp/test_app/controllers.py
+++ tests/apps/tethysapp-test_app/tethysapp/test_app/controllers.py
@@ -5,6 +5,16 @@ from tethys_sdk.gizmos import Button
 from channels.generic.websocket import WebsocketConsumer
 import json
 
+import pandas as pd
+import numpy as np
+
+from typing import Any
+
+from bokeh.models import ColumnDataSource, Slider
+from bokeh.plotting import figure
+from bokeh.layouts import column
+from bokeh.embed import server_document
+
 
 @login_required()
 def home(request):
@@ -67,19 +77,44 @@ def home(request):
         }
     )
 
+    script = server_document(request.build_absolute_uri())
+
     context = {
         'save_button': save_button,
         'edit_button': edit_button,
         'remove_button': remove_button,
         'previous_button': previous_button,
-        'next_button': next_button
+        'next_button': next_button,
+        'script': script
     }
 
     return render(request, 'test_app/home.html', context)
 
 
 def home_handler(doc):
-    pass
+    dates = pd.date_range(start='1/1/2019', end='1/06/2019', freq='D')
+
+    data = dict(dates=dates, values=np.array([0, 10, 20, 30, 40, 50]))
+    df = pd.DataFrame(data)
+    source = ColumnDataSource(data=df)
+
+    plot = figure(x_axis_type="datetime", y_range=(0, 50), y_axis_label="Values",
+                  title="Test App Bokeh + Channels Plot", height=250)
+    plot.line("dates", "values", source=source)
+
+    def callback(attr: str, old: Any, new: Any) -> None:
+        if new == 1:
+            data = df.copy()
+        else:
+            data = df.copy()
+            data.loc[:, 'values'] *= new
+        source.data = ColumnDataSource(data=data).data
+        plot.y_range.end = data['values'].max()
+
+    slider = Slider(start=1, end=5, value=1, step=1, title="Test App Bokeh + Channels Controller")
+    slider.on_change("value", callback)
+
+    doc.add_root(column(slider, plot))
 
 
 class TestWS(WebsocketConsumer):
--- tests/unit_tests/test_tethys_portal/test_consumers.py
+++ tests/unit_tests/test_tethys_portal/test_consumers.py
@@ -1,46 +1,51 @@
-# import os
-# import json
-# import asyncio
-#
-# from tethys_sdk.testing import TethysTestCase
-# from channels.testing import WebsocketCommunicator
-# from bokeh.server.django.consumers import SessionConsumer
-# from tethys_portal.consumers.bokeh_consumers import BokehAutoloadJsCDN
-#
-# from bokeh.server.django import autoload
-# from tethysapp.test_app.controllers import home_handler
-#
-# from unittest import mock
-#
-#
-# class TestBokehAutoloadJsCDN(TethysTestCase):
-#     @mock.patch('bokeh.server.django.consumers.SessionConsumer', spec=SessionConsumer)
-#     def test_bokeh_autoload_js_cdn(self, _):
-#         event_loop = asyncio.new_event_loop()
-#         asyncio.set_event_loop(event_loop)
-#
-#         bokeh_app = autoload("/apps/test-app", home_handler)
-#         # mock_kwargs = mock.MagicMock(return_value=dict(app_context=bokeh_app.app_context))
-#
-#         async def run_test():
-#             communicator = WebsocketCommunicator(BokehAutoloadJsCDN, "/apps/test-app/autoload.js",
-#                                                  headers=[(b'bokeh-autoload-element', b'1075'),
-#                                                           (b'bokeh-app-path', b'/apps/test-app&bokeh-absolute-url'),
-#                                                           (b'http://localhost:8000/apps/test-app')])
-#             connected, subprotocol = await communicator.connect()
-#
-#             # Test connection
-#             self.assertTrue(connected)
-#
-#             # Test sending and receiving messages
-#             await communicator.send_to(text_data=json.dumps({'msgid': "1"}))
-#             response = await communicator.receive_from()
-#             self.assertEqual(json.loads(response)["server_message"], "This is a consumer test")
-#
-#             # Close
-#             await communicator.disconnect()
-#
-#         # Run the async test
-#         coroutine = asyncio.coroutine(run_test)
-#         event_loop.run_until_complete(coroutine())
-#         event_loop.close()
+import asyncio
+import os
+import re
+
+from django.conf.urls import url
+
+from channels.testing import HttpCommunicator
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.http import AsgiHandler
+
+from bokeh.server.django import autoload
+
+from tethys_sdk.testing import TethysTestCase
+from tethys_portal.consumers.bokeh_consumers import BokehAutoloadJsCDN
+
+from tethysapp.test_app.controllers import home_handler
+
+
+class TestBokehAutoloadJsCDN(TethysTestCase):
+    def test_bokeh_autoload_js_cdn(self):
+        event_loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(event_loop)
+
+        bokeh_app = autoload("/apps/test-app", home_handler)
+        kwargs = dict(app_context=bokeh_app.app_context)
+
+        application = ProtocolTypeRouter({
+            'http': AuthMiddlewareStack(
+                URLRouter([
+                    url(os.path.join(re.escape(bokeh_app.url)) + '/autoload.js', BokehAutoloadJsCDN, kwargs=kwargs),
+                    url(r'', AsgiHandler)
+                ])
+            ),
+            # 'websocket': AuthMiddlewareStack(
+            #     URLRouter([
+            #         url(os.path.join(re.escape(bokeh_app.url)) + '/ws', BokehAutoloadJsCDN, kwargs=kwargs),
+            #     ])
+            # ),
+        })
+
+        async def run_test():
+            communicator = HttpCommunicator(application, "GET", "/apps/test-app")
+            response = await communicator.get_response()
+
+            assert response['status'] == 200
+
+        # Run the async test
+        coroutine = asyncio.coroutine(run_test)
+        event_loop.run_until_complete(coroutine())
+        event_loop.close()
