e0ba641c459051cf18f2efef1621b0da5562e284
nswain <nswain@aquaveo.com>
2019-08-30 22:30:41 -0600

    Update docs theme to material design theme.

--- docs/conf.py
+++ docs/conf.py
@@ -115,7 +115,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Tethys Platform'
-copyright = u'2014, Nathan Swain'
+copyright = u'2019, Tethys Platform'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -353,16 +353,10 @@ texinfo_documents = [
 # on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-# otherwise, readthedocs.org uses their theme by default, so no need to specify it
-
-
 # If this is True, todo and todolist produce output, else they produce nothing. The default is False.
 todo_include_todos = True
 
 # If this is True, todo emits a warning for each TODO entries. The default is False.
 todo_emit_warnings = True
+
+html_theme = 'sphinx_materialdesign_theme'
