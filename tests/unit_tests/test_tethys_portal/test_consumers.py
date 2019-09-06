a736e4d66cdd347b989ff585c754d1fcbeeec48c
msouff <msouffront@aquaveo.com>
2019-09-06 11:30:29 -0600

    working on consumer test

--- tests/unit_tests/test_tethys_portal/test_consumers.py
+++ tests/unit_tests/test_tethys_portal/test_consumers.py
@@ -1,29 +1,15 @@
-# from django.conf.urls import url
-#
-# from channels.testing import HttpCommunicator
-# from channels.routing import URLRouter
-# from channels.http import AsgiHandler
-#
-# from bokeh.server.django import autoload
-#
-# from tethys_sdk.testing import TethysTestCase
-# from tethys_portal.consumers.bokeh_consumers import BokehAutoloadJsCDN
-#
-# from tethysapp.test_app.controllers import home_handler
-#
-#
-# class TestBokehAutoloadJsCDN(TethysTestCase):
-#     def set_up(self):
-#         bokeh_app = autoload("apps/test-app", home_handler)
-#         kwargs = dict(app_context=bokeh_app.app_context)
-#
-#         self.application = URLRouter([
-#             url('apps/test-app/autoload.js', BokehAutoloadJsCDN, kwargs=kwargs),
-#             url(r'', AsgiHandler)
-#         ])
-#
-#     def test_bokeh_autoload_js_cdn(self):
-#         communicator = HttpCommunicator(self.application, "GET", "/apps/test-app/autoload.js/")
-#         response = communicator.get_response()
-#
-#         assert response['status'] == 200
+from tethys_sdk.testing import TethysTestCase
+
+from bokeh.server.django.consumers import SessionConsumer
+from tethys_portal.consumers.bokeh_consumers import BokehAutoloadJsCDN
+
+
+class TestConsumers(TethysTestCase):
+    def test_bokeh_autoload_resources(self):
+        res = BokehAutoloadJsCDN.resources(SessionConsumer)
+
+        self.assertEqual('cdn', res.mode)
+        self.assertIn('bokeh', res.js_components)
+        self.assertIn('bokeh-widgets', res.js_components)
+        self.assertIn('bokeh-tables', res.js_components)
+        self.assertIn('bokeh-gl', res.js_components)
