16d4cef72639610379ca10c5fbc908258ab4c833
Nathan Swain <swainn@users.noreply.github.com>
2019-10-04 16:15:52 -0600

    Added gen command for the conda recipe meta.yaml file (#490)
    * Added gen command that will create the conda recipe meta.yaml file based on the environment.yml requirements. Needs tests.
    
    * Tweaks CI to build conda and docker for PRs but not push
    
    * Fixes typo in Dockerfile
    
    * Added bokeh/label/dev channel to conda-build step of CI to fix the build.
    
    * Adds tests for new metayaml gen command.
    Adds pin-level optional argument to gen command to specify what level to pin dependencies when generating the meta yaml file.
    New update meta yaml file.
    
    * Refactors CI to generate meta.yaml
    
    Removes meta.yaml from repo and add CI steps to generate it after
    running tests successfully.
    
    * Only generate meta.yaml after linux tests
    
    * Enables artifacts addon
    
    * Changes to use safe_load for yaml parsing.
    Add contrained meta.yaml to get the conda build working.
    
    * Make default pin level argument consistent between functions and CLI interface.
    Add variable for the conda build pin level.
    
    * Default meta.yaml with no constraints.

--- tethys_cli/gen_commands.py
+++ tethys_cli/gen_commands.py
@@ -7,14 +7,15 @@
 * License: BSD 2-Clause
 ********************************************************************************
 """
-import os
 import string
 import random
-from tethys_apps.utilities import get_tethys_home_dir, get_tethys_src_dir
+import os
+from conda.cli.python_api import run_command, Commands
+from yaml import safe_load
 from distro import linux_distribution
-
 from django.conf import settings
 from jinja2 import Template
+from tethys_apps.utilities import get_tethys_home_dir, get_tethys_src_dir
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tethys_portal.settings")
@@ -29,6 +30,7 @@ GEN_PORTAL_OPTION = 'portal'
 GEN_SERVICES_OPTION = 'services'
 GEN_INSTALL_OPTION = 'install'
 GEN_SITE_YAML_OPTION = 'site_content'
+GEN_META_YAML_OPTION = 'metayaml'
 
 FILE_NAMES = {
     GEN_SETTINGS_OPTION: 'settings.py',
@@ -40,6 +42,7 @@ FILE_NAMES = {
     GEN_SERVICES_OPTION: 'services.yml',
     GEN_INSTALL_OPTION: 'install.yml',
     GEN_SITE_YAML_OPTION: 'site_content.yml',
+    GEN_META_YAML_OPTION: 'meta.yaml'
 }
 
 VALID_GEN_OBJECTS = (
@@ -51,7 +54,8 @@ VALID_GEN_OBJECTS = (
     GEN_PORTAL_OPTION,
     GEN_SERVICES_OPTION,
     GEN_INSTALL_OPTION,
-    GEN_SITE_YAML_OPTION
+    GEN_SITE_YAML_OPTION,
+    GEN_META_YAML_OPTION
 )
 
 TETHYS_SRC = get_tethys_src_dir()
@@ -64,6 +68,9 @@ def add_gen_parser(subparsers):
                                                    'creation of supporting files.')
     gen_parser.add_argument('type', help='The type of object to generate.', choices=VALID_GEN_OBJECTS)
     gen_parser.add_argument('-d', '--directory', help='Destination directory for the generated object.')
+    gen_parser.add_argument('-p', '--pin-level', choices=['major', 'minor', 'patch', 'none'],
+                            help='Level to pin dependencies when generating the meta.yaml. One of "major", "minor", '
+                                 '"patch", or "none". Defaults to "none".')
     gen_parser.add_argument('--allowed-hosts', dest='allowed_hosts', nargs='+',
                             help='Add one or more hostnames or IP addresses to ALLOWED_HOSTS in the settings file. '
                                  'e.g.: 127.0.0.1 localhost')
@@ -167,7 +174,7 @@ def add_gen_parser(subparsers):
                             open_signup=False, tethys_port=8000, overwrite=False, add_apps=None,
                             session_persist=False, session_warning=840, session_expire=900,
                             bypass_portal_home=False, channel_layer='', recaptcha_private_key=None,
-                            recaptcha_public_key=None)
+                            recaptcha_public_key=None, pin_level='none')
 
 
 def get_environment_value(value_name):
@@ -375,6 +382,78 @@ def gen_services_yaml(args):
     return context
 
 
+def derive_version_from_conda_environment(dep_str, level='none'):
+    """
+    Determine dependency string based on the current tethys environment.
+
+    Args:
+        dep_str(str): The dep string from the environment.yml (e.g. 'python>=3.6').
+        level(str): Level to lock dependencies to. One of 'major', 'minor', 'patch', or None. Defaults to 'minor'.
+
+    Returns:
+        str: the dependency string.
+    """
+    stdout, stderr, ret = run_command(Commands.LIST, dep_str)
+
+    if ret != 0:
+        print(f'ERROR: Something went wrong looking up dependency "{dep_str}" in environment')
+        print(stderr)
+        return dep_str
+
+    lines = stdout.split('\n')
+
+    for line in lines:
+        if line.startswith('#'):
+            continue
+
+        try:
+            package, version, build, channel = line.split()
+        except ValueError:
+            continue
+
+        if package != dep_str:
+            continue
+
+        version_numbers = version.split('.')
+
+        if level == 'major':
+            if len(version_numbers) >= 2:
+                dep_str = f'{package}={version_numbers[0]}.*'
+            if len(version_numbers) == 1:
+                dep_str = f'{package}={version_numbers[0]}'
+        elif level == 'minor':
+            if len(version_numbers) >= 3:
+                dep_str = f'{package}={version_numbers[0]}.{version_numbers[1]}.*'
+            elif len(version_numbers) == 2:
+                dep_str = f'{package}={version_numbers[0]}.{version_numbers[1]}'
+        elif level == 'patch':
+            if len(version_numbers) > 3:
+                dep_str = f'{package}={version_numbers[0]}.{version_numbers[1]}.{version_numbers[2]}.*'
+            elif len(version_numbers) >= 1:
+                dep_str = f'{package}={".".join(version_numbers)}'
+
+    return dep_str
+
+
+def gen_meta_yaml(args):
+    environment_file_path = os.path.join(TETHYS_SRC, 'environment.yml')
+    with open(environment_file_path, 'r') as env_file:
+        environment = safe_load(env_file)
+
+    dependencies = environment.get('dependencies', [])
+    run_requirements = []
+
+    for dependency in dependencies:
+        if not any([operator in dependency for operator in ['=', '<', '>']]):
+            conda_env_version = derive_version_from_conda_environment(dependency, level=args.pin_level)
+            run_requirements.append(conda_env_version)
+        else:
+            run_requirements.append(dependency)
+
+    context = dict(run_requirements=run_requirements)
+    return context
+
+
 def gen_install(args):
     print('Please review the generated install.yml file and fill in the appropriate information '
           '(app name is requited).')
@@ -399,6 +478,9 @@ def get_destination_path(args):
 
     if args.type in [GEN_SERVICES_OPTION, GEN_INSTALL_OPTION, GEN_SITE_YAML_OPTION]:
         destination_dir = os.getcwd()
+
+    elif args.type == GEN_META_YAML_OPTION:
+        destination_dir = os.path.join(TETHYS_SRC, 'conda.recipe')
 
     if args.directory:
         directory = os.path.abspath(args.directory)
@@ -447,7 +529,7 @@ def render_template(file_type, context, destination_path):
             f.write(template.render(context))
 
 
-gen_commands = {
+GEN_COMMANDS = {
     GEN_SETTINGS_OPTION: gen_settings,
     GEN_ASGI_SERVICE_OPTION: gen_asgi_service,
     GEN_NGINX_OPTION: gen_nginx,
@@ -456,6 +538,7 @@ gen_commands = {
     GEN_SERVICES_OPTION: gen_services_yaml,
     GEN_INSTALL_OPTION: gen_install,
     GEN_SITE_YAML_OPTION: gen_site_content_yaml,
+    GEN_META_YAML_OPTION: gen_meta_yaml
 }
 
 
@@ -464,7 +547,7 @@ def generate_command(args):
     Generate a settings file for a new installation.
     """
     # Setup variables
-    context = gen_commands[args.type](args)
+    context = GEN_COMMANDS[args.type](args)
 
     destination_path = get_destination_path(args)
 
