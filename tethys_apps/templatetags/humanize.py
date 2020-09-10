d6dbbde16a116b23ed26103a6d05310a5f85439b
nswain <nswain@aquaveo.com>
2020-09-10 17:00:21 -0600

    Adds custom template filter to humanize ISO 8601 time durations (e.g. "PT30M" -> "in 30 minutes"). Use custom template filter to provide cool off period in lockout message. Hide flash messages on lockout template.

--- tethys_apps/templatetags/humanize.py
+++ tethys_apps/templatetags/humanize.py
@@ -6,8 +6,17 @@ register = template.Library()
 
 
 @register.filter
-def human_duration(iso_duration):
-    delta = isodate.parse_duration(iso_duration)
+def human_duration(iso_duration_str):
+    """
+    Converts an ISO 8601 formatted duration to a humanized time from now (UTC).
+
+    Args:
+        iso_duration_str: An ISO 8601 formatted string (e.g. "P1DT3H6M")
+
+    Returns:
+        str: humanized string representing the amount of time from now (e.g.: "in 30 minutes").
+    """
+    time_change = isodate.parse_duration(iso_duration_str)
     now = arrow.utcnow()
-    sum = now + delta
-    return sum.humanize()
+    time_from_now = now + time_change
+    return time_from_now.humanize()
