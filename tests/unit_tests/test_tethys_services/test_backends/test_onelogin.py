8e0519febcd177c6fa0060a988301fb0678731b1
nswain <nswain@aquaveo.com>
2020-09-11 11:34:02 -0600

    Fix and improve tests for OneLogin.

--- tests/unit_tests/test_tethys_services/test_backends/test_onelogin.py
+++ tests/unit_tests/test_tethys_services/test_backends/test_onelogin.py
@@ -1,55 +1,125 @@
+import datetime as dt
+import hashlib
+import random
+import string
 from unittest import mock
-from django import test
+import uuid
 from social_core.exceptions import AuthTokenError
 from jose.jwt import JWTError, JWTClaimsError, ExpiredSignatureError
+from jose import jwt
+from jose.utils import base64url_encode
+from django import test
 from tethys_services.backends.onelogin import OneLoginOIDC
 
 
+client_id = '182f8cb0-b8b2-0138-3936-0647e35409d7174645'
+
+
 @test.override_settings(
-    SOCIAL_AUTH_ONELOGIN_OIDC_KEY='182f8cb0-b8b2-0138-3936-0647e35409d7174645',
+    SOCIAL_AUTH_ONELOGIN_OIDC_KEY=client_id,
     SOCIAL_AUTH_ONELOGIN_OIDC_SECRET='032f86d0f5f367171fd6270f7f5fa383fa27604bace9c5f1a0a5d5d2b4227ace',
     SOCIAL_AUTH_ONELOGIN_OIDC_SUBDOMAIN='https://my-org.onelogin.mok'
 )
 class OneLoginOIDCBackendTest(test.SimpleTestCase):
 
     def setUp(self):
-        self.id_token = \
-            'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkpSY080bnhzNWpnYzhZZE43STJoTE80Vl9xbDFiZG9pTVhtY1lnSG00SHMi' \
-            'fQ.eyJzdWIiOiI5MTI5MTU4NiIsImVtYWlsIjoibnN3YWluQGFxdWF2ZW8uY29tIiwicHJlZmVycmVkX3VzZXJuYW1lIjoibnN3YWluQ' \
-            'GFxdWF2ZW8uY29tIiwibmFtZSI6Ik5hdGhhbiBTd2FpbiIsInVwZGF0ZWRfYXQiOjE1OTk2ODk2NjIsImdpdmVuX25hbWUiOiJOYXRoY' \
-            'W4iLCJmYW1pbHlfbmFtZSI6IlN3YWluIiwibm9uY2UiOiJVQmY4bzlNRHd4M01aWUFaNlpPMVY4VWVybUVmSDc1dVhyT09CTUdoSGxFb' \
-            '2dzaU5WdEsydGd5M1dmbk9CUVZKIiwiYXRfaGFzaCI6InBYZ0VkR3RFUU5vRGRrMlU2RnRzcmciLCJzaWQiOiIwNzM5NGE2YS01YzgyL' \
-            'TRjMTctYmFjMS1iYTJmZWE4ZmFhYjgiLCJhdWQiOiIxODJmOGNiMC1iOGIyLTAxMzgtMzkzNi0wNjQ3ZTM1NDA5ZDcxNzQ2NDUiLCJle' \
-            'HAiOjE1OTk2OTg3OTIsImlhdCI6MTU5OTY5MTU5MiwiaXNzIjoiaHR0cHM6Ly9hcXVhdmVvLWRldi5vbmVsb2dpbi5jb20vb2lkYy8yI' \
-            'n0.yhr6PujjojakrIHkzfOXYhBpgA3fEUcpbet4XvmM0zwoAJUfwXlsxS8hJZ0ub67H9CShhSbW7apUVZCjC_E6ENzyah9ccdF5-G0wJ' \
-            'Uw4dnraP_WROaXpOY4bx18PBSzjFEFSsg2-b32zMkOsL4vnKUXrgN1itekrX9BTUqwPd_Q-WelRMnA3Rrmp7galL1pbSl-QIU33Zubyv' \
-            'J1_rf6hMWKhB5NaUVX_ZlnYZpxNy1m1hPqyVDSXHjZ3PZj0CpD0Zc60_XNgRzOl03RXZb_XmImuXsf6Pk3X3TV2xGnQxaXXKFN0ctGiW' \
-            'QsQyeoIdYzgEv_XLGCuCjEz-wFU1p-ylA'
-
-        self.access_token = \
-            'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkpSY080bnhzNWpnYzhZZE43STJoTE80Vl9xbDFiZG9pTVhtY1lnSG00SHM' \
-            'ifQ.eyJqdGkiOiJlS1pWVGpkUHVHOG5IeVFnbGp5dm8iLCJzdWIiOiI5MTI5MTU4NiIsImlzcyI6Imh0dHBzOi8vYXF1YXZlby1kZXY' \
-            'ub25lbG9naW4uY29tL29pZGMvMiIsImlhdCI6MTU5OTY5MTU5MiwiZXhwIjoxNTk5Njk1MTkyLCJzY29wZSI6Im9wZW5pZCBwcm9maW' \
-            'xlIGVtYWlsIiwiYXVkIjoiMTgyZjhjYjAtYjhiMi0wMTM4LTM5MzYtMDY0N2UzNTQwOWQ3MTc0NjQ1In0.Vwr-QII98aopLNRtYG5qv' \
-            '2DqWAd2Q2Z7DGCbH_Ck9nNKp-zXmDnUYPB9lbOs1HIJVzCYvRxsyjjzcPwOfuYSoJHm1Jd1bNX5n-GnzmuLyicUdjHprlDM5lRxkdPE' \
-            'CAMUAdAJupc9OLVUs5hJwaoHlfGrkz-DiUpP6TjVbPIgB3XqkmkyvoEftzJ4Pz62Jr9zP0WGjVFhySLVmYgvnsPwwKa6nlkhiRABNux' \
-            'Qr8OR7gy9sUgcIaTYGI9TXCjO4sY_ITL08-vhXJIFd3zj0oDlIL15NnX_6ehgDEapDh60S0EHIhzJ4oqGyV8YjJcq2avk3cPGGXvjjV' \
-            'ztpUN011HjhQ'
-
-        self.key = {
-            'kty': 'RSA',
-            'kid': 'JRcO4nxs5jgc8YdN7I2hLO4V_ql1bdoiMXmcYgHm4Hs',
-            'use': 'sig',
-            'n': 'z8fZszkUNh1y1iSI6ZCkrwoZx1ZcFuQEngI8G_9VPjJXupqbgXedsV0YqDzQzYmdXd_lLb_OYWdyAP1FV6d2d4PfVjw4rGLqgYN'
-                 '5hEPFYqDEusiKtXyeh38xl37Nb8LGTX1qdstZjcXRo2YQ64W4UyuMko_TGOCxRNJg1fAfxRt1yV_ZeFV_93BMNjubV2D7kvpzaS'
-                 'tJmYJi8A6QHqaqHaQkxAvYhJVi9XDajD3vvUlTVyOjURAnuaByA749glGBio5N9AfFTnYbHbeBOK3VJi6EJZzsuj3-5P4GUTYnS'
-                 'frScs_kblaoeqt4GkExJqMZXGJTfGnX2UbYAjGHSTAoQw',
-            'e': 'AQAB'
-        }
+        numbers = "0123456789"
+        alphanumeric = string.ascii_letters + numbers
+        self.nounce = "".join(random.choices(alphanumeric, k=64))
+        self.sub = "".join(random.choices(numbers, k=8))
+        self.iat = dt.datetime.utcnow()
+        self.id_exp = self.iat + dt.timedelta(hours=3)
+        self.access_exp = self.iat + dt.timedelta(hours=1)
+        self.sid = str(uuid.uuid4())
+        self.jti = "".join(random.choices(string.ascii_letters, k=21))
+        self.kid = "".join(random.choices(alphanumeric + '_', k=43))
         self.issuer = 'https://aquaveo-dev.onelogin.com/oidc/2'
+
+        self.pub_key = {
+            "kty": "RSA",
+            "e": "AQAB",
+            "use": "sig",
+            "kid": "APiZ0bGTMSgbLm_UitMgU7TDlE_9UC-RZArArPU4vcc",
+            "n": "ynmjBq8tJtmvvlKIhSHFBPWu1maBVa4y-r6Q4_ODbP956CvDd9w7ttHojyn-kMeIqiNTdDe7O3B7KRJ7qlLRRWERrwm-FVeQGVltNY5h7QuLjiwyA5Wcc5auczPeAPPJLy-MVo0xgmQHt_otr8nPM-groDwhecpZPlsF8XSRU5RBjfu04s52bFku26PonBLUopkJ05EKCf2r4sPqhunhfZi_nJ6iR5DHFovKOKJ5m4ER44vZW2WnuqSFZuO2zgeRwnJo2gdBLLDh6fiJmnyaTrW0oD4h7sW1BLJWgcEBBMC2-9qyeCdGwSQLqGGdoEI4JnFGbM6y6U6twNUsCB7mmQ"
+        }
+
+        self.key_pair = {
+            "p": "5lRvOpLl0ryH9ReBhZfojJuY69Z-UhwcfaxCuxfNY19ehCQav5qpNL-A8DX3e7jfKQ7MLRWmVjOR8PAkIx7-lBn_U5Pd9DdE9lcUfGSvrNDnKkwMIx-7ybsUau2qi-SfHAjk3a-1scOInmTsY6uO9RmpotZvc5c31plSBNRzRH8",
+            "kty": "RSA",
+            "q": "4Qp6OV-Nm2jrea1hLGPaITjdV0V8AaXxken3AY0qdATJPTxddXi903-U4mMCTjoX3yoDsmI_xiy-VAH1pyMt7JPhFDPtMPBKxYqnka6BI96cTG5vld8xe2jyiTE7vBHDXzCMld00teABThE6gYe7uI3nuvCp3c2-lxdVLXl36Oc",
+            "d": "QsRHkQ20-umvvTfkEtqm6FLHu6WDoqhV4T9z8ec8AHZiUcAUrfKuskOXx5XWfxF9LHWHsHwsPpaB1nJk2y6CGbm_phO1Xb5JI0LM82snOY6kD1bdizcLp0YZ8p046OF7BxdL1MKsf83SicVyyVPoArrdn_IUK-Ag-6HyXMPOpFtZ9Sm0xk2OdiqO5DIEfGrR0QnAs4t8Qk5uIQMzzB0PXYJYI6ecPSZC6zPYWwq6Gf6i320rIuzNHBVKR_jKCvhLMphOyB-QMdTTgMXJCj1gmCFuLdSk2-poQhLh160-dtF2LU6dU973yAApp1ubszsRrXLSecpNBOJBBdTTU6Z4vQ",
+            "e": "AQAB",
+            "use": "sig",
+            "kid": "APiZ0bGTMSgbLm_UitMgU7TDlE_9UC-RZArArPU4vcc",
+            "qi": "udVEXUjdnR-1Hh7QrYz3SHXcJRILy_AS1bbNQ5PaU-26IVkJquuUgHXIJHsqN8SjUwGSNJTtDkFlodo9O6sUgCtphMUzl8Hrr5wc6gcQTai4F8VEevRSSb7aElGlmHLHNJ49Sh2X_0ClWHbV8YbU_NplkTEMEos6LhGTUiAi-Bg",
+            "dp": "N6Ukbhk5Ls1fZ0Xzg6vsgbPBoRBj2kByXaY1RO_-LcjHk9MJJ0sdH-I4K1BOZvSF7WxmxWmaku7IjlXl1RpS1MLnHXD2yV5k6cKl71t_2ZdqkZxnvkCXZKguyZp7fmqQSfyYFjqyAa1En0ewmG_FDM_TXMTDjkB1PAI0f3g4FM8",
+            "dq": "2M6CUHIJeHdNnY13OA__H9CsFh9ASEo6gMw6h4kcJGhUBKX6mGk54OBibPTcTMdVJlBQ_XQAYwnKWB8HLv4KiYky6IbadjKaptRdelH34rneNRiUcnx0LKRAPgJrvVYTtsqMNvnFhY-JUk_w4MgHB9fXMFgaHzHP87qr-kKzTnM",
+            "n": "ynmjBq8tJtmvvlKIhSHFBPWu1maBVa4y-r6Q4_ODbP956CvDd9w7ttHojyn-kMeIqiNTdDe7O3B7KRJ7qlLRRWERrwm-FVeQGVltNY5h7QuLjiwyA5Wcc5auczPeAPPJLy-MVo0xgmQHt_otr8nPM-groDwhecpZPlsF8XSRU5RBjfu04s52bFku26PonBLUopkJ05EKCf2r4sPqhunhfZi_nJ6iR5DHFovKOKJ5m4ER44vZW2WnuqSFZuO2zgeRwnJo2gdBLLDh6fiJmnyaTrW0oD4h7sW1BLJWgcEBBMC2-9qyeCdGwSQLqGGdoEI4JnFGbM6y6U6twNUsCB7mmQ"
+        }
 
     def tearDown(self):
         pass
+
+    def generate_access_token(self):
+        """
+        Generate JWT Access Token.
+        """
+        access_claims = {
+            "jti": self.jti,
+            "sub": self.sub,
+            "iss": self.issuer,
+            "iat": self.iat,
+            "exp": self.access_exp,
+            "scope": "openid profile email",
+            "aud": client_id
+        }
+        access_token = jwt.encode(
+            access_claims,
+            self.key_pair,
+            algorithm='RS256',
+            headers={'kid': self.kid}
+        )
+        return access_token
+
+    def generate_id_token(self, access_token, at_hash=None):
+        """
+        Generate JWT ID Token.
+        """
+        id_claims = {
+            "sub": self.sub,
+            "email": "foo.bar@baz.com",
+            "preferred_username": "fbar",
+            "name": "Foo Bar",
+            "updated_at": 1599689662,
+            "given_name": "Foo",
+            "family_name": "Bar",
+            "nonce": self.nounce,
+            "at_hash": self.generate_at_hash(access_token) if not at_hash else at_hash,
+            "sid": self.sid,
+            "aud": client_id,
+            "exp": int(self.id_exp.timestamp()),
+            "iat": int(self.iat.timestamp()),
+            "iss": self.issuer
+        }
+        id_token = jwt.encode(
+            id_claims,
+            self.key_pair,
+            algorithm='RS256',
+            headers={'kid': self.kid}
+        )
+        return id_token
+
+    @staticmethod
+    def generate_at_hash(access_token):
+        """
+        Get hash for JWT access token.
+        """
+        hash = hashlib.sha256()
+        hash.update(access_token.encode())
+        digest = hash.digest()
+        digest_truncated = digest[:(int(len(digest) / 2))]
+        at_hash = base64url_encode(digest_truncated).decode()
+        return at_hash
 
     def test_oidc_endpoint(self):
         inst = OneLoginOIDC()
@@ -78,58 +148,75 @@ class OneLoginOIDCBackendTest(test.SimpleTestCase):
         self.assertEqual('https://my-org.onelogin.mok/oidc/2', ret)
 
     def test_find_valid_key(self):
+        access_token = self.generate_access_token()
+        id_token = self.generate_id_token(access_token)
         inst = OneLoginOIDC()
-        inst.get_jwks_keys = mock.MagicMock(return_value=[self.key])
-        ret = inst.find_valid_key(self.id_token)
-        self.assertEqual(self.key, ret)
+        inst.get_jwks_keys = mock.MagicMock(return_value=[self.pub_key])
+        ret = inst.find_valid_key(id_token)
+        self.assertEqual(self.pub_key, ret)
 
     def test_validate_and_return_id_token(self):
+        access_token = self.generate_access_token()
+        id_token = self.generate_id_token(access_token)
         inst = OneLoginOIDC()
-        inst.find_valid_key = mock.MagicMock(return_value=self.key)
+        inst.find_valid_key = mock.MagicMock(return_value=self.pub_key)
         inst.id_token_issuer = mock.MagicMock(return_value=self.issuer)
         inst.validate_claims = mock.MagicMock()
-        inst.validate_and_return_id_token(self.id_token, self.access_token)
+        inst.validate_and_return_id_token(id_token, access_token)
 
     def test_validate_and_return_id_token__no_valid_key(self):
+        access_token = self.generate_access_token()
+        id_token = self.generate_id_token(access_token)
         inst = OneLoginOIDC()
         inst.find_valid_key = mock.MagicMock(return_value=None)
+
         with self.assertRaises(AuthTokenError) as cm:
-            inst.validate_and_return_id_token(self.id_token, self.access_token)
+            inst.validate_and_return_id_token(id_token, access_token)
 
         self.assertEqual(str(cm.exception), 'Token error: Signature verification failed')
 
-    @mock.patch('tethys_services.backends.onelogin.jwt.decode')
-    def test_validate_and_return_id_token__expired_signature(self, mock_decode):
+    def test_validate_and_return_id_token__expired_signature(self):
+        # Backdate iat and exp parameters 1 day to make them expired
+        self.iat = dt.datetime.utcnow() - dt.timedelta(days=1)
+        self.id_exp = self.iat + dt.timedelta(hours=3)
+        self.access_exp = self.iat + dt.timedelta(hours=1)
+
+        access_token = self.generate_access_token()
+        id_token = self.generate_id_token(access_token)
+
         inst = OneLoginOIDC()
-        inst.find_valid_key = mock.MagicMock(return_value=self.key)
+        inst.find_valid_key = mock.MagicMock(return_value=self.pub_key)
         inst.id_token_issuer = mock.MagicMock(return_value=self.issuer)
-        mock_decode.side_effect = ExpiredSignatureError
 
         with self.assertRaises(AuthTokenError) as cm:
-            inst.validate_and_return_id_token(self.id_token, self.access_token)
+            inst.validate_and_return_id_token(id_token, access_token)
 
         self.assertEqual(str(cm.exception), 'Token error: Signature has expired')
 
-    @mock.patch('tethys_services.backends.onelogin.jwt.decode')
-    def test_validate_and_return_id_token__claims_error(self, mock_decode):
+    def test_validate_and_return_id_token__claims_error(self):
+        # Generate id_token with an invalid access token hash
+        access_token = self.generate_access_token()
+        id_token = self.generate_id_token(access_token, at_hash='iNvAlIdAtHaSh')
+
         inst = OneLoginOIDC()
-        inst.find_valid_key = mock.MagicMock(return_value=self.key)
+        inst.find_valid_key = mock.MagicMock(return_value=self.pub_key)
         inst.id_token_issuer = mock.MagicMock(return_value=self.issuer)
-        mock_decode.side_effect = JWTClaimsError('Foo bar')
 
         with self.assertRaises(AuthTokenError) as cm:
-            inst.validate_and_return_id_token(self.id_token, self.access_token)
+            inst.validate_and_return_id_token(id_token, access_token)
 
-        self.assertEqual(str(cm.exception), 'Token error: Foo bar')
+        self.assertEqual(str(cm.exception), 'Token error: at_hash claim does not match access_token.')
 
     @mock.patch('tethys_services.backends.onelogin.jwt.decode')
     def test_validate_and_return_id_token__jwt_error(self, mock_decode):
+        access_token = self.generate_access_token()
+        id_token = self.generate_id_token(access_token)
         inst = OneLoginOIDC()
-        inst.find_valid_key = mock.MagicMock(return_value=self.key)
+        inst.find_valid_key = mock.MagicMock(return_value=self.pub_key)
         inst.id_token_issuer = mock.MagicMock(return_value=self.issuer)
         mock_decode.side_effect = JWTError
 
         with self.assertRaises(AuthTokenError) as cm:
-            inst.validate_and_return_id_token(self.id_token, self.access_token)
+            inst.validate_and_return_id_token(id_token, access_token)
 
         self.assertEqual(str(cm.exception), 'Token error: Invalid signature')
