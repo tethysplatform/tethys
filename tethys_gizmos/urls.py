b7424b642938445837ba403ac81b069aca420345
sdc50 <scott.d.christensen@erdc.dren.mil>
2020-11-06 13:37:25 -0700

    Jobs Table Actions (#577)
    * start of jobs table action list
    
    action list and style enhancements
    
    action list fixes
    
    start of jobs table action list
    
    action list and style enhancements
    
    action list fixes
    
    * fix/add tests
    
    * bugfix for get_log_content
    
    * Add deprecation warning
    Remove deprecated AXES setting
    
    * Add TethysJob status message
    fix jobs table bugs
    Improve styling
    
    * Improve styling
    
    * Apply suggestions from code review
    
    Co-authored-by: Nathan Swain <swainn@users.noreply.github.com>
    
    Co-authored-by: Nathan Swain <swainn@users.noreply.github.com>

--- tethys_gizmos/urls.py
+++ tethys_gizmos/urls.py
@@ -15,15 +15,17 @@ ajax_urls = [
     url(r'^get-kml/$', gizmo_showcase_views.get_kml, name='get_kml'),
     url(r'^swap-kml/$', gizmo_showcase_views.swap_kml, name='swap_kml'),
     url(r'^swap-overlays/$', gizmo_showcase_views.swap_overlays, name='swap_overlays'),
+    url(r'^(?P<job_id>[\d.@+-]+)/terminate', jobs_table_views.terminate, name='terminate_job'),
     url(r'^(?P<job_id>[\d.@+-]+)/delete$', jobs_table_views.delete, name='delete_job'),
     url(r'^(?P<job_id>[\d.@+-]+)/resubmit$', jobs_table_views.resubmit, name='resubmit_job'),
     url(r'^(?P<job_id>[\d.@+-]+)/show-log', jobs_table_views.show_log, name='show_log'),
+    url(r'^(?P<job_id>[\d.@+-]+)/log-content/(?P<key1>[\w+_-]+)(?:/(?P<key2>.*))?', jobs_table_views.get_log_content,
+        name='log_content'),
     url(r'^(?P<job_id>[\d.@+-]+)/execute', jobs_table_views.execute, name='execute_job'),
     url(r'^(?P<job_id>[\d.@+-]+)/update-row', jobs_table_views.update_row, name='update_job_row'),
     url(r'^(?P<job_id>[\d.@+-]+)/update-workflow-nodes-row', jobs_table_views.update_workflow_nodes_row,
         name='update_workflow_nodes_row'),
     url(r'^(?P<job_id>[\d.@+-]+)/(?P<type>[\w-]+)/insert-bokeh-row', jobs_table_views.bokeh_row, name='bokeh_row'),
-    url(r'^(?P<job_id>[\d.@+-]+)/update-status', jobs_table_views.update_status, name='update_job_status'),
 ]
 
 urlpatterns = [
