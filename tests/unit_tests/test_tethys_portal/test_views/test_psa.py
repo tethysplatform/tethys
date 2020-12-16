63eb8b6b1e7591024215db5ea041b8373f3cfc62
nswain <nswain@aquaveo.com>
2020-12-16 15:01:57 -0700

    Adds tests for tenant view.

--- tests/unit_tests/test_tethys_portal/test_views/test_psa.py
+++ tests/unit_tests/test_tethys_portal/test_views/test_psa.py
@@ -4,6 +4,7 @@ from unittest import mock
 from django.http import HttpResponseBadRequest
 from django.test import override_settings
 from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.core.exceptions import ImproperlyConfigured
 from social_core.backends.base import BaseAuth
 
 from tethys_services.backends.multi_tenant_mixin import MultiTenantMixin
@@ -69,9 +70,9 @@ class TethysPortalViewsAccountsTest(unittest.TestCase):
     def test_tenant_view_post_no_submit(self, mock_tenant_form):
         mock_backend = mock.MagicMock(spec=MultiTenantMixin)
         mock_request = mock.MagicMock(method='POST', POST=dict(), backend=mock_backend)  # Empty POST dict
-        backend = 'foo'
+        url_backend = 'foo'
 
-        ret = tenant(mock_request, backend)
+        ret = tenant(mock_request, url_backend)
 
         mock_tenant_form.assert_not_called()
 
@@ -90,9 +91,9 @@ class TethysPortalViewsAccountsTest(unittest.TestCase):
             'remember': False
         }
         mock_request = mock.MagicMock(method='POST', POST=post_params, backend=mock_backend)  # valid POST request
-        backend = 'foo'
+        url_backend = 'foo'
 
-        ret = tenant(mock_request, backend)
+        ret = tenant(mock_request, url_backend)
 
         # Make sure form is bound to POST data
         mock_tenant_form.assert_called_with(mock_request.POST)
@@ -100,3 +101,67 @@ class TethysPortalViewsAccountsTest(unittest.TestCase):
         mock_do_auth.assert_called_with(mock_backend, redirect_name=REDIRECT_FIELD_NAME)
         self.assertEqual('GitHub', mock_backend.tenant)
         self.assertEqual(mock_do_auth(), ret)
+
+    @override_settings(SSO_TENANT_ALIAS='foo bar')
+    @mock.patch('tethys_portal.views.psa.log')
+    @mock.patch('tethys_portal.views.psa.redirect')
+    @mock.patch('tethys_portal.views.psa.do_auth')
+    @mock.patch('tethys_portal.views.psa.SsoTenantForm', spec=SsoTenantForm)
+    def test_tenant_view_post_improperly_configured(self, mock_tenant_form, mock_do_auth, mock_redirect, mock_log):
+        mock_tenant_form.is_valid = mock.MagicMock(return_value=True)
+        mock_tenant_form().cleaned_data = {'tenant': 'GitHub'}
+        mock_backend = mock.MagicMock(spec=MultiTenantMixin)
+        type(mock_backend).tenant = mock.PropertyMock(side_effect=ImproperlyConfigured('some error message'))
+        post_params = {
+            'sso-tenant-submit': 'submit',
+            'tenant': 'GitHub',
+            'remember': False
+        }
+        mock_request = mock.MagicMock(method='POST', POST=post_params, backend=mock_backend)  # valid POST request
+        url_backend = 'foo'
+
+        ret = tenant(mock_request, url_backend)
+
+        # Make sure form is bound to POST data
+        mock_tenant_form.assert_called_with(mock_request.POST)
+        mock_tenant_form().is_valid.assert_called()
+        mock_do_auth.assert_not_called()
+        mock_log.error.assert_called_with('some error message')
+        mock_redirect.assert_called_with('accounts:login')
+        self.assertEqual(mock_redirect(), ret)
+
+    @override_settings(SSO_TENANT_ALIAS='Thingy')
+    @mock.patch('tethys_portal.views.psa.render')
+    @mock.patch('tethys_portal.views.psa.do_auth')
+    @mock.patch('tethys_portal.views.psa.SsoTenantForm', spec=SsoTenantForm)
+    def test_tenant_view_post_value_error(self, mock_tenant_form, mock_do_auth, mock_render):
+        mock_tenant_form.is_valid = mock.MagicMock(return_value=True)
+        mock_tenant_form().cleaned_data = {'tenant': 'GitHub'}
+        mock_backend = mock.MagicMock(spec=MultiTenantMixin)
+        type(mock_backend).tenant = mock.PropertyMock(side_effect=ValueError)
+        post_params = {
+            'sso-tenant-submit': 'submit',
+            'tenant': 'GitHub',
+            'remember': False
+        }
+        mock_request = mock.MagicMock(method='POST', POST=post_params, backend=mock_backend)  # valid POST request
+        url_backend = 'foo'
+
+        ret = tenant(mock_request, url_backend)
+
+        # Make sure form is bound to POST data
+        mock_tenant_form.assert_called_with(mock_request.POST)
+        mock_tenant_form().is_valid.assert_called()
+        mock_do_auth.assert_not_called()
+        mock_tenant_form().add_error.assert_called_with('tenant', 'Invalid thingy provided.')
+        mock_render.assert_called_with(
+            mock_request,
+            'tethys_portal/accounts/sso_tenant.html',
+            {
+                'form': mock_tenant_form(),
+                'form_title': 'Thingy',
+                'page_title': 'Thingy',
+                'backend': url_backend
+            }
+        )
+        self.assertEqual(mock_render(), ret)
