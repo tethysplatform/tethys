40282311f9ceadc0232570d27847f11f412dd633
ezrajrice <ezra.j.rice@gmail.com>
2021-02-08 15:22:51 +0000

    Changed customized view to be a subclass of Django's built-in PasswordResetView. Updated urls tests. Updated documentation.
    Signed-off-by: ezrajrice <ezra.j.rice@gmail.com>

--- tethys_portal/views/email.py
+++ tethys_portal/views/email.py
@@ -1,43 +1,17 @@
 from django.conf import settings
-from django.contrib.auth.forms import PasswordResetForm
-from django.contrib.auth.tokens import default_token_generator
-from django.contrib.auth.views import PasswordContextMixin
-from django.urls import reverse_lazy
-from django.utils.decorators import method_decorator
-from django.utils.translation import gettext_lazy as _
-from django.views.decorators.csrf import csrf_protect
-from django.views.generic import FormView
+from django.contrib.auth.views import PasswordResetView
 
 
-class TethysPasswordResetView(PasswordContextMixin, FormView):
-    email_template_name = 'registration/password_reset_email.html'
-    extra_email_context = None
-    form_class = PasswordResetForm
+class TethysPasswordResetView(PasswordResetView):
+    # Subclass Django's PasswordResetView and override the 'from_email' form input
+    # This now uses the same settings format as django-mfa2
     from_email_address = settings.EMAIL_HOST_USER
     if '@' not in from_email_address:
         from_email_address = settings.DEFAULT_FROM_EMAIL
+    # This removes the first and last angle brackets if email format is <foo@email.com>
+    # and is for backwards compatibility
+    if from_email_address[0] == '<':
+        from_email_address = from_email_address[1:len(from_email_address)]
+    if from_email_address[len(from_email_address)] == '>':
+        from_email_address = from_email_address[0:len(from_email_address) - 1]
     from_email = "%s <%s>" % (settings.EMAIL_FROM, from_email_address)
-    html_email_template_name = None
-    subject_template_name = 'registration/password_reset_subject.txt'
-    success_url = reverse_lazy('password_reset_done')
-    template_name = 'registration/password_reset_form.html'
-    title = _('Password reset')
-    token_generator = default_token_generator
-
-    @method_decorator(csrf_protect)
-    def dispatch(self, *args, **kwargs):
-        return super().dispatch(*args, **kwargs)
-
-    def form_valid(self, form):
-        opts = {
-            'use_https': self.request.is_secure(),
-            'token_generator': self.token_generator,
-            'from_email': self.from_email,
-            'email_template_name': self.email_template_name,
-            'subject_template_name': self.subject_template_name,
-            'request': self.request,
-            'html_email_template_name': self.html_email_template_name,
-            'extra_email_context': self.extra_email_context,
-        }
-        form.save(**opts)
-        return super().form_valid(form)
