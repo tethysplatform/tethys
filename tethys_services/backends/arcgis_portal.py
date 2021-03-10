2b761a19a9cd28c25cb037340e4d130574cea115
ShawnCrawley-NOAA <shawn.crawley@noaa.gov>
2021-03-10 11:58:43 -0600

    Implements social login for ArcGIS (Online/Portal) (#666)
    * Implements social login for ArcGIS (Online/Portal)
    
    With these changes, ArcGIS Online or an ArcGIS Enterprise Portal can be 
    used to authenticate your Tethys Portal users. The portal_config.yml 
    must be modified to include the proper entries in the 
    AUTHENTICATION_BACKENDS and OAUTH_CONFIGS sections. For ArcGIS Online, 
    the backend is "social_core.backends.arcgis" and the OAUTH_CONFIG 
    settings are SOCIAL_AUTH_ARCGIS_KEY and SOCIAL_AUTH_ARCGIS_SECRET. 
    For ArcGIS Enterprise Portal, the backend is 
    "tethys_services.backends.arcgisportal.ArcGISPortalOAuth2" and the
    OAUTH_CONFIG settings are SOCIAL_AUTH_ARCGIS_PORTAL_URL, 
    SOCIAL_AUTH_ARCGIS_KEY, and SOCIAL_AUTH_ARCGIS_SECRET. The key and 
    secret are obtained from an "Application Item" that must be created
    and registered either on ArcGIS Online or the ArcGIS Enterprise 
    Portal that will authenticate your users.
    
    * fix linter errors and broken test
    
    * Fixes a few more linter errors
    
    * Fixes a broken test
    
    * apply fixes suggested by @swainn
    
    * fix test
    
    * apply fix suggested by @swainn

---
+++ tethys_services/backends/arcgis_portal.py
@@ -0,0 +1,66 @@
+"""
+********************************************************************************
+* Name: arcgis_portal.py
+* Author: Shawn Crawley
+* Created On: March 2, 2021
+* License: BSD 2-Clause
+********************************************************************************
+"""
+from social_core.backends.arcgis import ArcGISOAuth2
+from django.conf import settings
+
+
+class ArcGISPortalOAuth2(ArcGISOAuth2):
+    """
+    ArcGISPortal OAuth2 authentication backend.
+    """
+    name = 'arcgis_portal'
+
+    @property
+    def PORTAL_URL(self):
+        portal_url = ''
+        try:
+            portal_url = settings.OAUTH_CONFIG['SOCIAL_AUTH_ARCGIS_PORTAL_URL']
+            if not portal_url:
+                raise ValueError()
+            if portal_url[-1] == '/':
+                portal_url = portal_url[0:-1]
+        except (AttributeError, KeyError, ValueError):
+            raise ValueError('You must specify the url of your ArcGIS Enterprise Portal via '
+                             'the "SOCIAL_AUTH_ARCGIS_PORTAL_URL" setting in your '
+                             'portal_config.yml file.')
+        return portal_url
+
+    @property
+    def AUTHORIZATION_URL(self):
+        return f'{self.PORTAL_URL}/sharing/rest/oauth2/authorize'
+
+    @property
+    def ACCESS_TOKEN_URL(self):
+        return f'{self.PORTAL_URL}/sharing/rest/oauth2/token'
+
+    def user_data(self, access_token, *args, **kwargs):
+        """Loads user data from service"""
+        return self.get_json(
+            f'{self.PORTAL_URL}/sharing/rest/community/self',
+            params={
+                'token': access_token,
+                'f': 'json'
+            }
+        )
+
+    def get_user_details(self, response):
+        """Return user details from ArcGIS Enterprise Portal account"""
+        name_parts = response['fullName'].split(' ')
+        first_name = name_parts[0]
+        last_name = ''
+        if len(name_parts) > 1:
+            last_name = name_parts[-1]
+
+        return {
+            'username': response['username'],
+            'email': response['email'],
+            'fullname': response['fullName'],
+            'first_name': first_name,
+            'last_name': last_name
+        }
