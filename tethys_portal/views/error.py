991888d225ebda048d2429030bce385e89957d7d
michael <17328135+msouff@users.noreply.github.com>
2021-03-14 01:03:42 -0700

    working on translation: finished tethys_portal

--- tethys_portal/views/error.py
+++ tethys_portal/views/error.py
@@ -8,6 +8,7 @@
 ********************************************************************************
 """
 from django.shortcuts import render
+from django.utils.translation import ugettext_lazy as _
 
 
 def handler_400(request, exception, *args, **kwargs):
@@ -15,8 +16,8 @@ def handler_400(request, exception, *args, **kwargs):
     Handle 400 errors
     """
     context = {'error_code': '400',
-               'error_title': 'Bad Request',
-               'error_message': "Sorry, but we can't process your request. Try something different.",
+               'error_title': _('Bad Request'),
+               'error_message': _("Sorry, but we can't process your request. Try something different."),
                'error_image': '/static/tethys_portal/images/error_500.png'}
     return render(request, 'tethys_portal/error.html', context, status=400)
 
@@ -25,13 +26,13 @@ def handler_403(request, exception, *args, **kwargs):
     """
     Handle 403 errors
     """
-    error_message = "Permission Denied"
+    error_message = _("Permission Denied")
 
     if exception:
         error_message = str(exception)
 
     context = {'error_code': '403',
-               'error_title': 'Sorry, you are unable to access this page right now.',
+               'error_title': _('Sorry, you are unable to access this page right now.'),
                'error_message': error_message,
                'error_image': '/static/tethys_portal/images/data.png'}
     return render(request, 'tethys_portal/403error.html', context, status=403)
@@ -43,10 +44,10 @@ def handler_404(request, exception, error_message=None, *args, **kwargs):
     """
 
     if not error_message:
-        error_message = "We are unable to find the page you requested. Please, check the address and try again."
+        error_message = _("We are unable to find the page you requested. Please, check the address and try again.")
 
     context = {'error_code': '404',
-               'error_title': 'Page Not Found',
+               'error_title': _('Page Not Found'),
                'error_message': error_message,
                'error_image': '/static/tethys_portal/images/error_404.png'}
     return render(request, 'tethys_portal/error.html', context, status=404)
@@ -57,7 +58,7 @@ def handler_500(request):
     Handle 500 errors
     """
     context = {'error_code': '500',
-               'error_title': 'Internal Server Error',
-               'error_message': "We're sorry, but we seem to have a problem. Please, come back later and try again.",
+               'error_title': _('Internal Server Error'),
+               'error_message': _("We're sorry, but we seem to have a problem. Please, come back later and try again."),
                'error_image': '/static/tethys_portal/images/error_500.png'}
     return render(request, 'tethys_portal/error.html', context, status=500)
