a711891a0ef836ae2a706d1599738830151e12e4
nswain <nswain@aquaveo.com>
2021-07-27 14:20:46 -0600

    Remove color ramps from map_layout.py (it's in map_layout_mixin now).

--- tethys_layouts/views/map_layout.py
+++ tethys_layouts/views/map_layout.py
@@ -29,35 +29,6 @@ from tethys_sdk.gizmos import ToggleSwitch, CesiumMapView, MapView, MVView, Slid
 
 log = logging.getLogger(f'tethys.{__name__}')
 
-_COLOR_RAMPS = {
-    "Default": ["#fff100", "#ff8c00", "#e81123", "#ec008c", "#68217a", "#00188f", "#00bcf2", "#00b294", "#009e49",
-                "#bad80a"],
-    "Blue": ["#f7fbff", "#deebf7", "#c6dbef", "#9ecae1", "#6baed6", "#4292c6", "#2171b5", "#08519c", "#083582",
-             "#022259"],
-    "Blue and Red": ["#a50026", "#d73027", "#f46d43", "#fdae61", "#fee090", "#e0f3f8", "#abd9e9", "#74add1", "#4575b4",
-                     "#313695"],
-    "Elevated": ["#96D257", "#278C39", "#2A7B45", "#829C41", "#DBB82E", "#AE4818", "#842511", "#61370F", "#806346",
-                 "#C2C2C2", "#FFFFFF"],
-    "Flower Field": ["#e60049", "#0bb4ff", "#50e991", "#e6d800", "#9b19f5", "#ffa300", "#dc0ab4", "#b3d4ff",
-                     "#00bfa0", "#f0cccc"],
-    "Galaxy Berries": ["#0040bf", "#a3cc52", "#b9a087", "#a01fcc", "#5bb698", "#5e851e", "#d1943f",
-                       "#96aedc", "#629ed9", "#8a64b3"],
-    "Heat Map": ["#90a1be", "#a761aa", "#af4980", "#b83055", "#c80000", "#d33300", "#de6600", "#e99900", "#f4cc00",
-                 "#ffff00"],
-    "Olive Harmony": ["#437a75", "#d9d78c", "#bf7860", "#72231f", "#afbfa2", "#5a9bc8", "#89a6a6", "#99905c",
-                      "#414b8c", "#a664a0"],
-    "Mother Earth": ["#a03500", "#d9b400", "#3264c8", "#72b38e", "#986ba1", "#b9a087", "#4c91bf", "#a5d236",
-                     "#96aedc", "#ad8516"],
-    "Rainbow": ["#fff100", "#ff8c00", "#e81123", "#ec008c", "#68217a", "#00188f", "#00bcf2", "#00b294", "#009e49",
-                "#bad80a"],
-    "Rainforest Frogs": ["#dc4b00", "#3c6ccc", "#d9dc00", "#91d900", "#986ba1", "#d99f00", "#4db478",
-                         "#4cafdc", "#96aedc", "#d7a799"],
-    "Retro FLow": ["#007fd9", "#443dbf", "#881fc5", "#bf00bf", "#d43f70", "#d9874c", "#b6a135", "#adbf27",
-                   "#c4dc66", "#ebe498"],
-    "Sunset Fade": ["#b30000", "#7c1158", "#4421af", "#1a53ff", "#0d88e6", "#00b7c7", "#5ad45a", "#8be04e",
-                    "#c5d96d", "#ebdc78"],
-}
-
 
 class MapLayout(TethysLayout, MapLayoutMixin):
     """
