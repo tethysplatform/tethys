ae77e2b205dff0ca435e7bc1ed6c36fe678ec434
Nathan Swain <nswain@aquaveo.com>
2021-12-11 13:13:12 -0700

    Added better way to set default values for site settings.

--- tethys_config/context_processors.py
+++ tethys_config/context_processors.py
@@ -19,6 +19,25 @@ def tethys_global_settings_context(request):
 
     # Get settings
     site_globals = Setting.as_dict()
+    
+    # Set default settings
+    if not site_globals.get('primary_color'):
+        site_globals['primary_color'] =  '#0a62a9'
+
+    if not site_globals.get('secondary_color'):
+        site_globals['secondary_color'] =  '#a2d6f9'
+
+    if not site_globals.get('primary_text_color'):
+        site_globals['primary_text_color'] =  '#ffffff'
+
+    if not site_globals.get('primary_text_hover_color'):
+        site_globals['primary_text_hover_color'] =  '#eeeeee'
+
+    if not site_globals.get('secondary_text_color'):
+        site_globals['secondary_text_color'] =  '#212529'
+
+    if not site_globals.get('secondary_text_hover_color'):
+        site_globals['secondary_text_hover_color'] =  '#aaaaaa'
 
     # Get terms and conditions
     site_globals.update({'documents': TermsAndConditions.get_active_terms_list()})
