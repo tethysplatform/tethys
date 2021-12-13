bcd00ceeae3e76c65a8835a21b096d03c21b7f76
Nathan Swain <nswain@aquaveo.com>
2021-12-13 15:50:48 -0700

    Select input gizmo upgraded to bootstrap5 and same verison of select2 as portal. Removed vendored versions of select2.

--- tethys_apps/context_processors.py
+++ tethys_apps/context_processors.py
@@ -8,6 +8,7 @@
 ********************************************************************************
 """
 from tethys_apps.utilities import get_active_app
+from tethys_apps.dependencies import dependencies
 
 
 def tethys_apps_context(request):
@@ -42,25 +43,6 @@ def tethys_apps_context(request):
                 context['tethys_app']['enable_feedback'] = app.enable_feedback
 
     # Dependency Versions
-    context.update({
-        'tethys': {
-            'jquery': {
-                'version': '3.6.0',
-                'integrity': 'sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=',
-
-            },
-            'bootstrap': {
-                'version': '5.1.3',
-                'css_integrity': 'sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3',
-                'js_integrity': 'sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p'
-            },
-            'bootstrap_icons': {
-                'version': '1.7.1',
-            },
-            'select2': {
-                'version': '4.1.0',
-            }
-        }
-    })
+    context.update({'tethys': dependencies})
 
     return context
---
+++ tethys_apps/dependencies.py
@@ -0,0 +1,18 @@
+dependencies = {
+    'jquery': {
+        'version': '3.6.0',
+        'integrity': 'sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=',
+    },
+    'bootstrap': {
+        'version': '5.1.3',
+        'css_integrity': 'sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3',
+        'js_integrity': 'sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p'
+    },
+    'bootstrap_icons': {
+        'version': '1.7.1',
+    },
+    'select2': {
+        'version': '4.1.0-rc.0',
+    }
+}
+
