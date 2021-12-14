2e7405ab5ac11f2e201c8cc6f500fc4c155466cf
Nathan Swain <nswain@aquaveo.com>
2021-12-14 16:28:18 -0700

    Upgraded plotting gizmos. Migrated dependencies to use CDNs and removed vendored packages.

--- tethys_apps/dependencies.py
+++ tethys_apps/dependencies.py
@@ -1,7 +1,7 @@
 dependencies = {
     'jquery': {
         'version': '3.6.0',
-        'integrity': 'sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=',
+        'integrity': 'sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4='
     },
     'bootstrap': {
         'version': '5.1.3',
@@ -9,16 +9,25 @@ dependencies = {
         'js_integrity': 'sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p'
     },
     'bootstrap_icons': {
-        'version': '1.7.1',
+        'version': '1.7.1'
     },
     'select2': {
-        'version': '4.1.0-rc.0',
+        'version': '4.1.0-rc.0'
     },
     'datatables': {
-        'version': '1.11.3',
+        'version': '1.11.3'
     },
     'bootstrap-switch': {
-        'version': '4.0.0-alpha.1',
+        'version': '4.0.0-alpha.1'
+    },
+    'd3': {
+        'version': '3.5.5'  # Current, 7.2.1, breaks plot view
+    },
+    'd3-tip': {
+        'version': '0.9.1'  # 0.6.3
+    },
+    'highcharts': {
+        'version': '9.3.2'
     }
 }
 
