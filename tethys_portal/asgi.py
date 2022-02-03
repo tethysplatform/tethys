da4757307786cabca2e687331213c0d735acc389
sdc50 <scott.d.christensen@erdc.dren.mil>
2022-02-03 13:58:20 -0700

    Fix URL routing for http handler URLS (#766)
    * fix URL routing for http handler URLS
    
    * replace deprecated `url` function with `re_path`
    
    * fix tests
    
    * fix inadvertently changed docstrings

--- tethys_portal/asgi.py
+++ tethys_portal/asgi.py
@@ -6,12 +6,18 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from tethys_apps.urls import app_websocket_urls
+from django.urls import re_path
+from tethys_apps.urls import app_websocket_urls, http_handler_patterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tethys_portal.settings")
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': AuthMiddlewareStack(
+        URLRouter([
+            *http_handler_patterns,
+            re_path(r'', get_asgi_application()),
+        ])
+    ),
     'websocket': AuthMiddlewareStack(
         URLRouter(
             app_websocket_urls
