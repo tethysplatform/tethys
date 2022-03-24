4ae26a869722024c5daa0ceb93762712e974daa9
Michael Souffront <17328135+msouff@users.noreply.github.com>
2022-03-24 16:24:00 -0600

    Comprehensive Tethys Manage Command (#783)
    * added get manage.py path command
    
    * added helper function to get django commands
    
    * added django commands
    
    * handle django options
    
    * tests for django manage commands
    
    * linting
    
    * added unknown arg support to tethys commands
    
    * added missing test for helper function
    
    * addressed suggestions; got django command commands from installed apps working
    
    * tethys command unknown_args overhaul
    
    * tethys command unknown_args overhaul
    
    * tethys test --file fix
    
    * addressed last comments
    
    * fixed tests issues with load_apps

--- tethys_cli/manage_commands.py
+++ tethys_cli/manage_commands.py
@@ -8,22 +8,27 @@
 ********************************************************************************
 """
 
+from django.core.management import get_commands
+
 from tethys_cli.cli_helpers import get_manage_path, run_process
 
 MANAGE_START = 'start'
 MANAGE_COLLECTSTATIC = 'collectstatic'
 MANAGE_COLLECTWORKSPACES = 'collectworkspaces'
 MANAGE_COLLECT = 'collectall'
-MANAGE_CREATESUPERUSER = 'createsuperuser'
-MANAGE_SHELL = 'shell'
+MANAGE_GET_PATH = 'path'
 
 
 def add_manage_parser(subparsers):
-    # Setup start server command
+    # sub-command choices
+    TETHYS_COMMANDS = [MANAGE_START, MANAGE_COLLECTSTATIC, MANAGE_COLLECTWORKSPACES, MANAGE_COLLECT, MANAGE_GET_PATH]
+    DJANGO_COMMANDS = [i for i in sorted(list(get_commands().keys())) if i not in TETHYS_COMMANDS]
     manage_parser = subparsers.add_parser('manage', help='Management commands for Tethys Platform.')
-    manage_parser.add_argument('command', help='Management command to run.',
-                               choices=[MANAGE_START, MANAGE_COLLECTSTATIC, MANAGE_COLLECTWORKSPACES,
-                                        MANAGE_COLLECT, MANAGE_CREATESUPERUSER])
+    manage_parser.add_argument(
+        'command', help='Management command to run.'
+        f'\n{{{", ".join(e for e in [*TETHYS_COMMANDS, *DJANGO_COMMANDS])}}}'
+        '\n Also accepts any valid manage.py command from Django (see --django-help).'
+    )
     manage_parser.add_argument('-m', '--manage', help='Absolute path to manage.py for Tethys Platform installation.')
     manage_parser.add_argument('-p', '--port', type=str,
                                help='Host and/or port on which to bind the development server.')
@@ -35,13 +40,19 @@ def add_manage_parser(subparsers):
     manage_parser.add_argument('-l', '--link', required=False, action='store_true',
                                help='Only used with collectstatic command. Link static directory to STATIC_ROOT '
                                     'instead of copying it. Not recommended.')
-    manage_parser.set_defaults(func=manage_command)
+    manage_parser.add_argument('--django-help', action='store_true',
+                               help='Display the help for specific commands coming from Django. '
+                               'E.g. "tethys manage <django command> --django-help"')
+    manage_parser.set_defaults(func=manage_command, parsing_method='accepts_unknown_args')
 
 
-def manage_command(args):
+def manage_command(args, unknown_args=None):
     """
     Management commands.
     """
+    if not unknown_args:
+        unknown_args = []
+
     # Get the path to manage.py
     manage_path = get_manage_path(args)
 
@@ -93,8 +104,14 @@ def manage_command(args):
         # Run collectworkspaces command
         primary_process = ['python', manage_path, 'collectworkspaces']
 
-    elif args.command == MANAGE_CREATESUPERUSER:
-        primary_process = ['python', manage_path, 'createsuperuser']
+    elif args.command == MANAGE_GET_PATH:
+        print(manage_path)
+
+    else:
+        if args.django_help:
+            primary_process = ['python', manage_path, args.command, '--help']
+        else:
+            primary_process = ['python', manage_path, args.command, *unknown_args]
 
     if primary_process:
         run_process(primary_process)
