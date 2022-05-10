ec8972b2b52ff2cb45d93bbb1ac07257da679a8e
Nathan Swain <nswain@aquaveo.com>
2022-05-10 12:03:56 -0600

    Add layers from layer groups automatically for MapLayout

--- tethys_layouts/views/map_layout.py
+++ tethys_layouts/views/map_layout.py
@@ -296,6 +296,13 @@ class MapLayout(TethysLayout, MapLayoutMixin):
         # Add layers to the Map
         log.debug('Composing layers...')
         layer_groups = self.compose_layers(request=request, map_view=map_view, *args, **kwargs)
+        
+        # Add layers to map view if not already added
+        for layer_group in layer_groups:
+            for layer in layer_group['layers']:
+                if layer not in map_view.layers:
+                    map_view.layers.append(layer)
+
         log.debug(f'Number of Layers: {len(map_view.layers)}')
 
         # Check if we need to create a blank custom layer group
