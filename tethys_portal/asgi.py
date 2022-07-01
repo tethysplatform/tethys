fa01e8fabf8681d5ad7b05e3ec3dd18349a3d60c
Nathan Swain <swainn@users.noreply.github.com>
2022-07-01 13:32:16 -0600

    Tethys4 Docker Updates (#828)
    * Fix issue with Dockerfile env variables
    
    * Updates to the Docker build for Tethys 4
    Using micromamba as base image
    
    * Add micromamba option for asgi_service config for docker generation
    
    * Fix for hard-coded node_modules static dir
    Fix bootstrap jsdelivr shas
    Add TETHYS_PUBLIC_HOST env
    
    * Add test for changes in settings.py

--- tethys_portal/asgi.py
+++ tethys_portal/asgi.py
@@ -7,20 +7,29 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from django.urls import re_path
-from tethys_apps.urls import app_websocket_urls, http_handler_patterns
+
+
+def build_application(asgi_app):
+    from tethys_apps.urls import app_websocket_urls, http_handler_patterns
+
+    application = ProtocolTypeRouter({
+        'http': AuthMiddlewareStack(
+            URLRouter([
+                *http_handler_patterns,
+                re_path(r'', asgi_app),
+            ])
+        ),
+        'websocket': AuthMiddlewareStack(
+            URLRouter(
+                app_websocket_urls
+            )
+        )
+    })
+    return application
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tethys_portal.settings")
 
-application = ProtocolTypeRouter({
-    'http': AuthMiddlewareStack(
-        URLRouter([
-            *http_handler_patterns,
-            re_path(r'', get_asgi_application()),
-        ])
-    ),
-    'websocket': AuthMiddlewareStack(
-        URLRouter(
-            app_websocket_urls
-        )
-    )
-})
+# This needs to be called before any model imports
+asgi_app = get_asgi_application()
+application = build_application(asgi_app)
