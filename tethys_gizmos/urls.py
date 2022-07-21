0661c06285f3b57208edc9a41b915cce6a1b1567
sdc50 <scott.d.christensen@erdc.dren.mil>
2022-07-21 15:38:28 -0600

    Custom jobs table actions (#837)
    * Custom job actions
    
    * add confirmation to custom actions
    
    additional features for custom actions and gizmo showcase jobs table
    
    refactor actions to all use custom actions
    
    bugfix view logs action
    
    jobs table bugfixes
    add modals to gizmo_dependencies template tag
    
    allow getting multi-level extended properties for jobs table
    
    Custom Action Url (#6)
    
    * added ability to provide a URL to load content into custom job action modal
    Co-authored-by: Dong, Quyen T <quyen.t.dong@erdc.dren.mil>
    
    Fix Jobs Table Custom Modal
    
    fix delayed loading for jobs table
    
    * bootstrap 5 fixes for the jobs table
    
    * stickler
    
    * tests complete
    
    * stickler
    
    * docs
    
    * stickler
    
    * Update tethys_gizmos/static/tethys_gizmos/js/jobs_table.js
    
    Co-authored-by: Nathan Swain <swainn@users.noreply.github.com>
    
    * suggestions
    
    Co-authored-by: Nathan Swain <swainn@users.noreply.github.com>

--- tethys_gizmos/urls.py
+++ tethys_gizmos/urls.py
@@ -11,17 +11,17 @@ from django.urls import re_path, include
 from tethys_gizmos.views.gizmos import jobs_table as jobs_table_views
 
 ajax_urls = [
-    re_path(r'^(?P<job_id>[\d.@+-]+)/terminate', jobs_table_views.terminate, name='terminate_job'),
-    re_path(r'^(?P<job_id>[\d.@+-]+)/delete$', jobs_table_views.delete, name='delete_job'),
-    re_path(r'^(?P<job_id>[\d.@+-]+)/resubmit$', jobs_table_views.resubmit, name='resubmit_job'),
-    re_path(r'^(?P<job_id>[\d.@+-]+)/show-log', jobs_table_views.show_log, name='show_log'),
+    re_path(r'^(?P<job_id>[\d.@+-]+)/action/delete$', jobs_table_views.delete, name='delete_job'),
+    re_path(r'^(?P<job_id>[\d.@+-]+)/action/resubmit$', jobs_table_views.resubmit, name='resubmit_job'),
+    re_path(r'^(?P<job_id>[\d.@+-]+)/action/show-log', jobs_table_views.show_log, name='show_log'),
     re_path(r'^(?P<job_id>[\d.@+-]+)/log-content/(?P<key1>[\w+_-]+)(?:/(?P<key2>.*))?',
             jobs_table_views.get_log_content, name='log_content'),
-    re_path(r'^(?P<job_id>[\d.@+-]+)/execute', jobs_table_views.execute, name='execute_job'),
     re_path(r'^(?P<job_id>[\d.@+-]+)/update-row', jobs_table_views.update_row, name='update_job_row'),
     re_path(r'^(?P<job_id>[\d.@+-]+)/update-workflow-nodes-row', jobs_table_views.update_workflow_nodes_row,
             name='update_workflow_nodes_row'),
     re_path(r'^(?P<job_id>[\d.@+-]+)/(?P<type>[\w-]+)/insert-bokeh-row', jobs_table_views.bokeh_row, name='bokeh_row'),
+    re_path(r'^(?P<job_id>[\d.@+-]+)/action/(?P<action>[\w+-]+)', jobs_table_views.perform_action,
+            name='custom_action'),
 ]
 
 urlpatterns = [
