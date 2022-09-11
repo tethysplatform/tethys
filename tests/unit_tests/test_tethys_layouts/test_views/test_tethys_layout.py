9c6457d41b19bd431ca9a7744e649ea7cf10665f
Nathan Swain <nswain@aquaveo.com>
2022-09-10 23:11:26 -0600

    Full coverage for TethysLayout

--- tests/unit_tests/test_tethys_layouts/test_views/test_tethys_layout.py
+++ tests/unit_tests/test_tethys_layouts/test_views/test_tethys_layout.py
@@ -1,12 +1,19 @@
 import unittest
 from unittest import mock
+from django.http import HttpResponse
+from django.test import RequestFactory, TestCase, override_settings
 
 from tethys_layouts.views.tethys_layout import TethysLayout
 
 
-class TestTethysLayout(unittest.TestCase):
+class MethodLayout(TethysLayout):
+    def some_method(self, request, *args, **kwargs):
+        return 'some-response'
+
+class TestTethysLayout(TestCase):
     def setUp(self):
         self.inst = TethysLayout()
+        self.factory = RequestFactory()
 
     def tearDown(self):
         pass
@@ -21,46 +28,98 @@ class TestTethysLayout(unittest.TestCase):
         self.assertEqual('', ret.layout_title)
         self.assertEqual('', ret.layout_subtitle)
 
-    def test_get(self):
-        pass
+    @mock.patch('tethys_layouts.views.tethys_layout.render')
+    @override_settings(
+        DEBUG=False,
+        ENABLE_OPEN_PORTAL=True,
+    )
+    def test_get(self, mock_render):
+        class CustomLayout(TethysLayout):
+            base_template = 'bar.html'
+            template_name = 'foo.html'
+            back_url = '/a/back/url'
+            layout_title = 'Foo'
+            layout_subtitle = 'Bar'
 
-    def test_get_http_response(self):
-        pass
+            def get_context(self, request, context, *args, **kwargs):
+                context['bar'] = 'baz'
+                return context
 
-    def test_get_method(self):
-        pass
+            def get_permissions(self, request, permissions, *args, **kwargs):
+                permissions['can_do_the_thing'] = True
+                return permissions
 
-    def test_post(self):
-        pass
+        request = self.factory.get('/some/endpoint')
+        mock_render.return_value = 'render-response'
+        controller = CustomLayout.as_controller()
+        ret = controller(request)
+        self.assertEqual(ret, 'render-response')
+        expected_context = {
+            'base_template': 'bar.html',
+            'back_url': '/a/back/url',
+            'is_in_debug': False,
+            'nav_title': 'Foo',
+            'nav_subtitle': 'Bar',
+            'open_portal_mode': True,
+            'bar': 'baz',
+            'can_do_the_thing': True,
+        }
+        mock_render.assert_called_with(
+            request,
+            'foo.html',
+            expected_context
+        )
 
-    def test_post_method(self):
-        pass
+    def test_get_on_get_http_response(self):
+        class OnGetLayout(TethysLayout):
+            def on_get(self, request, *args, **kwargs):
+                return HttpResponse('on-get-response')
+
+        request = self.factory.get('/some/endpoint')
+        controller = OnGetLayout.as_controller()
+        ret = controller(request)
+        self.assertEqual(ret.content, b'on-get-response')
+
+    def test_get_with_method(self):
+        request = self.factory.get('/some/endpoint', data={'method': 'some-method'})
+        controller = MethodLayout().as_controller()
+        ret = controller(request)
+        self.assertEqual(ret, 'some-response')
+
+    def test_post_no_method(self):
+        request = self.factory.post('/some/endpoint')
+        controller = MethodLayout().as_controller()
+        ret = controller(request)
+        self.assertEqual(ret.status_code, 404)
+
+    def test_post_with_method(self):
+        request = self.factory.post('/some/endpoint', data={'method': 'some-method'})
+        controller = MethodLayout().as_controller()
+        ret = controller(request)
+        self.assertEqual(ret, 'some-response')
 
     def test_request_to_method_get_with_dashes(self):
-        class CustomLayout(TethysLayout):
-            def some_method(self):
-                pass
-        mock_request = mock.MagicMock(method='GET', GET=dict(method='some-method'))
-        inst = CustomLayout()
-        ret = inst.request_to_method(mock_request)
-        self.assertEqual(inst.some_method, ret)
+        request = self.factory.get('/some/endpoint', data={'method': 'some-method'})
+        inst = MethodLayout()
+        ret = inst.request_to_method(request)
+        self.assertEqual(ret, inst.some_method)
 
     def test_request_to_method_post_with_underscores(self):
-        class CustomLayout(TethysLayout):
-            def some_method(self):
-                pass
-        mock_request = mock.MagicMock(method='POST', POST=dict(method='some_method'))
-        inst = CustomLayout()
-        ret = inst.request_to_method(mock_request)
-        self.assertEqual(inst.some_method, ret)
+        request = self.factory.post('/some/endpoint', data={'method': 'some_method'})
+        inst = MethodLayout()
+        ret = inst.request_to_method(request)
+        self.assertEqual(ret, inst.some_method)
+
+    def test_request_to_method_put(self):
+        request = self.factory.put('/some/endpoint', data={'method': 'some-method'})
+        inst = MethodLayout()
+        ret = inst.request_to_method(request)
+        self.assertIsNone(ret)
 
     def test_request_to_method_no_request_method(self):
-        class CustomLayout(TethysLayout):
-            def some_method(self):
-                pass
-        mock_request = mock.MagicMock(method='GET', GET=dict(method='not-a-method'))
-        inst = CustomLayout()
-        ret = inst.request_to_method(mock_request)
+        request = self.factory.get('/some/endpoint', data={'method': 'not-a-method'})
+        inst = MethodLayout()
+        ret = inst.request_to_method(request)
         self.assertIsNone(ret)
 
     def test_on_get(self):
