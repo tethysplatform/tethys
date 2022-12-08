tethysplatform/tethys

commit 867996fbc867b0de88ebccc018c860ec0e0c5d92

Remove array fields (#890)
* fix install settings query
initial migrations

* remove array field migrations
fix app setting database syncing

* black

* more black

* Update app testing documentation

fixes #889
fixes #887
addresses #888

* Fix docs

* Fix logging docs

tethys_compute/migrations/0002_alter_condorpyjob__remote_input_files.py
@@ -1,0 +1,50 @@
+# Generated by Django 3.2.16 on 2022-12-02 18:07
+
+import json
+from django.db import migrations, models
+
+remote_input_files = {}
+
+
+def save_remote_files_as_json(apps, schema_editor):
+    CondorPyJob = apps.get_model("tethys_compute", "CondorPyJob")
+    db_alias = schema_editor.connection.alias
+    for job in CondorPyJob.objects.using(db_alias).all():
+        remote_input_files[job.id] = json.dumps(job._remote_input_files)
+
+
+def load_saved_remote_files(apps, schema_editor):
+    CondorPyJob = apps.get_model("tethys_compute", "CondorPyJob")
+    db_alias = schema_editor.connection.alias
+    for job_id, files in remote_input_files.items():
+        job = CondorPyJob.objects.using(db_alias).get(id=job_id)
+        job._remote_input_files = files
+        job.save()
+
+
+def save_remote_files_as_list(apps, schema_editor):
+    CondorPyJob = apps.get_model("tethys_compute", "CondorPyJob")
+    db_alias = schema_editor.connection.alias
+    for job in CondorPyJob.objects.using(db_alias).all():
+        remote_input_files[job.id] = json.loads(job._remote_input_files)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("tethys_compute", "0001_initial_40"),
+    ]
+
+    operations = [
+        migrations.RunPython(save_remote_files_as_json, load_saved_remote_files),
+        migrations.RemoveField(
+            model_name="condorpyjob",
+            name="_remote_input_files",
+        ),
+        migrations.AddField(
+            model_name="condorpyjob",
+            name="_remote_input_files",
+            field=models.JSONField(blank=True, default=list, null=True),
+        ),
+        migrations.RunPython(load_saved_remote_files, save_remote_files_as_list),
+    ]

docs/installation/resources/example-portal-config.yml
@@ -51,18 +51,22 @@ settings:
       PORT:  5436
       DIR: psql
 
-  LOGGING_CONFIG:
-    TETHYS_LOGGING:
-      handlers:
-        - console_verbose
-      level: INFO
-    TETHYS_APPS_LOGGING:
-      handlers:
-        - console_verbose
-      level: INFO
-    LOGGING_FORMATTERS: {}
-    LOGGING_HANDLERS: {}
-    LOGGERS: {}
+  # LOGGING:
+  #   formatters: {}
+  #   handlers: {}
+  #   loggers:
+  #     django:
+  #       handlers:
+  #       - console_simple
+  #       level: WARNING
+  #     tethys:
+  #       handlers:
+  #       - console_verbose
+  #       level: INFO
+  #     tethysapp:
+  #       handlers:
+  #       - console_verbose
+  #       level: INFO
 
   #  INSTALLED_APPS_OVERRIDE: []
   INSTALLED_APPS: []

scripts/generate_portal_config_tables.py
@@ -51,9 +51,9 @@ d = {
                 "handlers": "override the default handlers for the ``tethys`` logger. Defaults to ``['console_verbose']``.",
                 "level": "override the default level for the ``tethys`` logger. Defaults to ``'INFO'``.",
             },
-            "tethys.apps": {
-                "handlers": "override the default handlers for the ``tethys.apps`` logger. Defaults to ``['console_verbose']``.",
-                "level": "override the default level for the ``tethys.apps`` logger. Defaults to ``'INFO'``.",
+            "tethysapp": {
+                "handlers": "override the default handlers for the ``tethysapp`` logger. Defaults to ``['console_verbose']``.",
+                "level": "override the default level for the ``tethysapp`` logger. Defaults to ``'INFO'``.",
             },
         },
     },

tests/unit_tests/test_tethys_apps/test_base/test_app_base.py
@@ -3,7 +3,7 @@ import unittest
 from unittest import mock
 import uuid
 from django.db.utils import ProgrammingError
-from django.test import RequestFactory
+from django.test import RequestFactory, override_settings
 from django.core.exceptions import ValidationError, ObjectDoesNotExist
 
 from tethys_apps.exceptions import (
@@ -1357,7 +1357,7 @@ class TestTethysAppBase(unittest.TestCase):
         self.assertTrue(mock_ta().save.call_count == 2)
 
         # Check if add_settings is called 6 times
-        self.assertTrue(mock_ta().add_settings.call_count == 6)
+        self.assertTrue(mock_ta().sync_settings.call_count == 6)
 
     @mock.patch("django.conf.settings")
     @mock.patch("tethys_apps.models.TethysApp")
@@ -1374,7 +1374,7 @@ class TestTethysAppBase(unittest.TestCase):
         self.assertTrue(mock_app.save.call_count == 2)
 
         # Check if add_settings is called 6 times
-        self.assertTrue(mock_app.add_settings.call_count == 6)
+        self.assertTrue(mock_app.sync_settings.call_count == 6)
 
     @mock.patch("django.conf.settings")
     @mock.patch("tethys_apps.models.TethysApp")
@@ -1418,6 +1418,30 @@ class TestTethysAppBase(unittest.TestCase):
 
         # Check if delete is called
         mock_ta.objects.filter().delete.assert_called()
+
+    @override_settings(DEBUG=True)
+    @mock.patch("tethys_cli.cli_colors.write_error")
+    @mock.patch("tethys_apps.base.app_base.input")
+    @mock.patch("tethys_apps.models.TethysApp")
+    def test_remove_from_db_prompt_yes(self, mock_ta, mock_input, mock_write_error):
+        mock_input.side_effect = "y"
+        self.app.remove_from_db()
+
+        # Check if delete is called
+        mock_ta.objects.filter().delete.assert_called()
+        mock_write_error.assert_called_once()
+
+    @override_settings(DEBUG=True)
+    @mock.patch("tethys_cli.cli_colors.write_error")
+    @mock.patch("tethys_apps.base.app_base.input")
+    @mock.patch("tethys_apps.models.TethysApp")
+    def test_remove_from_db_prompt_no(self, mock_ta, mock_input, mock_write_error):
+        mock_input.side_effect = "n"
+        self.app.remove_from_db()
+
+        # Check if delete is called
+        mock_ta.objects.filter().delete.assert_not_called()
+        mock_write_error.assert_called_once()
 
     @mock.patch("tethys_apps.base.app_base.tethys_log")
     @mock.patch("tethys_apps.models.TethysApp")

tests/unit_tests/test_tethys_apps/test_models/test_TethysApp.py
@@ -90,6 +90,27 @@ class TethysAppTests(TethysTestCase):
         settings = app.settings_set.filter(name="new_setting")
         self.assertEqual(1, len(settings))
 
+    def test_sync_settings(self):
+        app = TethysApp.objects.get(package="test_app")
+
+        new_setting = TethysAppSetting(name="new_setting", required=False)
+        existing_setting = TethysAppSetting(name="existing_setting", required=False)
+        obsolete_setting = TethysAppSetting(
+            name="obsolete_setting", tethys_app=app, required=False
+        )
+        obsolete_setting.save()  # needs to be saved to it can be deleted.
+
+        self.test_app.sync_settings(
+            [existing_setting, new_setting], [existing_setting, obsolete_setting]
+        )
+
+        settings = [
+            setting for setting in app.settings if setting.__class__ == TethysAppSetting
+        ]
+        self.assertEqual(2, len(settings))
+        self.assertIn(new_setting, settings)
+        self.assertNotIn(obsolete_setting, settings)
+
     def test_settings_prop(self):
         ret = self.test_app.settings
         self.assertEqual(15, len(ret))

tests/unit_tests/test_tethys_cli/test_install_commands.py
@@ -407,8 +407,14 @@ class TestInstallServicesCommands(TestCase):
     @mock.patch("tethys_cli.install_commands.find_and_link")
     @mock.patch("tethys_cli.cli_colors.pretty_output")
     @mock.patch("tethys_apps.models.CustomSetting")
+    @mock.patch("tethys_apps.models.TethysApp")
     def test_configure_services_from_file(
-        self, mock_CustomSetting, mock_pretty_output, mock_find_and_link, mock_gas
+        self,
+        mock_TethysApp,
+        mock_CustomSetting,
+        mock_pretty_output,
+        mock_find_and_link,
+        mock_gas,
     ):
         app_name = "foo"
         invalid_custom_setting_name = "custom_setting_name"
@@ -505,12 +511,14 @@ class TestInstallServicesCommands(TestCase):
             app_name,
             mock_persistent_database_setting,
         )
+        mock_TethysApp.objects.get.assert_called_once()
 
     @mock.patch("tethys_cli.install_commands.get_app_settings")
     @mock.patch("tethys_cli.install_commands.find_and_link")
     @mock.patch("tethys_cli.cli_colors.pretty_output")
+    @mock.patch("tethys_apps.models.TethysApp")
     def test_configure_services_from_file_no_settings_for_app(
-        self, mock_pretty_output, mock_find_and_link, mock_gas
+        self, mock_TethysApp, mock_pretty_output, mock_find_and_link, mock_gas
     ):
         app_name = "foo"
         persistent_setting_name = "persistent_setting_name"
@@ -532,6 +540,7 @@ class TestInstallServicesCommands(TestCase):
             po_call_args[0][0][0],
         )
         mock_find_and_link.assert_not_called()
+        mock_TethysApp.objects.get.assert_called_once()
 
     @mock.patch("tethys_cli.cli_colors.pretty_output")
     def test_run_portal_install_path_none(self, _):

tethys_apps/base/app_base.py
@@ -10,6 +10,7 @@ import logging
 import traceback
 import uuid
 
+from django.conf import settings
 from django.db.utils import ProgrammingError
 from django.core.exceptions import ValidationError, ObjectDoesNotExist
 from django.urls import re_path
@@ -1634,6 +1635,33 @@ class TethysAppBase(TethysBase):
         ps_database_setting.persistent_store_database_exists()
         return True
 
+    def sync_all_settings(self, db_app):
+        # custom settings
+        db_app.sync_settings(self.custom_settings(), db_app.custom_settings)
+        # dataset services settings
+        db_app.sync_settings(
+            self.dataset_service_settings(), db_app.dataset_service_settings
+        )
+        # spatial dataset services settings
+        db_app.sync_settings(
+            self.spatial_dataset_service_settings(),
+            db_app.spatial_dataset_service_settings,
+        )
+        # wps settings
+        db_app.sync_settings(
+            self.web_processing_service_settings(), db_app.wps_services_settings
+        )
+        # persistent store settings
+        db_app.sync_settings(
+            self.persistent_store_settings(),
+            db_app.persistent_store_connection_settings
+            | db_app.persistent_store_database_settings,
+        )
+        # scheduler settings
+        db_app.sync_settings(self.scheduler_settings(), db_app.scheduler_settings)
+
+        db_app.save()
+
     def sync_with_tethys_db(self):
         """
         Sync installed apps with database.
@@ -1664,20 +1692,7 @@ class TethysAppBase(TethysBase):
                 )
                 db_app.save()
 
-                # custom settings
-                db_app.add_settings(self.custom_settings())
-                # dataset services settings
-                db_app.add_settings(self.dataset_service_settings())
-                # spatial dataset services settings
-                db_app.add_settings(self.spatial_dataset_service_settings())
-                # wps settings
-                db_app.add_settings(self.web_processing_service_settings())
-                # persistent store settings
-                db_app.add_settings(self.persistent_store_settings())
-                # scheduler settings
-                db_app.add_settings(self.scheduler_settings())
-
-                db_app.save()
+                self.sync_all_settings(db_app)
 
             # If the app is in the database, update developer priority attributes
             elif len(db_apps) == 1:
@@ -1685,20 +1700,7 @@ class TethysAppBase(TethysBase):
                 db_app.index = self.index
                 db_app.root_url = self.root_url
 
-                # custom settings
-                db_app.add_settings(self.custom_settings())
-                # dataset services settings
-                db_app.add_settings(self.dataset_service_settings())
-                # spatial dataset services settings
-                db_app.add_settings(self.spatial_dataset_service_settings())
-                # wps settings
-                db_app.add_settings(self.web_processing_service_settings())
-                # persistent store settings
-                db_app.add_settings(self.persistent_store_settings())
-                # scheduler settings
-                db_app.add_settings(self.scheduler_settings())
-
-                db_app.save()
+                self.sync_all_settings(db_app)
 
                 # In debug mode, update all fields, not just developer priority attributes
                 if hasattr(settings, "DEBUG") and settings.DEBUG:
@@ -1731,11 +1733,28 @@ class TethysAppBase(TethysBase):
         """
         from tethys_apps.models import TethysApp
 
-        try:
-            # Attempt to delete the object
-            TethysApp.objects.filter(package__exact=self.package).delete()
-        except Exception as e:
-            tethys_log.error(e)
+        proceed = None if settings.DEBUG else True
+        if proceed is None:
+            from tethys_cli.cli_colors import write_error
+
+            write_error(
+                f'There was an error loading the "{self.name}" app. '
+                f"Do you want to remove it from the database?"
+            )
+            while proceed is None:
+
+                response = input("[y/n]")
+                if response.lower() == "y":
+                    proceed = True
+                if response.lower() == "n":
+                    proceed = False
+
+        if proceed:
+            try:
+                # Attempt to delete the object
+                TethysApp.objects.filter(package__exact=self.package).delete()
+            except Exception as e:
+                tethys_log.error(e)
 
     @classmethod
     def _log_tethys_app_setting_not_assigned_error(cls, setting_type, setting_name):

tethys_apps/migrations/0003_alter_tethysapp_feedback_emails.py
@@ -1,0 +1,51 @@
+# Generated by Django 3.2.16 on 2022-12-02 18:07
+
+import json
+from django.db import migrations, models
+
+
+feedback_emails = {}
+
+
+def save_feedback_emails_as_json(apps, schema_editor):
+    TethysApp = apps.get_model("tethys_apps", "TethysApp")
+    db_alias = schema_editor.connection.alias
+    for app in TethysApp.objects.using(db_alias).all():
+        feedback_emails[app.id] = json.dumps(app.feedback_emails)
+
+
+def load_saved_feedback_emails(apps, schema_editor):
+    TethysApp = apps.get_model("tethys_apps", "TethysApp")
+    db_alias = schema_editor.connection.alias
+    for app_id, emails in feedback_emails.items():
+        app = TethysApp.objects.using(db_alias).get(id=app_id)
+        app.feedback_emails = emails
+        app.save()
+
+
+def save_feedback_emails_as_list(apps, schema_editor):
+    TethysApp = apps.get_model("tethys_apps", "TethysApp")
+    db_alias = schema_editor.connection.alias
+    for app in TethysApp.objects.using(db_alias).all():
+        feedback_emails[app.id] = json.loads(app.feedback_emails)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("tethys_apps", "0002_auto_20221130_2305"),
+    ]
+
+    operations = [
+        migrations.RunPython(save_feedback_emails_as_json, load_saved_feedback_emails),
+        migrations.RemoveField(
+            model_name="tethysapp",
+            name="feedback_emails",
+        ),
+        migrations.AddField(
+            model_name="tethysapp",
+            name="feedback_emails",
+            field=models.JSONField(blank=True, default=list, null=True),
+        ),
+        migrations.RunPython(load_saved_feedback_emails, save_feedback_emails_as_list),
+    ]

commit 99e67fcb43a176b00d30210ed85c5952e14fbe18

Fixes issues with migration introduced in #890

tethys_apps/admin.py
@@ -427,7 +427,7 @@ def register_custom_group():
 
         admin.site.unregister(Group)
         admin.site.register(Group, CustomGroup)
-    except ProgrammingError:
+    except (ProgrammingError, TypeError):
         tethys_log.warning("Unable to register CustomGroup.")
 
 

tethys_compute/migrations/0002_alter_condorpyjob__remote_input_files.py
@@ -10,14 +10,14 @@ def save_remote_files_as_json(apps, schema_editor):
     CondorPyJob = apps.get_model("tethys_compute", "CondorPyJob")
     db_alias = schema_editor.connection.alias
     for job in CondorPyJob.objects.using(db_alias).all():
-        remote_input_files[job.id] = json.dumps(job._remote_input_files)
+        remote_input_files[job.condorpyjob_id] = json.dumps(job._remote_input_files)
 
 
 def load_saved_remote_files(apps, schema_editor):
     CondorPyJob = apps.get_model("tethys_compute", "CondorPyJob")
     db_alias = schema_editor.connection.alias
     for job_id, files in remote_input_files.items():
-        job = CondorPyJob.objects.using(db_alias).get(id=job_id)
+        job = CondorPyJob.objects.using(db_alias).get(condorpyjob_id=job_id)
         job._remote_input_files = files
         job.save()
 