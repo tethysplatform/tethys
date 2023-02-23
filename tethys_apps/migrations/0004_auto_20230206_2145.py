f422f1207f65f1ddfdb4876ca37f4da3ad93d64c
romer8 <gio.busrom@gmail.com>
2023-02-23 09:50:41 -0700

    Rename CustomJSONSetting to JSONCustomSetting in new migrations

--- tethys_apps/migrations/0004_auto_20230206_2145.py
+++ tethys_apps/migrations/0004_auto_20230206_2145.py
@@ -43,7 +43,7 @@ class Migration(migrations.Migration):
             bases=('tethys_apps.customsettingbase',),
         ),
         migrations.CreateModel(
-            name='CustomJSONSetting',
+            name='JSONCustomSetting',
             fields=[
                 ('customsettingbase_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='tethys_apps.customsettingbase')),
                 ('value', models.JSONField(blank=True, default=dict)),
