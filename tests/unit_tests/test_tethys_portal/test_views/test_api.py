0cc6b6fb4d8be8b4e8016c228ef391156a8e0440
romer8 <gio.busrom@gmail.com>
2023-07-06 22:03:33 -0600

    changes to 4/11 tests still needs some work

--- tests/unit_tests/test_tethys_portal/test_views/test_api.py
+++ tests/unit_tests/test_tethys_portal/test_views/test_api.py
@@ -4,11 +4,6 @@ from django.urls import reverse
 from django.test import override_settings
 
 from tethys_apps.base.testing.testing import TethysTestCase
-from django.conf import settings
-
-prefix_to_path = ""
-if settings.PREFIX_TO_PATH is not None and len(settings.PREFIX_TO_PATH) != 0:
-    prefix_to_path = f"/{settings.PREFIX_TO_PATH}"
 
 
 class TethysPortalApiTests(TethysTestCase):
@@ -69,7 +64,7 @@ class TethysPortalApiTests(TethysTestCase):
         self.assertEqual("foo", json["username"])
         self.assertTrue(json["isAuthenticated"])
 
-    @override_settings(STATIC_URL="/static")
+    # @override_settings(STATIC_URL="/static")
     def test_get_app_valid_id(self):
         """Test get_app API endpoint with valid app id."""
         response = self.client.get(reverse("api:get_app", kwargs={"app": "test-app"}))
@@ -95,11 +90,137 @@ class TethysPortalApiTests(TethysTestCase):
         self.assertEqual("test_app", json["urlNamespace"])
         self.assertEqual("#2c3e50", json["color"])
         self.assertEqual("/static/test_app/images/icon.gif", json["icon"])
-        self.assertEqual(f"{prefix_to_path}/apps/", json["exitUrl"])
-        self.assertEqual(f"{prefix_to_path}/apps/test-app/", json["rootUrl"])
+        self.assertEqual(f"/apps/", json["exitUrl"])
+        self.assertEqual(f"/apps/test-app/", json["rootUrl"])
         self.assertRegex(
             json["settingsUrl"],
-            rf"^{prefix_to_path}/admin/tethys_apps/tethysapp/[0-9]+/change/$",
+            r"^/admin/tethys_apps/tethysapp/[0-9]+/change/$",
+        )
+
+    def test_get_app_invalid_id(self):
+        """Test get_app API endpoint with invalid app id."""
+        response = self.client.get(reverse("api:get_app", kwargs={"app": "foo-bar"}))
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response, JsonResponse)
+        json = response.json()
+        self.assertIn("error", json)
+        self.assertEqual('Could not find app "foo-bar".', json["error"])
+
+
+@override_settings(PREFIX_URL="test/prefix")
+@override_settings(LOGIN_URL="/test/prefix/test/login/")
+@override_settings(STATIC_URL="/test/prefix/test/static/")
+class TethysPortalApiTestsWithPrefix(TethysTestCase):
+    import sys
+    from importlib import reload, import_module
+    from django.conf import settings
+    from django.urls import clear_url_caches
+
+    @classmethod
+    def reload_urlconf(self, urlconf=None):
+        self.clear_url_caches()
+        if urlconf is None:
+            urlconf = self.settings.ROOT_URLCONF
+        if urlconf in self.sys.modules:
+            self.reload(self.sys.modules[urlconf])
+        else:
+            self.import_module(urlconf)
+
+    def set_up(self):
+        self.user = User.objects.create_user(username="foo")
+        self.user.save()
+        self.reload_urlconf()
+        pass
+
+    @override_settings(PREFIX_URL="/")
+    def tearDown(self):
+        self.user.delete()
+        self.reload_urlconf()
+        pass
+
+    def test_get_csrf_not_authenticated(self):
+        """Test get_csrf API endpoint not authenticated."""
+        response = self.client.get(reverse("api:get_csrf"))
+        self.assertEqual(response.status_code, 401)
+
+    def test_get_csrf_authenticated(self):
+        """Test get_csrf API endpoint authenticated."""
+        self.client.force_login(self.user)
+        response = self.client.get(reverse("api:get_csrf"))
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response, HttpResponse)
+        self.assertIn("X-CSRFToken", response.headers)
+
+    def test_get_session_not_authenticated(self):
+        """Test get_session API endpoint not authenticated."""
+        response = self.client.get(reverse("api:get_session"))
+        self.assertEqual(response.status_code, 401)
+
+    def test_get_session_authenticated(self):
+        """Test get_session API endpoint authenticated."""
+        self.client.force_login(self.user)
+        response = self.client.get(reverse("api:get_session"))
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response, JsonResponse)
+        # self.assertIn('Set-Cookie', response.headers)
+        json = response.json()
+        self.assertIn("isAuthenticated", json)
+        self.assertTrue(json["isAuthenticated"])
+
+    def test_get_whoami_not_authenticated(self):
+        """Test get_whoami API endpoint not authenticated."""
+        response = self.client.get(reverse("api:get_whoami"))
+        self.assertEqual(response.status_code, 401)
+
+    def test_get_whoami_authenticated(self):
+        """Test get_whoami API endpoint authenticated."""
+        self.client.force_login(self.user)
+        response = self.client.get(reverse("api:get_whoami"))
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response, JsonResponse)
+        json = response.json()
+        self.assertIn("username", json)
+        self.assertIn("firstName", json)
+        self.assertIn("lastName", json)
+        self.assertIn("email", json)
+        self.assertIn("isAuthenticated", json)
+        self.assertIn("isStaff", json)
+        self.assertEqual("foo", json["username"])
+        self.assertTrue(json["isAuthenticated"])
+
+    # @override_settings(STATIC_URL="/static")
+    def test_get_app_valid_id(self):
+        """Test get_app API endpoint with valid app id."""
+        response = self.client.get(reverse("api:get_app", kwargs={"app": "test-app"}))
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response, JsonResponse)
+        json = response.json()
+        self.assertIn("title", json)
+        self.assertIn("description", json)
+        self.assertIn("tags", json)
+        self.assertIn("package", json)
+        self.assertIn("urlNamespace", json)
+        self.assertIn("color", json)
+        self.assertIn("icon", json)
+        self.assertIn("exitUrl", json)
+        self.assertIn("rootUrl", json)
+        self.assertIn("settingsUrl", json)
+        self.assertEqual("Test App", json["title"])
+        self.assertEqual(
+            "Place a brief description of your app here.", json["description"]
+        )
+        self.assertEqual("", json["tags"])
+        self.assertEqual("test_app", json["package"])
+        self.assertEqual("test_app", json["urlNamespace"])
+        self.assertEqual("#2c3e50", json["color"])
+        self.assertEqual(
+            "/test/prefix/test/static/test_app/images/icon.gif", json["icon"]
+        )
+        self.assertEqual("/test/prefix/apps/", json["exitUrl"])
+        self.assertEqual("/test/prefix/apps/test-app/", json["rootUrl"])
+        self.assertRegex(
+            json["settingsUrl"],
+            r"^/test/prefix/admin/tethys_apps/tethysapp/[0-9]+/change/$",
         )
 
     def test_get_app_invalid_id(self):
