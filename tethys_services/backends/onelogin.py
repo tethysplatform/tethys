89ea73e1aa0e5578b1c5810b8991039500da1110
Nathan Swain <nswain@aquaveo.com>
2023-11-09 13:50:58 -0700

    Fix issue per recommendation by @ezrajrice

--- tethys_services/backends/onelogin.py
+++ tethys_services/backends/onelogin.py
@@ -27,47 +27,6 @@ class OneLoginOIDC(OpenIdConnectAuth):
 
         return subdomain + "/oidc/2"
 
-    def find_valid_key(self, id_token):
-        for key in self.get_jwks_keys():
-            rsakey = jwk.construct(key, algorithm=ALGORITHMS.RS256)
-            message, encoded_sig = id_token.rsplit(".", 1)
-            decoded_sig = base64url_decode(encoded_sig.encode("utf-8"))
-            if rsakey.verify(message.encode("utf-8"), decoded_sig):
-                return key
-
-    def validate_and_return_id_token(self, id_token, access_token):
-        """
-        Validates the id_token according to the steps at
-        http://openid.net/specs/openid-connect-core-1_0.html#IDTokenValidation.
-        """
-        client_id, client_secret = self.get_key_and_secret()
-
-        key = self.find_valid_key(id_token)
-
-        if not key:
-            raise AuthTokenError(self, "Signature verification failed")
-
-        rsakey = jwk.construct(key, algorithm=ALGORITHMS.RS256)
-
-        try:
-            claims = jwt.decode(
-                id_token,
-                rsakey.to_pem().decode("utf-8"),
-                algorithms=[ALGORITHMS.HS256, ALGORITHMS.RS256, ALGORITHMS.ES256],
-                audience=client_id,
-                issuer=self.id_token_issuer(),
-                access_token=access_token,
-                options=self.JWT_DECODE_OPTIONS,
-            )
-        except ExpiredSignatureError:
-            raise AuthTokenError(self, "Signature has expired")
-        except JWTClaimsError as error:
-            raise AuthTokenError(self, str(error))
-        except JWTError:
-            raise AuthTokenError(self, "Invalid signature")
-
-        self.validate_claims(claims)
-
 
 class OneLoginOIDCMultiTenant(MultiTenantMixin, OneLoginOIDC):
     pass
