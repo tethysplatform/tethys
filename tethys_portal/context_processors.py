4bb9d030a75294299d7e764ffcd4456cc9623d36
sdc50 <sdc50@byu.net>
2024-03-21 16:36:35 -0600

    Configure Social Login Buttons to work with SAML

--- tethys_portal/context_processors.py
+++ tethys_portal/context_processors.py
@@ -9,9 +9,11 @@
 """
 
 from .optional_dependencies import has_module
+from django.conf import settings
 
 
 def tethys_portal_context(request):
+    idps = settings.SOCIAL_AUTH_SAML_ENABLED_IDPS if hasattr(settings, 'SOCIAL_AUTH_SAML_ENABLED_IDPS') else {}
     context = {
         "has_analytical": has_module("analytical"),
         "has_recaptcha": has_module("snowpenguin.django.recaptcha2"),
@@ -20,6 +22,7 @@ def tethys_portal_context(request):
         "has_gravatar": has_module("django_gravatar"),
         "has_session_security": has_module("session_security"),
         "has_oauth2_provider": has_module("oauth2_provider"),
+        "idp_backends": idps.keys(),
     }
 
     return context
