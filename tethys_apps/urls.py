d66f16217cd0430b476617bd13dbd5aca3b9076e
ckrew <153777116+ckrew@users.noreply.github.com>
2024-04-01 17:48:22 -0500

    Standalone App Mode (#1015)
    * initial implementation working for stand alone apps
    home and app_library now redirected to the configured app
    admin capabilities still working
    need to figure out a way to get the admin dropdown menu in the stand alone app and clean up header buttons
    
    * new site setting for standalone_app_mode
    adder user menu button to app base header when in standalone app mode
    
    * updated setting name to STANDALONE_APP and removed the necessity to add the controller name since that can be controlled by the application itself
    
    add some logic to the site commands, if a standalone app is given, then update site settings accordingly
    
    fixed some old url patterns for non standalone_app_mode that had been messed up
    
    updated header.html to not show App Library button if standalone app is enabled
    
    * additional updates and url fixes
    
    * liniting
    
    * keep brand text for portal customization
    update the apps library title instead
    
    * cleaned up urls and fixed testing issues with urls
    
    * added new python tests
    black formatted and linted
    
    * updated the  STANDALONE_APP setting to be a boolean MULTIPLE_APP_MODE instead
    MULTIPLE_APP_MODE defaults to False so this new standalone app behavior would be the default
    moved away from updating site settings to use context processor instead
    
    * reverted old site_command code
    black format and lint
    
    * added standalone app settings so users can decide which app to set as the standalone if they want
    
    * MULTIPLE_APP_MODE defaults to True now so no breaking changes happen
    fixed tests with the new MULTIPLE_APP_MODE default
    Added an ObjectDoesNotExist exception for when no apps are installed
    
    * linted and black format
    
    * removed namespace for standalone apps
    updated get_active_app to just grab the standalone app if MULTIPLE_APP_MODE is turned off
    
    * lint and black format
    
    * cleaned up redirect functions for apps
    
    * fixed extension and websocket urls for single app mode
    
    * reverted some conditional statements for get_active_app
    
    * added new portal configurations to the documentation
    
    * black formatted
    
    * removed old test file
    
    * moved user menu to its own template
    
    * black formatted and linted

--- tethys_apps/urls.py
+++ tethys_apps/urls.py
@@ -11,36 +11,69 @@
 import logging
 from django.urls import include, re_path
 from channels.routing import URLRouter
+from django.views.generic import RedirectView
 from tethys_apps.harvester import SingletonHarvester
 from tethys_apps.views import library, send_beta_feedback_email
+from tethys_apps.utilities import get_configured_standalone_app
 from django.conf import settings
 
 tethys_log = logging.getLogger("tethys." + __name__)
 prefix_url = f"{settings.PREFIX_URL}"
 
 urlpatterns = [
-    re_path(r"^$", library, name="app_library"),
     re_path(
         r"^send-beta-feedback/$", send_beta_feedback_email, name="send_beta_feedback"
     ),
 ]
+if settings.MULTIPLE_APP_MODE:
+    urlpatterns.append(re_path(r"^$", library, name="app_library"))
+    url_namespaces = None
+else:
+    standalone_app = get_configured_standalone_app()
+    urlpatterns.append(
+        re_path(
+            r"^apps/",
+            RedirectView.as_view(pattern_name="home"),
+            name="app_library",
+        )
+    )
+    url_namespaces = [standalone_app.url_namespace]
 
 # Append the app urls urlpatterns
 harvester = SingletonHarvester()
-normal_url_patterns = harvester.get_url_patterns()
-handler_url_patterns = harvester.get_handler_patterns()
+normal_url_patterns = harvester.get_url_patterns(url_namespaces=url_namespaces)
+handler_url_patterns = harvester.get_handler_patterns(url_namespaces=url_namespaces)
 
 # configure handler HTTP routes
 http_handler_patterns = []
 for namespace, urls in handler_url_patterns["http_handler_patterns"].items():
-    root_pattern = r"^apps/{0}/".format(namespace.replace("_", "-"))
+
+    if settings.MULTIPLE_APP_MODE:
+        root_pattern = f'apps/{namespace.replace("_", "-")}/'
+    else:
+        root_pattern = ""
+
     if prefix_url is not None and prefix_url != "/":
-        root_pattern = rf"^{prefix_url}/apps/{0}/".format(namespace.replace("_", "-"))
+        root_pattern = f"{prefix_url}/{root_pattern}"
+    root_pattern = rf"^{root_pattern}"
+
     http_handler_patterns.append(re_path(root_pattern, URLRouter(urls)))
 
 # Add app url patterns to urlpatterns, namespaced per app appropriately
 for namespace, urls in normal_url_patterns["app_url_patterns"].items():
-    root_pattern = r"^{0}/".format(namespace.replace("_", "-"))
+    if settings.MULTIPLE_APP_MODE:
+        root_pattern = r"^{0}/".format(namespace.replace("_", "-"))
+    else:
+        root_pattern = ""
+        home_urls = [url for url in urls if url.name == "home"]
+        urlpatterns.append(
+            re_path(
+                r"",
+                include(home_urls[:1]),
+                name="home",
+            ),
+        )
+
     urlpatterns.append(
         re_path(root_pattern, include((urls, namespace), namespace=namespace))
     )
@@ -49,7 +82,11 @@ for namespace, urls in normal_url_patterns["app_url_patterns"].items():
 ext_url_patterns = normal_url_patterns["ext_url_patterns"]
 extension_urls = []
 for namespace, urls in ext_url_patterns.items():
-    root_pattern = r"^{0}/".format(namespace.replace("_", "-"))
+    if settings.MULTIPLE_APP_MODE:
+        root_pattern = r"^{0}/".format(namespace.replace("_", "-"))
+    else:
+        root_pattern = r"^"
+
     extension_urls.append(
         re_path(root_pattern, include((urls, namespace), namespace=namespace))
     )
@@ -62,12 +99,16 @@ handler_websocket_url_patterns = handler_url_patterns["ws_handler_patterns"]
 def prepare_websocket_urls(app_websocket_url_patterns):
     prepared_urls = []
     for namespace, urls in app_websocket_url_patterns.items():
-        root_url = namespace.replace("_", "-")
+        if settings.MULTIPLE_APP_MODE:
+            root_url = f'apps/{namespace.replace("_", "-")}/'
+        else:
+            root_url = ""
+
         for u in urls:
             url_str = str(u.pattern).replace("^", "")
-            namespaced_url_str = f"^apps/{root_url}/{url_str}"
+            namespaced_url_str = rf"^{root_url}{url_str}"
             if prefix_url is not None and prefix_url != "/":
-                namespaced_url_str = f"^{prefix_url}/apps/{root_url}/{url_str}"
+                namespaced_url_str = rf"^{prefix_url}/{root_url}{url_str}"
             namespaced_url = re_path(namespaced_url_str, u.callback, name=u.name)
             prepared_urls.append(namespaced_url)
 
