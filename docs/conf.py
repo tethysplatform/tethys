fb3ed84daf6c9025dd287cbef246c775bd45ae19
Nathan Swain <nswain@aquaveo.com>
2024-07-23 17:29:46 -0600

    Fix the broken heading links

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,7 @@ import django
 from django.conf import settings
 from setuptools_scm import get_version
 from sphinxawesome_theme import ThemeOptions, LinkIcon
+from sphinxawesome_theme.postprocess import Icons
 
 # Mock Dependencies
 # NOTE: No obvious way to automatically anticipate all the sub modules without
@@ -295,10 +296,4 @@ html_theme_options = asdict(theme_options)
 html_collapsible_definitions = True
 
 # Link icon for header links instead of pharagraph icons that are the default
-html_permalinks_icon = (
-    '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">'
-    '<path d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 '
-    "5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 "
-    '0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z">'
-    "</path></svg>"
-)
+html_permalinks_icon = Icons.permalinks_icon
