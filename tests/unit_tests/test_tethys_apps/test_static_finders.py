af5cb81e7b0f6b59fe240a75a18bfec81f5f5c68
Shawn Crawley <shawn.crawley@noaa.gov>
2024-08-29 16:39:52 -0600

    black reformatting

--- tests/unit_tests/test_tethys_apps/test_static_finders.py
+++ tests/unit_tests/test_tethys_apps/test_static_finders.py
@@ -28,13 +28,18 @@ class TestTethysStaticFinder(unittest.TestCase):
         tethys_static_finder = TethysStaticFinder()
         path = os.path.join("test_app", "css", "main.css")
         ret = tethys_static_finder.find(path)
-        self.assertEqual(os.path.join(self.root.lower(), "css", "main.css"), ret.lower())
+        self.assertEqual(
+            os.path.join(self.root.lower(), "css", "main.css"), ret.lower()
+        )
 
     def test_find_all(self):
         tethys_static_finder = TethysStaticFinder()
         path = os.path.join("test_app", "css", "main.css")
         ret = tethys_static_finder.find(path, all=True)
-        self.assertIn(os.path.join(self.root.lower(), "css", "main.css"), list(map(lambda x: x.lower(), ret)))
+        self.assertIn(
+            os.path.join(self.root.lower(), "css", "main.css"),
+            list(map(lambda x: x.lower(), ret)),
+        )
 
     def test_find_location_with_no_prefix(self):
         prefix = None
