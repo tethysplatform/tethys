43ca9eea38c19d6f184cb119c49c488062314c17
Corey Krewson <ckrewson@aquaveo.com>
2024-08-30 14:40:25 -0500

    black format

--- tethys_cli/db_commands.py
+++ tethys_cli/db_commands.py
@@ -275,7 +275,9 @@ def create_db_user(
     db_name = db_name or username
 
     if is_superuser:
-        create_user_command = f"CREATE USER {username} WITH CREATEDB NOCREATEROLE PASSWORD '{password}';"
+        create_user_command = (
+            f"CREATE USER {username} WITH CREATEDB NOCREATEROLE PASSWORD '{password}';"
+        )
     else:
         create_user_command = f"CREATE USER {username} WITH NOCREATEDB NOCREATEROLE NOSUPERUSER PASSWORD '{password}';"
 
