5b67abcc8d0692da0faa8bf30dc9eb64655dd717
sdc50 <sdc50@byu.net>
2024-10-06 14:16:00 -0600

    tasks and delayed update job status

---
+++ tethys_compute/tasks.py
@@ -0,0 +1,48 @@
+"""
+********************************************************************************
+* Name: tasks.py
+* Author: Scott Christensen
+* Created On: 2024
+* Copyright: (c) Tethys Geospatial Foundation 2024
+* License: BSD 2-Clause
+********************************************************************************
+"""
+
+import asyncio
+import logging
+
+logger = logging.getLogger(f"tethys.{__name__}")
+
+
+def create_task(func, /, *args, delay=0, periodic=False, count=None, **kwargs):
+    asyncio.create_task(
+        _run_after_delay(
+            func, *args, delay=delay, periodic=periodic, count=count, **kwargs
+        )
+    )
+
+
+async def _run_after_delay(func, /, *args, delay, periodic, count, **kwargs):
+    await asyncio.sleep(delay)
+    try:
+        logger.info(f'Running task "{func}" with args="{args}" and kwargs="{kwargs}".')
+        result = func(*args, **kwargs)
+        if asyncio.iscoroutine(result):
+            await result
+    except Exception as e:
+        logger.info(e)
+        logger.exception(e)
+    if periodic and (count is None or count > 0):
+
+        if isinstance(count, int):
+            count -= 1
+        asyncio.create_task(
+            _run_after_delay(
+                func, *args, delay=delay, periodic=periodic, count=count, **kwargs
+            )
+        )
+
+
+async def _await_after_delay(coro, delay):
+    await asyncio.sleep(delay)
+    await coro
---
+++ tethys_compute/views/__init__.py
@@ -0,0 +1,7 @@
+from .dask_dashboard import dask_dashboard
+from .update_status import (
+    get_job,
+    do_job_action,
+    update_job_status,
+    update_dask_job_status,
+)
