59f63b90fa6879e61c9d26b8b52f8566966e83c5
Corey Krewson <ckrewson@aquaveo.com>
2024-10-28 17:09:16 -0500

    added ability for ENABLE_RESTRICTED_APP_ACCESS to be a list and maintain original functionality with boolean if desired if an app is in the given list, then it cannot be access unless given explicit permissions

--- tethys_portal/settings.py
+++ tethys_portal/settings.py
@@ -82,7 +82,7 @@ ENABLE_OPEN_SIGNUP = TETHYS_PORTAL_CONFIG.pop("ENABLE_OPEN_SIGNUP", False)
 # Set to True to allow Open Portal mode. This mode supersedes any specific user/group app access permissions
 ENABLE_OPEN_PORTAL = TETHYS_PORTAL_CONFIG.pop("ENABLE_OPEN_PORTAL", False)
 
-# Set to True to allow Open Portal mode. This mode supersedes any specific user/group app access permissions
+# Set to True to allow restricted app access. This mode removes access to apps for nonadmins unless given explicit permission
 ENABLE_RESTRICTED_APP_ACCESS = TETHYS_PORTAL_CONFIG.pop(
     "ENABLE_RESTRICTED_APP_ACCESS", False
 )
