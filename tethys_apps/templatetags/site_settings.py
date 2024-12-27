921c6da54805376f7dc8dfdfa4e04f3ab1aa4b39
Mark Lugar <14322382+araglu@users.noreply.github.com>
2024-12-27 12:04:50 -0600

    Log a warning if var looks like a filename but can't load
    The login page calls load_custom_css() twice with an empty var, so this now returns early for that.

--- tethys_apps/templatetags/site_settings.py
+++ tethys_apps/templatetags/site_settings.py
@@ -1,3 +1,4 @@
+import logging
 from pathlib import Path
 
 from django import template
@@ -10,6 +11,8 @@ static_finder = TethysStaticFinder()
 
 register = template.Library()
 
+log = logging.getLogger(f"tethys.{__name__}")
+
 
 @register.filter
 @stringfilter
@@ -19,9 +22,12 @@ def load_custom_css(var):
     Args:
         var: a filename of CSS to load or CSS text to embed into the page
 
-    Returns: a string of HTML that either embeds CSS text or points to a file
+    Returns:
+        a string of HTML that either embeds CSS text or points to a file
 
     """
+    if not var.strip():
+        return ""
     if var.startswith("/"):
         var = var.lstrip("/")
 
@@ -32,7 +38,18 @@ def load_custom_css(var):
         for path in settings.STATICFILES_DIRS:
             if (Path(path) / var).is_file():
                 return f'<link href="/static/{var}" rel="stylesheet" />'
-    except OSError:
+    except OSError as e:
+        oserror_exception = ": " + str(e)
         pass
+    else:
+        oserror_exception = ""
+
+    common_css_chars = "{};,"
+    if not any(c in var for c in common_css_chars):
+        # This appears to be a filename and not a CSS string
+        log.warning(
+            "Could not load file '%s' for custom styles%s", var, oserror_exception
+        )
+        return ""
 
     return "<style>" + var + "</style>"
