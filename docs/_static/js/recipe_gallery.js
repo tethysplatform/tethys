bb80c0f19260f834fc475536c75f19f8746c1d99
jakeymac <jakeymacaroni@gmail.com>
2025-01-27 10:14:01 -0700

    fixed bug when scrolling in a carousel after entering search terms
    cleaned up code in recipe_gallery.js

--- docs/_static/js/recipe_gallery.js
+++ docs/_static/js/recipe_gallery.js
@@ -1,24 +1,53 @@
 function prepareCarousel(carouselContainer) {
     let leftButton = carouselContainer.querySelector(".carousel-button-left");
     let rightButton = carouselContainer.querySelector(".carousel-button-right");
-
     let carousel = carouselContainer.querySelector(".carousel");
-    // TODO Get all the cards in the carousel that are visible
-    let cards = Array.from(carousel.querySelectorAll(".recipe-card"));
 
-    let cardWidth = cards[0].getBoundingClientRect().width;
-    let cardMargin = parseInt(getComputedStyle(cards[0]).marginRight);
+    function getCards() {
+        // Function to get all visible cards in the carousel that are not cloned
+        return Array.from(carousel.querySelectorAll(".recipe-card")).filter(card => !card.classList.contains("hidden") && !card.classList.contains("cloned"));
+    }
 
-    let extraCardsInLastSlide = cards.length % 4;
+    function getNumOfCards() {
+        return getCards().length;
+    }
+
+    function getMaxIndex() {
+        // Function to get the maximum index of the carousel accounting for the cloned cards at the beginning
+        return getCards().length + 5;
+    }
+
+    let cards = getCards();
+
+    let cardWidth;
+    let cardMargin;
+    let extraCardsInLastSlide;
+
+    if(getNumOfCards() > 0) {
+        cardWidth = cards[0].getBoundingClientRect().width;
+        cardMargin = parseInt(getComputedStyle(cards[0]).marginRight);
+        extraCardsInLastSlide = getNumOfCards() % 4;
+    }
     
+    // Set the initial index to 5 to account for cloned cards at the beginning
     let currentIndex = 5;
-    let maxIndex = cards.length + 5;
 
-    if (cards.length > 4) {
+    // Only show and prepare buttons if there are more than 4 cards
+    if (getNumOfCards() > 4) {
+        leftButton.classList.remove("hidden");
+        rightButton.classList.remove("hidden");
+
         // Clone the first and last 5 cards to the opposite end of the carousel to create the illusion of infinite scrolling
-        let lastSlideClones = cards.slice(-5).map(card => card.cloneNode(true));
-        let firstSlideClones = cards.slice(0, 4).map(card =>card.cloneNode(true));
-
+        let lastSlideClones = cards.slice(-5).map(card => { 
+            let clone = card.cloneNode(true);
+            clone.classList.add("cloned");
+            return clone;
+        });
+        let firstSlideClones = cards.slice(0, 4).map(card => {
+            let clone = card.cloneNode(true);
+            clone.classList.add("cloned");
+            return clone;
+        });
         lastSlideClones.forEach(clone => carousel.insertBefore(clone, cards[0]));
         firstSlideClones.forEach(clone => carousel.appendChild(clone));
         
@@ -30,20 +59,33 @@ function prepareCarousel(carouselContainer) {
         
         rightButton.addEventListener("click", () => {
             updateCarousel(4);
-    });
+        }); 
+    } else {
+        leftButton.classList.add("hidden");
+        rightButton.classList.add("hidden");
+        setTimeout(() => {
+            carousel.style.transition = "none";
+            carousel.style.transform = `translateX(${0}px)`;
+            setTimeout(() => carousel.style.transition = "transform 0.5s ease", 50); // Re-enable transition
+        }, 500);
     }
     
     function updateCarousel(offset) {
+        // Function to update the carousel position by the given offset
+        cards = getCards();
+        let maxIndex = getMaxIndex();
         let remainingCards = maxIndex - (currentIndex + offset);
         
+        // Check if there is less than 4 cards remaining in the carousel, and if so, move that many remaining cards to the right.
         if (offset > 0 && remainingCards <= 4 && remainingCards > 0 && currentIndex < maxIndex) {
             currentIndex += remainingCards
+        // Check if returning to the first slide with less than 4 cards to reach the beginning of the carousel, if so, move that many cards to the left.
         } else if (offset < 0 && currentIndex + offset < 5 && currentIndex > 5) {
             currentIndex = 5;
         } else {
             currentIndex += offset;
         }
-        console.log("Post, Current index: ", currentIndex);
+        // Calculate the new position of the carousel
         let shift = -currentIndex * (cardWidth + cardMargin);
         carousel.style.transform = `translateX(${shift}px)`;
         checkBounds();
@@ -52,29 +94,29 @@ function prepareCarousel(carouselContainer) {
     function checkBounds() {
         // Check if carousel is at either end of the slides, and if so, reset the position to the 
         // opposite end seamlessly without any transition to perform illusion of infinite scrolling
-        if (currentIndex >= cards.length + 5) {
+        let numOfCards = getNumOfCards();
+        let isAtLastSlide = currentIndex >= numOfCards + 5;
+        let isAtFirstSlide = currentIndex < 5;
+
+        if (isAtLastSlide || isAtFirstSlide) {
             setTimeout(() => {
-                carousel.style.transition = "none"; 
-                currentIndex = 5; //Reset to first slide
+                carousel.style.transition = "none"; // Disable transition
+                if (isAtFirstSlide) {
+                    currentIndex = 5;
+                }
+                if (isAtLastSlide) {
+                    currentIndex = numOfCards + 1;
+                }
+
                 let shift = -currentIndex * (cardWidth + cardMargin);
                 carousel.style.transform = `translateX(${shift}px)`;
-                
                 setTimeout(() => carousel.style.transition = "transform 0.5s ease", 50); // Re-enable transition
-            }, 500);
-        } else if (currentIndex < 5) {
-            setTimeout(() => {
-                console.log("Pre, Current index: ", currentIndex);
-                carousel.style.transition = "none";
-                currentIndex = cards.length + 1; // Reset to last slide
-                let shift = -currentIndex * (cardWidth + cardMargin);
-                carousel.style.transform = `translateX(${shift}px)`;
-                console.log("Post, Current index: ", currentIndex);
-                setTimeout(() => carousel.style.transition = "transform 0.5s ease-in-out", 50); // Re-enable transition
             }, 500);
         }
     }
     
     function setInitialPosition() {
+        // Set the initial position of the carousel to the first slide
         carousel.style.transition = "none"; // Disable transition to avoid animation on initial load
         let shift = -currentIndex * (cardWidth + cardMargin);
         carousel.style.transform = `translateX(${shift}px)`;
@@ -83,24 +125,39 @@ function prepareCarousel(carouselContainer) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    const carouselContainers = document.querySelectorAll(".carousel-container");
-    const carousels = document.querySelectorAll(".carousel");
+    let carouselContainers = document.querySelectorAll(".carousel-container");
+    // Initialize all carousel containers
     carouselContainers.forEach(carouselContainer => {
         prepareCarousel(carouselContainer);
     });
 
+    // Add event listener to search bar to filter recipes by tags
     var searchBar = document.querySelector("#recipe-tag-search-bar");
     if (searchBar) {
         searchBar.addEventListener("input", () => {
             let searchValue = searchBar.value.toLowerCase();
-            let cards = document.querySelectorAll(".recipe-card");
-            cards.forEach(card => {
-                const tags = card.querySelector(".recipe-tags").textContent.toLowerCase();
-                if (tags.includes(searchValue)) {
-                    card.classList.remove("hidden");
-                } else {
-                    card.classList.add("hidden");
-                }
+
+            carouselContainers.forEach(carouselContainer => {
+                let carousel = carouselContainer.querySelector(".carousel");
+
+                // Remove all cloned cards in preparation to re-initialize the carousel
+                let clonedCards = carousel.querySelectorAll(".recipe-card.cloned");
+                clonedCards.forEach(clone => clone.remove());
+                
+                // Hide all cards without tags that match the search terms
+                let cards = document.querySelectorAll(".recipe-card");
+                cards.forEach(card => {
+                    // Check if the tags of the card contain the search value
+                    let tags = card.querySelector(".recipe-tags").textContent.toLowerCase();
+                    if (tags.includes(searchValue)) {
+                        card.classList.remove("hidden");
+                    } else {
+                        card.classList.add("hidden");
+                    }
+                });
+
+                // Re-initialize the carousel container
+                prepareCarousel(carouselContainer);
             });
         });
     }
