ed21b0e8d2d3ace249fda60f05ed02f96a3b7e59
Nathan Swain <nswain@aquaveo.com>
2025-02-07 10:15:35 -0700

    Add test and and addition request.user check form tos

--- tests/unit_tests/test_tethys_portal/test_context_processors.py
+++ tests/unit_tests/test_tethys_portal/test_context_processors.py
@@ -30,6 +30,27 @@ class TestTethysPortalContext(TestCase):
         }
         self.assertTrue(context == expected_context)
 
+    @override_settings(MULTIPLE_APP_MODE=True)
+    def test_context_processors_multiple_app_mode_no_request_user(self):
+        mock_request = mock.MagicMock()
+        del mock_request.user
+        assert not hasattr(mock_request, "user")
+        context = context_processors.tethys_portal_context(mock_request)
+
+        expected_context = {
+            "has_analytical": True,
+            "has_terms": False,
+            "has_mfa": True,
+            "has_gravatar": True,
+            "has_session_security": True,
+            "has_oauth2_provider": True,
+            "show_app_library_button": False,
+            "single_app_mode": False,
+            "configured_single_app": None,
+            "idp_backends": {}.keys(),
+        }
+        self.assertTrue(context == expected_context)
+
     @override_settings(MULTIPLE_APP_MODE=False)
     @mock.patch("tethys_portal.context_processors.messages")
     @mock.patch("tethys_portal.context_processors.get_configured_standalone_app")
