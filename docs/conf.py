acb5e9cb2506b6551254b47772e2165fdcb72455
Nathan Swain <nswain@aquaveo.com>
2025-03-05 10:40:00 -0700

    add some words to the wordlist

--- docs/conf.py
+++ docs/conf.py
@@ -311,3 +311,6 @@ html_collapsible_definitions = True
 
 # Link icon for header links instead of pharagraph icons that are the default
 html_permalinks_icon = Icons.permalinks_icon
+
+
+spelling_show_suggestions = True
