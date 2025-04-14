848cc88696546450bb0a23fa9c6b3fdaac4a2e13
Shawn Crawley <shawn.crawley@noaa.gov>
2025-04-14 08:02:14 -0600

    Use "geoscience" verbiage

--- tethys_compute/views/__init__.py
+++ tethys_compute/views/__init__.py
@@ -3,7 +3,7 @@
 * Name: tethys_compute/views/__init__.py
 * Author: Scott Christensen
 * Created On: October 2024
-* Copyright: (c) Tethys Geospatial Foundation 2024
+* Copyright: (c) Tethys Geoscience Foundation 2024
 * License: BSD 2-Clause
 ********************************************************************************
 """
