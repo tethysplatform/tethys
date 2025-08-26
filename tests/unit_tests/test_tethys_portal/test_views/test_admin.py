d19a6d982cfe3cf94a69a893ea63ad491b105150
jakeymac <jakeymacaroni@gmail.com>
2025-08-26 10:21:25 -0600

    test fixes

--- tests/unit_tests/test_tethys_portal/test_views/test_admin.py
+++ tests/unit_tests/test_tethys_portal/test_views/test_admin.py
@@ -38,7 +38,7 @@ class TethysPortalTethysAppTests(unittest.TestCase):
         )
 
     @mock.patch("tethys_portal.views.admin.get_app_media")
-    @mock.patch("tethys_portal.views.admin.get_app_workspace")
+    @mock.patch("tethys_apps.base.paths._get_app_workspace")
     @mock.patch("tethys_portal.views.admin.get_app_class")
     @mock.patch("tethys_portal.views.admin.TethysApp")
     @mock.patch("tethys_portal.views.admin.messages.success")
--- tests/unit_tests/test_tethys_portal/test_views/test_user.py
+++ tests/unit_tests/test_tethys_portal/test_views/test_user.py
@@ -484,7 +484,7 @@ class TethysPortalUserTests(unittest.TestCase):
         )
 
     @mock.patch("tethys_quotas.utilities.log")
-    @mock.patch("tethys_portal.views.user.get_user_workspace")
+    @mock.patch("tethys_portal.views.user._get_user_workspace")
     @mock.patch("tethys_portal.views.user._convert_storage_units")
     @mock.patch("tethys_portal.views.user.SingletonHarvester")
     @mock.patch("tethys_portal.views.user.render")
@@ -525,7 +525,7 @@ class TethysPortalUserTests(unittest.TestCase):
         )
 
     @mock.patch("tethys_portal.views.user.get_user_media")
-    @mock.patch("tethys_portal.views.user.get_user_workspace")
+    @mock.patch("tethys_portal.views.user._get_user_workspace")
     @mock.patch("tethys_portal.views.user.get_app_class")
     @mock.patch("tethys_portal.views.user.TethysApp")
     @mock.patch("tethys_portal.views.user.messages.success")
