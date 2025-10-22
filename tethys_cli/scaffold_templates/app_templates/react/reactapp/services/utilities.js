e90743a2c6fbbe44856efc8380a0ca0d181ab052
Corey Krewson <ckrewson@aquaveo.com>
2025-10-22 10:11:58 -0500

    removed tethysdash specific function removed prefix from getTethysPortalHost

--- tethys_cli/scaffold_templates/app_templates/react/reactapp/services/utilities.js
+++ tethys_cli/scaffold_templates/app_templates/react/reactapp/services/utilities.js
@@ -1,8 +1,5 @@
-const APP_ROOT_URL = process.env.TETHYS_APP_ROOT_URL;
-
 export function getTethysPortalHost() {
   let tethys_portal_host = process.env.TETHYS_PORTAL_HOST;
-  let tethys_prefix_url = process.env.TETHYS_PREFIX_URL.replace(/^\/|\/$/g, "");
 
   // If the .env property is not set, derive from current location
   if (!tethys_portal_host || !tethys_portal_host.length) {
@@ -11,16 +8,8 @@ export function getTethysPortalHost() {
     tethys_portal_host = url.origin;
   }
 
-  if (tethys_prefix_url) {
-    return `${tethys_portal_host}/${tethys_prefix_url}`;
-  }
   return tethys_portal_host;
 }
-
-export const getPublicUrl = (uuid) => {
-  return new URL(getTethysPortalHost() + APP_ROOT_URL + "dashboard/" + uuid)
-    .href;
-};
 
 export function getTethysAppRoot() {
   let tethys_app_root_url = process.env.TETHYS_APP_ROOT_URL;
