5312adfa3f87e38eb71b5df5e035465c2c43bcfb
Michael Souffront <17328135+msouff@users.noreply.github.com>
2025-11-11 17:27:17 -0700

    tests

--- tethys_tenants/models.py
+++ tethys_tenants/models.py
@@ -13,8 +13,8 @@ from tethys_portal.optional_dependencies import optional_import, has_module
 TenantMixin = optional_import("TenantMixin", from_module="django_tenants.models")
 DomainMixin = optional_import("DomainMixin", from_module="django_tenants.models")
 
-if has_module(TenantMixin):
 
+if has_module(TenantMixin):
     class Tenant(TenantMixin):
         name = models.CharField(max_length=100)
         created_on = models.DateField(auto_now_add=True)
@@ -24,6 +24,5 @@ if has_module(TenantMixin):
 
 
 if has_module(DomainMixin):
-
     class Domain(DomainMixin):
         pass
