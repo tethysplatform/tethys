d85a561c054df967f5b48c62facba17ca4622626
Corey Krewson <ckrewson@aquaveo.com>
2025-11-20 13:16:47 -0600

    updated npm packages for dev server to work out of the box added a user header menu added env for react app to track if single_app_mode is on added gravatar url to whoami only return a 401 status for get_session, get_whoami, and get_csrf if the user is not authenticated and the portal is not in open portal mode

--- tethys_cli/scaffold_templates/app_templates/react/reactapp/components/layout/Header.js
+++ tethys_cli/scaffold_templates/app_templates/react/reactapp/components/layout/Header.js
@@ -1,48 +1,89 @@
-import styled from 'styled-components';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Navbar from 'react-bootstrap/Navbar';
-import PropTypes from 'prop-types';
-import { useContext } from 'react';
-import { BsX, BsGear } from 'react-icons/bs';
-import { LinkContainer } from 'react-router-bootstrap';
+import styled from "styled-components";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Navbar from "react-bootstrap/Navbar";
+import PropTypes from "prop-types";
+import { useContext } from "react";
+import { BsX, BsGear } from "react-icons/bs";
+import { LinkContainer } from "react-router-bootstrap";
 
-import HeaderButton from 'components/buttons/HeaderButton';
-import NavButton from 'components/buttons/NavButton';
-import { AppContext } from 'components/context';
+import HeaderButton from "components/buttons/HeaderButton";
+import NavButton from "components/buttons/NavButton";
+import UserHeaderMenu from "components/layout/UserHeaderMenu";
+import { AppContext } from "components/context";
+import { getTethysPortalBase } from "services/utilities";
 
 const CustomNavBar = styled(Navbar)`
   min-height: var(--ts-header-height);
 `;
 
-const Header = ({onNavChange}) => {
-  const {tethysApp, user} = useContext(AppContext);
+const TETHYS_SINGLE_APP_MODE = ["true", "True"].includes(
+  process.env.TETHYS_SINGLE_APP_MODE
+);
+const TETHYS_PORTAL_BASE = getTethysPortalBase();
+
+const Header = ({ onNavChange }) => {
+  const { tethysApp, user } = useContext(AppContext);
   const showNav = () => onNavChange(true);
 
   return (
     <>
-        <CustomNavBar fixed="top" bg="primary" variant="dark" className="shadow">
-          <Container as="header" fluid className="px-4">
-            <NavButton onClick={showNav}></NavButton>
-            <LinkContainer to="/">
-              <Navbar.Brand className="mx-0 d-none d-sm-block">
-                <img 
-                  src={tethysApp.icon} 
-                  width="30" 
-                  height="30"
-                  className="d-inline-block align-top rounded-circle"
-                  alt=""
-                />{' ' + tethysApp.title}
-              </Navbar.Brand>
-            </LinkContainer>
-            <Form inline="true">
-              {user.isStaff && 
-                <HeaderButton href={tethysApp.settingsUrl} tooltipPlacement="bottom" tooltipText="Settings" className="me-2"><BsGear size="1.5rem"/></HeaderButton>
-              }
-              <HeaderButton href={tethysApp.exitUrl} tooltipPlacement="bottom" tooltipText="Exit"><BsX size="1.5rem"/></HeaderButton>
-            </Form>
-          </Container>
-        </CustomNavBar>
+      <CustomNavBar fixed="top" bg="primary" variant="dark" className="shadow">
+        <Container as="header" fluid className="px-4">
+          <NavButton onClick={showNav}></NavButton>
+          <LinkContainer to="/">
+            <Navbar.Brand className="mx-0 d-none d-sm-block">
+              <img
+                src={tethysApp.icon}
+                width="30"
+                height="30"
+                className="d-inline-block align-top rounded-circle"
+                alt=""
+              />
+              {" " + tethysApp.title}
+            </Navbar.Brand>
+          </LinkContainer>
+          <Form className="d-flex align-items-center">
+            {user.isStaff && (
+              <HeaderButton
+                href={tethysApp.settingsUrl}
+                tooltipPlacement="bottom"
+                tooltipText="Settings"
+                className="me-2"
+              >
+                <BsGear size="1.5rem" />
+              </HeaderButton>
+            )}
+            {user.isAuthenticated ? (
+              <UserHeaderMenu
+                user={user}
+                gravatarUrl={user.gravatarUrl}
+                isStaff={user.isStaff}
+              />
+            ) : TETHYS_SINGLE_APP_MODE ? (
+              <HeaderButton
+                onClick={() => {
+                  window.location.assign(
+                    `${TETHYS_PORTAL_BASE}/accounts/login?next=${window.location.pathname}`
+                  );
+                }}
+                tooltipPlacement="bottom"
+                tooltipText="Log In"
+              >
+                Log In
+              </HeaderButton>
+            ) : (
+              <HeaderButton
+                href={tethysApp.exitUrl}
+                tooltipPlacement="bottom"
+                tooltipText="Exit"
+              >
+                <BsX size="1.5rem" />
+              </HeaderButton>
+            )}
+          </Form>
+        </Container>
+      </CustomNavBar>
     </>
   );
 };
