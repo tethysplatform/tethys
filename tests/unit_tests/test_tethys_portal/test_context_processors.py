7db69902bb456ca65975c28180758f3a0baa0930
Nathan <nswain@aquaveo.com>
2025-11-25 09:42:43 -0700

    made context processor tests more resilient to running in different environments

--- tests/unit_tests/test_tethys_portal/test_context_processors.py
+++ tests/unit_tests/test_tethys_portal/test_context_processors.py
@@ -10,16 +10,46 @@ class TestTethysPortalContext(TestCase):
     def tearDown(self):
         pass
 
+    @staticmethod
+    def has_module_side_effect(module_name):
+        modules = {
+            "analytical": False,
+            "cookie_consent": False,
+            "termsandconditions": False,
+            "mfa": True,
+            "django_gravatar": True,
+            "session_security": True,
+            "oauth2_provider": True,
+        }
+        return modules.get(module_name, False)
+
+    @staticmethod
+    def has_terms_side_effect(module_name):
+        modules = {
+            "analytical": False,
+            "cookie_consent": False,
+            "termsandconditions": True,  # Module installed
+            "mfa": True,
+            "django_gravatar": True,
+            "session_security": True,
+            "oauth2_provider": True,
+        }
+        return modules.get(module_name, False)
+
     @override_settings(MULTIPLE_APP_MODE=True)
-    def test_context_processors_multiple_app_mode(self):
+    @mock.patch("tethys_portal.context_processors.has_module")
+    def test_context_processors_multiple_app_mode(self, mock_has_module):
         mock_user = mock.MagicMock(is_authenticated=True, is_active=True)
         mock_request = mock.MagicMock(user=mock_user)
+        mock_has_module.side_effect = (
+            self.has_terms_side_effect
+        )  # Terms and Conditions module installed
         context = context_processors.tethys_portal_context(mock_request)
 
         expected_context = {
-            "has_analytical": True,
+            "has_analytical": False,
             "has_cookieconsent": False,
-            "has_terms": True,
+            "has_terms": True,  # enabled b/c terms module installed and user present
             "has_mfa": True,
             "has_gravatar": True,
             "has_session_security": True,
@@ -33,16 +63,23 @@ class TestTethysPortalContext(TestCase):
         self.assertDictEqual(context, expected_context)
 
     @override_settings(MULTIPLE_APP_MODE=True)
-    def test_context_processors_multiple_app_mode_no_request_user(self):
+    @mock.patch("tethys_portal.context_processors.has_module")
+    def test_context_processors_multiple_app_mode_no_request_user(
+        self, mock_has_module
+    ):
         mock_request = mock.MagicMock()
         del mock_request.user
         assert not hasattr(mock_request, "user")
+
+        mock_has_module.side_effect = (
+            self.has_terms_side_effect
+        )  # Terms and Conditions module installed
         context = context_processors.tethys_portal_context(mock_request)
 
         expected_context = {
-            "has_analytical": True,
+            "has_analytical": False,
             "has_cookieconsent": False,
-            "has_terms": False,
+            "has_terms": False,  # disabled still because no user in request
             "has_mfa": True,
             "has_gravatar": True,
             "has_session_security": True,
@@ -56,20 +93,22 @@ class TestTethysPortalContext(TestCase):
         self.assertDictEqual(context, expected_context)
 
     @override_settings(MULTIPLE_APP_MODE=False)
+    @mock.patch("tethys_portal.context_processors.has_module")
     @mock.patch("tethys_portal.context_processors.messages")
     @mock.patch("tethys_portal.context_processors.get_configured_standalone_app")
     def test_context_processors_single_app_mode(
-        self, mock_get_configured_standalone_app, mock_messages
+        self, mock_get_configured_standalone_app, mock_messages, mock_has_module
     ):
         mock_user = mock.MagicMock(is_authenticated=True, is_active=True)
         mock_request = mock.MagicMock(user=mock_user)
         mock_get_configured_standalone_app.return_value = None
+        mock_has_module.side_effect = self.has_module_side_effect
         context = context_processors.tethys_portal_context(mock_request)
 
         expected_context = {
-            "has_analytical": True,
+            "has_analytical": False,
             "has_cookieconsent": False,
-            "has_terms": True,
+            "has_terms": False,
             "has_mfa": True,
             "has_gravatar": True,
             "has_session_security": True,
@@ -87,14 +126,17 @@ class TestTethysPortalContext(TestCase):
         )
 
     @override_settings(DEBUG=True)
-    def test_context_processors_debug_mode_true(self):
+    @mock.patch("tethys_portal.context_processors.has_module")
+    def test_context_processors_debug_mode_true(self, mock_has_module):
+        mock_has_module.side_effect = self.has_module_side_effect
+
         mock_request = mock.MagicMock()
         del mock_request.user
         assert not hasattr(mock_request, "user")
         context = context_processors.tethys_portal_context(mock_request)
 
         expected_context = {
-            "has_analytical": True,
+            "has_analytical": False,
             "has_cookieconsent": False,
             "has_terms": False,
             "has_mfa": True,
