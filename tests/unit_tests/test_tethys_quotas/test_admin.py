74195041b9180e66a774b643efaa52bb5c4ed039
Nathan <nswain@aquaveo.com>
2025-11-26 08:30:10 -0700

    Convert tethys_quotas/admin.py tests to pytest Uses new approach with pytest_django admin_client

--- tests/unit_tests/test_tethys_quotas/test_admin.py
+++ tests/unit_tests/test_tethys_quotas/test_admin.py
@@ -1,63 +1,125 @@
-from unittest import mock
-from django.test import TestCase
-from tethys_quotas.admin import ResourceQuotaAdmin, UserQuotasSettingInline
-from tethys_quotas.models import UserQuota
+import pytest
+from tethys_quotas.models import ResourceQuota, UserQuota, TethysAppQuota
+from tethys_apps.models import TethysApp
 
 
-class TethysQuotasAdminTest(TestCase):
-    def setUp(self):
-        pass
+@pytest.mark.django_db
+def test_admin_resource_quotas_list(admin_client, load_quotas):
+    assert ResourceQuota.objects.count() == 2
+    response = admin_client.get("/admin/tethys_quotas/resourcequota/")
+    assert response.status_code == 200
 
-    def tearDown(self):
-        pass
 
-    def test_ResourceQuotaAdmin(self):
-        expected_fields = (
-            "name",
-            "description",
-            "default",
-            "units",
-            "codename",
-            "applies_to",
-            "help",
-            "active",
-            "impose_default",
-        )
-        expected_readonly_fields = (
-            "codename",
-            "name",
-            "description",
-            "units",
-            "applies_to",
-        )
-        ret = ResourceQuotaAdmin(mock.MagicMock(), mock.MagicMock())
+@pytest.mark.django_db
+def test_admin_resource_quotas_change(admin_client, load_quotas):
+    assert ResourceQuota.objects.count() == 2
+    user_quota = ResourceQuota.objects.get(applies_to="django.contrib.auth.models.User")
+    response = admin_client.get(
+        f"/admin/tethys_quotas/resourcequota/{user_quota.id}/change/"
+    )
+    assert response.status_code == 200
 
-        self.assertEqual(expected_fields, ret.fields)
-        self.assertEqual(expected_readonly_fields, ret.readonly_fields)
 
-    def test_has_delete_permission(self):
-        mock_request = mock.MagicMock()
-        ret = ResourceQuotaAdmin(mock.MagicMock(), mock.MagicMock())
-        self.assertFalse(ret.has_delete_permission(mock_request))
+@pytest.mark.django_db
+def test_admin_tethys_app_quotas_inline_inactive(admin_client, load_quotas):
+    assert ResourceQuota.objects.count() == 2
+    arq = ResourceQuota.objects.get(applies_to="tethys_apps.models.TethysApp")
+    arq.active = False
+    arq.save()
+    app = TethysApp.objects.get(package="test_app")
+    response = admin_client.get(f"/admin/tethys_apps/tethysapp/{app.id}/change/")
+    assert response.status_code == 200
+    assert b"Tethys App Quotas" in response.content
+    assert TethysAppQuota.objects.count() == 0
 
-    def test_has_add_permission(self):
-        mock_request = mock.MagicMock()
-        ret = ResourceQuotaAdmin(mock.MagicMock(), mock.MagicMock())
-        self.assertFalse(ret.has_add_permission(mock_request))
 
-    def test_UserQuotasSettingInline(self):
-        expected_readonly_fields = ("name", "description", "default", "units")
-        expected_fields = ("name", "description", "value", "default", "units")
-        expected_model = UserQuota
+@pytest.mark.django_db
+def test_admin_tethys_app_quotas_inline_active(admin_client, load_quotas):
+    assert ResourceQuota.objects.count() == 2
+    arq = ResourceQuota.objects.get(applies_to="tethys_apps.models.TethysApp")
+    arq.active = True
+    arq.save()
+    app = TethysApp.objects.get(package="test_app")
+    response = admin_client.get(f"/admin/tethys_apps/tethysapp/{app.id}/change/")
+    assert response.status_code == 200
+    assert b"Tethys App Quotas" in response.content
+    assert TethysAppQuota.objects.count() == 1
+    arq.active = False
+    arq.save()
 
-        ret = UserQuotasSettingInline(mock.MagicMock(), mock.MagicMock())
 
-        self.assertEqual(expected_readonly_fields, ret.readonly_fields)
-        self.assertEqual(expected_fields, ret.fields)
-        self.assertEqual(expected_model, ret.model)
+@pytest.mark.django_db
+def test_admin_tethys_app_quotas_inline_active_impose_default(
+    admin_client, load_quotas
+):
+    assert ResourceQuota.objects.count() == 2
+    arq = ResourceQuota.objects.get(applies_to="tethys_apps.models.TethysApp")
+    arq.active = True
+    arq.impose_default = True
+    arq.save()
+    app = TethysApp.objects.get(package="test_app")
+    response = admin_client.get(f"/admin/tethys_apps/tethysapp/{app.id}/change/")
+    assert response.status_code == 200
+    assert b"Tethys App Quotas" in response.content
+    assert TethysAppQuota.objects.count() == 1
+    arq.active = False
+    arq.impose_default = False
+    arq.save()
 
-    # Need to check
-    # def test_UserQuotasSettingInline_get_queryset(self):
-    #     obj = UserQuotasSettingInline(mock.MagicMock(), mock.MagicMock())
-    #     mock_request = mock.MagicMock()
-    #     obj.get_queryset(mock_request)
+
+@pytest.mark.django_db
+def test_admin_user_quotas_inline_inactive(admin_client, admin_user, load_quotas):
+    assert ResourceQuota.objects.count() == 2
+    urq = ResourceQuota.objects.get(applies_to="django.contrib.auth.models.User")
+    urq.active = False
+    urq.save()
+    response = admin_client.get(f"/admin/auth/user/{admin_user.id}/change/")
+    assert response.status_code == 200
+    assert b"User Quotas" in response.content
+    assert UserQuota.objects.count() == 1
+
+
+@pytest.mark.django_db
+def test_admin_user_quotas_inline_active(admin_client, admin_user, load_quotas):
+    assert ResourceQuota.objects.count() == 2
+    urq = ResourceQuota.objects.get(applies_to="django.contrib.auth.models.User")
+    urq.active = True
+    urq.save()
+    response = admin_client.get(f"/admin/auth/user/{admin_user.id}/change/")
+    assert response.status_code == 200
+    assert b"User Quotas" in response.content
+    assert UserQuota.objects.count() == 1
+    urq.active = False
+    urq.save()
+
+
+@pytest.mark.django_db
+def test_admin_user_quotas_inline_active_impose_default(
+    admin_client, admin_user, load_quotas
+):
+    assert ResourceQuota.objects.count() == 2
+    urq = ResourceQuota.objects.get(applies_to="django.contrib.auth.models.User")
+    urq.active = True
+    urq.impose_default = True
+    urq.save()
+    response = admin_client.get(f"/admin/auth/user/{admin_user.id}/change/")
+    assert response.status_code == 200
+    assert b"User Quotas" in response.content
+    assert UserQuota.objects.count() == 1
+    urq.active = False
+    urq.impose_default = False
+    urq.save()
+
+
+@pytest.mark.django_db
+def test_admin_user_quotas_inline_add_user(admin_client, load_quotas):
+    assert ResourceQuota.objects.count() == 2
+    urq = ResourceQuota.objects.get(applies_to="django.contrib.auth.models.User")
+    urq.active = True
+    urq.save()
+    response = admin_client.get("/admin/auth/user/add/")
+    assert response.status_code == 200
+    assert b"User Quotas" not in response.content
+    assert UserQuota.objects.count() == 0
+    urq.active = False
+    urq.save()
