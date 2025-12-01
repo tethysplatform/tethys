230d200a3dec6277ee94377c0a4dc467d6b36cd2
Corey Krewson <ckrewson@aquaveo.com>
2025-12-01 15:41:46 -0700

    in reactapp, replace getSession and getCSRF with getJWTToken. getJWTToken will return the access and refresh token of an authenticated user or null if noone is logged in
    in tethys, replace api views with django rest apis. removed csrf and session endpoints. updated whoami. added get_token

--- tethys_portal/settings.py
+++ tethys_portal/settings.py
@@ -341,7 +341,10 @@ CHANNEL_LAYERS = {"default": {"BACKEND": "channels.layers.InMemoryChannelLayer"}
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.TokenAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
 }
 
