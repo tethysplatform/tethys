25aa3da4e4cefa2a2e7001d1784d5d7a6891e702
Corey Krewson <ckrewson@aquaveo.com>
2025-12-15 15:17:21 -0600

    added option to get JWT using username and password authentication if using the POST method

--- tethys_portal/settings.py
+++ tethys_portal/settings.py
@@ -342,7 +342,6 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
