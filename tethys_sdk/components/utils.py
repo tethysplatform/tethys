6dd5ffcb42d740736e27c6fb7a9adee59747febc
Shawn Crawley <shawn.crawley@noaa.gov>
2025-12-19 14:22:12 -0700

    Add Component App support for inline features

--- tethys_sdk/components/utils.py
+++ tethys_sdk/components/utils.py
@@ -1,2 +1,2 @@
 from reactpy import component, event  # noqa: F401
-from tethys_components.utils import Props, background_execute  # noqa: F401
+from tethys_components.utils import Props, background_execute, transform_coordinate  # noqa: F401
