9ecedfa910995d58db5484e6ea484f7ad076a159
Nathan <nswain@aquaveo.com>
2025-12-22 11:05:45 -0700

    add Jacob's test fixes for quotas admin

--- tests/unit_tests/test_tethys_quotas/test_admin.py
+++ tests/unit_tests/test_tethys_quotas/test_admin.py
@@ -56,11 +56,33 @@ def test_admin_tethys_app_quotas_inline_active_impose_default(
     arq = ResourceQuota.objects.get(applies_to="tethys_apps.models.TethysApp")
     arq.active = True
     arq.impose_default = True
+    arq.default = 37.6
     arq.save()
     app = TethysApp.objects.get(package="test_app")
     response = admin_client.get(f"/admin/tethys_apps/tethysapp/{app.id}/change/")
     assert response.status_code == 200
     assert b"Tethys App Quotas" in response.content
+    assert b"<p>37.6</p>" in response.content
+    assert TethysAppQuota.objects.count() == 1
+    arq.active = False
+    arq.impose_default = False
+    arq.save()
+
+
+@pytest.mark.django_db
+def test_admin_tethys_app_quotas_inline_active_no_default(admin_client, load_quotas):
+    assert ResourceQuota.objects.count() == 2
+    app = TethysApp.objects.get(package="test_app")
+    arq = ResourceQuota.objects.get(applies_to="tethys_apps.models.TethysApp")
+    arq.active = True
+    arq.impose_default = False
+    TethysAppQuota.objects.create(resource_quota=arq, entity=app, value=None)
+    arq.save()
+
+    response = admin_client.get(f"/admin/tethys_apps/tethysapp/{app.id}/change/")
+    assert response.status_code == 200
+    assert b"Tethys App Quotas" in response.content
+    assert b"<p>--</p>" in response.content
     assert TethysAppQuota.objects.count() == 1
     arq.active = False
     arq.impose_default = False
@@ -76,7 +98,7 @@ def test_admin_user_quotas_inline_inactive(admin_client, admin_user, load_quotas
     response = admin_client.get(f"/admin/auth/user/{admin_user.id}/change/")
     assert response.status_code == 200
     assert b"User Quotas" in response.content
-    assert UserQuota.objects.count() == 1
+    assert UserQuota.objects.count() == 0  # User quota is inactive
 
 
 @pytest.mark.django_db
@@ -101,13 +123,32 @@ def test_admin_user_quotas_inline_active_impose_default(
     urq = ResourceQuota.objects.get(applies_to="django.contrib.auth.models.User")
     urq.active = True
     urq.impose_default = True
+    urq.default = 29.5
     urq.save()
     response = admin_client.get(f"/admin/auth/user/{admin_user.id}/change/")
     assert response.status_code == 200
     assert b"User Quotas" in response.content
+    assert b"<p>29.5</p>" in response.content
     assert UserQuota.objects.count() == 1
     urq.active = False
     urq.impose_default = False
+    urq.save()
+
+
+@pytest.mark.django_db
+def test_admin_user_quotas_inline_no_default(admin_client, admin_user, load_quotas):
+    assert ResourceQuota.objects.count() == 2
+    urq = ResourceQuota.objects.get(applies_to="django.contrib.auth.models.User")
+    UserQuota.objects.create(resource_quota=urq, entity=admin_user, value=None)
+    urq.active = True
+    urq.impose_default = False
+    urq.save()
+    response = admin_client.get(f"/admin/auth/user/{admin_user.id}/change/")
+    assert response.status_code == 200
+    assert b"User Quotas" in response.content
+    assert b"<p>--</p>" in response.content
+    urq.active = False
+    urq.impose_default = True
     urq.save()
 
 
