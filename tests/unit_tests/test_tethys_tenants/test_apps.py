09af76adf87402447e7e0986c03dcc70d1c73203
Jacob Johnson <jakeymacaroni@gmail.com>
2025-12-27 05:06:23 -0700

    Converted tests to pytest

--- tests/unit_tests/test_tethys_tenants/test_apps.py
+++ tests/unit_tests/test_tethys_tenants/test_apps.py
@@ -4,21 +4,23 @@ from django.test import TestCase
 from django.apps import apps
 
 from tethys_tenants import apps as tenant_apps
+import pytest
 
 
-class TethysTenantsAppsTest(TestCase):
-    def test_tethys_tenants_config(self):
-        app_config = apps.get_app_config("tethys_tenants")
-        name = app_config.name
-        verbose_name = app_config.verbose_name
+def test_tethys_tenants_config():
+    app_config = apps.get_app_config("tethys_tenants")
+    name = app_config.name
+    verbose_name = app_config.verbose_name
 
-        self.assertEqual("tethys_tenants", name)
-        self.assertEqual("Tethys Tenants", verbose_name)
-        self.assertTrue(isinstance(app_config, tenant_apps.TethysTenantsConfig))
+    assert "tethys_tenants" == name
+    assert "Tethys Tenants" == verbose_name
+    assert isinstance(app_config, tenant_apps.TethysTenantsConfig)
 
-    @mock.patch("tethys_portal.optional_dependencies.has_module", return_value=False)
-    def test_tethys_tenants_config_unavailable(self, mock_has_module):
-        importlib.reload(tenant_apps)
 
-        # Verify has_module was called
-        mock_has_module.assert_called()
+
+@mock.patch("tethys_portal.optional_dependencies.has_module", return_value=False)
+def test_tethys_tenants_config_unavailable(mock_hm):
+    importlib.reload(tenant_apps)
+
+    # Verify has_module was called
+    mock_hm.assert_called()
