75d826dc5bda27bf08e3bdc62bffa6f518c8921e
ShawnCrawley-NOAA <shawn.crawley@noaa.gov>
2026-01-12 13:59:42 -0700

    Fix active_venv extraction logic (#1230)
    * Fix active_venv extraction logic
    
    Modify active_venv retrieval to only remove parenthesis if they are actually present.
    
    * Adds two more bugfixes and a test
    
    * Applies black formatting
    
    * Updates test artifacts

--- tests/unit_tests/test_tethys_components/test_resources/test_library/_test_page_3_expected.js
+++ tests/unit_tests/test_tethys_components/test_resources/test_library/_test_page_3_expected.js
@@ -61,6 +61,12 @@ function htmlToJsonObject(element) {
 }
 
 function jsonSanitizeObject(obj, maxDepth, refs, depth) {
+    try {
+        JSON.stringify(obj);
+        return obj;
+    } catch (e) {
+        "pass";
+    }
     if (!maxDepth) {
         maxDepth = 4;
     }
@@ -83,7 +89,6 @@ function jsonSanitizeObject(obj, maxDepth, refs, depth) {
         return undefined;
     }
     refs.push(obj);
-    delete obj.nativeEvent;
     let newObj = Array.isArray(obj) ? [] : {};
     if (depth > maxDepth) {
         newObj = "BEYOND MAX DEPTH";
--- tests/unit_tests/test_tethys_components/test_resources/test_library/_test_page_4_expected.js
+++ tests/unit_tests/test_tethys_components/test_resources/test_library/_test_page_4_expected.js
@@ -77,6 +77,12 @@ function htmlToJsonObject(element) {
 }
 
 function jsonSanitizeObject(obj, maxDepth, refs, depth) {
+    try {
+        JSON.stringify(obj);
+        return obj;
+    } catch (e) {
+        "pass";
+    }
     if (!maxDepth) {
         maxDepth = 4;
     }
@@ -99,7 +105,6 @@ function jsonSanitizeObject(obj, maxDepth, refs, depth) {
         return undefined;
     }
     refs.push(obj);
-    delete obj.nativeEvent;
     let newObj = Array.isArray(obj) ? [] : {};
     if (depth > maxDepth) {
         newObj = "BEYOND MAX DEPTH";
