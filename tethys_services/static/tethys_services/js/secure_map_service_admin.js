ce4ca8933ac9d39edee1f97a9859bf8486db39fd
Jacob Johnson <jakeymacaroni@gmail.com>
2026-09-08 18:23:59 -0600

    Removed debugging console.log statements

--- tethys_services/static/tethys_services/js/secure_map_service_admin.js
+++ tethys_services/static/tethys_services/js/secure_map_service_admin.js
@@ -6,15 +6,12 @@ document.addEventListener("DOMContentLoaded", function () {
         const method = authMethodField.value
 
         if (method === "api_key") {
-            console.log("API Key authentication selected")
             apiKeyRow.style.display = ""
             oauthProviderRow.style.display = "none"
         } else if (method === "oauth") {
-            console.log("OAuth authentication selected")
             oauthProviderRow.style.display = ""
             apiKeyRow.style.display = "none"
         } else {
-            console.log("No authentication selected")
             apiKeyRow.style.display = "none"
             oauthProviderRow.style.display = "none"
         }
